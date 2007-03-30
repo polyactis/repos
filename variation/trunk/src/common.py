6c0fb9251fb556d61a238f9449db282bbabeb51d
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2007-03-30 06:16:09 +0000

    add mappings for '-', 'N', and ambiguous letters ('M', 'R', 'W', 'S', 'Y', 'K') into nt2number
    git-svn-id: file:///usr/local/svn/repos@38 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/common.py
+++ variation/trunk/src/common.py
@@ -2,23 +2,37 @@
 2007-02-19
 	common stuff for variation/src
 """
-nt2number = {'NA': 0,
+
+
+"""
+2007-03-29
+	add mappings for '-', 'N', and ambiguous letters ('M', 'R', 'W', 'S', 'Y', 'K')
+"""
+nt2number = {'-': -1,	#deletion
+	'N': 0,
+	'NA': 0,
 	'A': 1,
 	'C': 2,
 	'G': 3,
 	'T': 4,
 	'AC':5,
 	'CA':5,
+	'M':5,
 	'AG':6,
 	'GA':6,
+	'R':6,
 	'AT':7,
 	'TA':7,
+	'W':7,
 	'CG':8,
 	'GC':8,
+	'S':8,
 	'CT':9,
 	'TC':9,
+	'Y':9,
 	'GT':10,
-	'TG':10
+	'TG':10,
+	'K':10
 	}
 
 number2nt = {0: 'NA',
