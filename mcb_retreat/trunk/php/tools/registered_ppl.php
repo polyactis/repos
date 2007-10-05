2885e8272b2a0557491a7d904c8a879687de5880
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2007-10-05 20:27:07 +0000

    update
    git-svn-id: file:///usr/local/svn/repos@151 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- mcb_retreat/trunk/php/tools/registered_ppl.php
+++ mcb_retreat/trunk/php/tools/registered_ppl.php
@@ -5,19 +5,24 @@
 $link = pg_connect("host=localhost dbname=yhdb user=yh password=123456")
 		or die('Could not connect: ' . pg_last_error());
 
-$result = pg_query( "select name, pi, nights, roommate1, roommate2, roommate3, roommate4 from retreat.register order by name,pi" )
+$result = pg_query( "select name, pi, nights,tshirt, roommate1, roommate2, roommate3, roommate4,date_created from retreat.register order by id" )
 or die("SELECT Error: ".pg_last_error());
 $num_rows = pg_num_rows($result);
+print '<pre>';
+print "$num_rows people has registered";
+print '</pre>';
 //
 print "<table width=829  border=2 align=center cellpadding=1 cellspacing=0>\n";
 print " <tr bgcolor=#42C1FF>";
 print "    <td>Name</td>";
 print     " <td>PI Name </td>";
 print     " <td>Nights</td>";
+print     " <td>Tshirt</td>";
 print     " <td>Roomate1</td>";
 print     " <td>Roomate2</td>";
 print     " <td>Roomate3</td>";
 print     " <td>Roomate4</td>";
+print     " <td>Date</td>";
 print "  </tr>";
 //print "<table width=200 border=1>\n";
 while ($get_info = pg_fetch_row($result)){
