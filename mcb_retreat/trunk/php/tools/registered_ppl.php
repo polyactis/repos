6a880caec72e55ea80775afd003e48a506ac26bc
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2007-10-05 07:56:29 +0000

    modify it to fit postgresql connection
    git-svn-id: file:///usr/local/svn/repos@150 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- mcb_retreat/trunk/php/tools/registered_ppl.php
+++ mcb_retreat/trunk/php/tools/registered_ppl.php
@@ -2,37 +2,34 @@
 <head><title>MCB Retreat</title></head>
 <body>
 <?php
-$db="retreat";
-$link = mysql_connect("localhost","root","");
-if (! $link)
-die("Couldn't connect to MySQL");
-mysql_select_db($db , $link)
-or die("Couldn't open $db: ".mysql_error());
-$result = mysql_query( "SELECT * FROM register" )
-or die("SELECT Error: ".mysql_error());
-$num_rows = mysql_num_rows($result);
+$link = pg_connect("host=localhost dbname=yhdb user=yh password=123456")
+		or die('Could not connect: ' . pg_last_error());
+
+$result = pg_query( "select name, pi, nights, roommate1, roommate2, roommate3, roommate4 from retreat.register order by name,pi" )
+or die("SELECT Error: ".pg_last_error());
+$num_rows = pg_num_rows($result);
 //
 print "<table width=829  border=2 align=center cellpadding=1 cellspacing=0>\n";
 print " <tr bgcolor=#42C1FF>";
-print "   <td>ID</td>";
 print "    <td>Name</td>";
-print     " <td>Email </td>";
 print     " <td>PI Name </td>";
-print     " <td>T-Shirt Size </td>";
+print     " <td>Nights</td>";
 print     " <td>Roomate1</td>";
 print     " <td>Roomate2</td>";
 print     " <td>Roomate3</td>";
 print     " <td>Roomate4</td>";
 print "  </tr>";
 //print "<table width=200 border=1>\n";
-while ($get_info = mysql_fetch_row($result)){
+while ($get_info = pg_fetch_row($result)){
 print "<tr bgcolor=#B9EBF9>\n";
 foreach ($get_info as $field)
 print "\t<td>$field</td>\n";
 print "</tr>\n";
 }
 print "</table>\n";
-mysql_close($link);
+print "";
+
+pg_close($link);
 ?>
 </body>
 </html>
