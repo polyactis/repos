d73c1437db1899e252ea753dfc21539f478c00ca
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2007-10-15 23:25:36 +0000

    a file to handle some latex output (escape characters , etc)
    git-svn-id: file:///usr/local/svn/repos@169 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

---
+++ pymodule/trunk/latex.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+2007-10-15
+	modules to deal with latex output
+"""
+
+esc_character_dict = {'_':'\\_'}
+
+def escape_characters(latex_line):
+	new_latex_line = ''
+	for character in latex_line:
+		if character in esc_character_dict:
+			new_latex_line += esc_character_dict[character]
+		else:
+			new_latex_line += character
+	return new_latex_line
