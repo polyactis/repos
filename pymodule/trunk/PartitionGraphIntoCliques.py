7a087cdb6f9fb76d3ee671aad84a03ce2aa0b6b8
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2007-10-15 23:24:11 +0000

    make pymodule/ a python module so that you could do from pymodule.latex import escape_characters
    git-svn-id: file:///usr/local/svn/repos@168 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- pymodule/trunk/PartitionGraphIntoCliques.py
+++ pymodule/trunk/PartitionGraphIntoCliques.py
@@ -7,30 +7,51 @@
 """
 
 from clique import clique
+from networkx import cliques
+import numpy
 
 class PartitionGraphIntoCliques:
-	def __init__(self, debug=0):
+	def __init__(self, algorithm_type=1, debug=0):
+		"""
+		2007-10-02
+			add algorithm_type
+		"""
+		self.algorithm_type = int(algorithm_type)
 		self.debug = int(debug)
+		
+		self.whichSubPartition = {1: self.subPartition,
+			2: self.subPartitionNx}
 		self.clique_ls = []
 		self.clique_ins = clique()
 	
 	def subPartition(self, graph):
 		self.clique_ins.max_clique(graph)
-		max_clique_ls = self.clique_ins.max_clique_ls
-		self.clique_ls.append(max_clique_ls)
-		graph.delete_nodes_from(max_clique_ls)
+		max_clique = self.clique_ins.max_clique_ls
+		self.clique_ls.append(max_clique)
+		graph.delete_nodes_from(max_clique)
 		if graph.number_of_nodes()==0:	#graph.size() is different, = number_of_edges()
 			return
 		else:
 			self.subPartition(graph)
 	
+	def subPartitionNx(self, graph):
+		max_cover_clique_ls = cliques.find_cliques(graph)
+		max_clique_index = numpy.argmax(map(len,max_cover_clique_ls))
+		max_clique = max_cover_clique_ls[max_clique_index]
+		self.clique_ls.append(max_clique)
+		graph.delete_nodes_from(max_clique)
+		if graph.number_of_nodes()==0:	#graph.size() is different, = number_of_edges()
+			return
+		else:
+			self.subPartitionNx(graph)
+	
 	def partition(self, graph):
 		if self.debug:
 			import pdb
 			pdb.set_trace()
-		graph1 = graph.copy()
+		graph1 = graph.copy()	# to avoid getting the original graph emptified.
 		self.clique_ls = []
-		self.subPartition(graph1)
+		self.whichSubPartition[self.algorithm_type](graph1)
 
 if __name__ == '__main__':
 	import networkx as nx
@@ -44,7 +65,24 @@ if __name__ == '__main__':
 	#G.add_edge(7,8)
 	#G.add_edge(3,4)
 	#G.add_edge(3,6)
-	PartitionGraphIntoCliques_ins = PartitionGraphIntoCliques(debug=1)
+	PartitionGraphIntoCliques_ins = PartitionGraphIntoCliques(debug=0)
 	PartitionGraphIntoCliques_ins.partition(G)
 	print "cliques:", PartitionGraphIntoCliques_ins.clique_ls
 	
+	"""
+	2007-10-02 a testing case that the clique algorithm from clique.py lasts forever
+	import sys, os
+	sys.path.insert(0, os.path.join(os.path.expanduser('~/script/variation/src')))
+	from misc import construct_graph_out_of_identity_pair
+	input_fname = os.path.expanduser('~/script/variation/stock20070919/data_d110_c0_5.tsv')
+	import cPickle
+	inf = open('%s_identity_pair_ls'%os.path.splitext(input_fname)[0], 'r')
+	identity_pair_ls = cPickle.load(inf)
+	del inf
+	strain_iden_g = construct_graph_out_of_identity_pair(identity_pair_ls)
+	import networkx as nx
+	strain_iden_g_cc = nx.connected_components(strain_iden_g)
+	g_cc1_sg = strain_iden_g.subgraph(strain_iden_g_cc[1])
+	PartitionGraphIntoCliques_ins.partition(g_cc1_sg)
+	print "cliques:", PartitionGraphIntoCliques_ins.clique_ls
+	"""
