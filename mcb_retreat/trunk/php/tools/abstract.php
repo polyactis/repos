96304537ea26c44df3373df7225f34b3c1106702
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2007-10-17 07:34:32 +0000

    fit for postgresql connection
    git-svn-id: file:///usr/local/svn/repos@174 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- mcb_retreat/trunk/php/tools/abstract.php
+++ mcb_retreat/trunk/php/tools/abstract.php
@@ -2,37 +2,34 @@
 <head><title>MCB Retreat</title></head>
 <body>
 <?php
-$db="retreat";
-$link = mysql_connect("localhost","root","");
-if (! $link)
-die("Couldn't connect to MySQL");
-mysql_select_db($db , $link)
-or die("Couldn't open $db: ".mysql_error());
-$result = mysql_query( "SELECT * FROM abstract" )
-or die("SELECT Error: ".mysql_error());
-$num_rows = mysql_num_rows($result);
+$link = pg_connect("host=localhost dbname=yhdb user=yh password=123456")
+		or die('Could not connect: ' . pg_last_error());
+
+$result = pg_query( "select name, pi, pref, title from retreat.abstract order by pref, name" )
+or die("SELECT Error: ".pg_last_error());
+$num_rows = pg_num_rows($result);
+print '<pre>';
+print "$num_rows abstracts";
+print '</pre>';
 //
-print "<table width=1000  border=2 align=center cellpadding=1 cellspacing=0>\n";
+print "<table width=829  border=2 align=center cellpadding=1 cellspacing=0>\n";
 print " <tr bgcolor=#42C1FF>";
-print "   <td>ID</td>";
-print "    <td>Name</td>";
-print     " <td>Email </td>";
-print     " <td>PI Name </td>";
-print     " <td>Preference:  </td>";
-print     " <td width=10%>Title</td>";
-print     " <td>Author's</td>";
-print     " <td>Abstract</td>";
-
+print "   <td>Name</td>";
+print "    <td>PI</td>";
+print "    <td>pref</td>";
+print     " <td>title </td>";
 print "  </tr>";
-
-while ($get_info = mysql_fetch_row($result)){
+//print "<table width=200 border=1>\n";
+while ($get_info = pg_fetch_row($result)){
 print "<tr bgcolor=#B9EBF9>\n";
 foreach ($get_info as $field)
 print "\t<td>$field</td>\n";
 print "</tr>\n";
 }
 print "</table>\n";
-mysql_close($link);
+print "";
+
+pg_close($link);
 ?>
 </body>
 </html>
