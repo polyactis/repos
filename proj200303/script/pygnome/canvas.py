9bb751556150c07e033d7e2ba8eeb7f71655eabc
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-01-21 00:53:07 +0000

    modify to fit the program into gtk2 and new gnomecanvas
    git-svn-id: file:///usr/local/svn/repos@300 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- proj200303/script/pygnome/canvas.py
+++ proj200303/script/pygnome/canvas.py
@@ -1,15 +1,16 @@
 #! /usr/bin/env python2.1
 
 from gtk import *
+import gnomecanvas
 from gnome.ui import *
-import GdkImlib
+#import GdkImlib
 
-win = GtkWindow()
+win = Window()
 #button=GtkButton('EMBEDDED')
-win.connect('destroy', mainquit)
+win.connect('destroy', main_quit)
 win.set_title('Canvas test')
 
-canvas = GnomeCanvas(aa=TRUE)
+canvas = gnomecanvas.Canvas(aa=True)
 #canvas.set_usize(300, 300)
 canvas.set_scroll_region(0,0,300,300)
 win.add(canvas)
@@ -17,9 +18,9 @@ win.add(canvas)
 #board=[]
 #canvas.set_data('board',board)
 root=canvas.root()
-group=root.add('group')
+group=root.add(gnomecanvas.CanvasGroup())
 #board.append(group)
-group.add('line',points=(0,0, 100,100),width_pixels=10,fill_color='slategray')
+group.add(gnomecanvas.CanvasLine,points=(0,0, 100,100),width_pixels=10,fill_color='slategray')
 
 #group=GnomeCanvasGroup()
 #group=root.add('group')
@@ -27,11 +28,11 @@ group.add('line',points=(0,0, 100,100),width_pixels=10,fill_color='slategray')
 #group=canvas.root().add('group')
 #board.append(group)
 group.move(50,50)
-group.add('polygon', points=(10,10, 90,10, 90,90, 10,90, 0,50),
-		  width_pixels=5, fill_color='slategray')
+group.add(gnomecanvas.CanvasPolygon, points=(10,10, 90,10, 90,90, 10,90, 0,50),
+		  width_units=5, fill_color='slategray')
 #group.add('rect',x1=100,y1=100,x2=200,y2=150,fill_color='lightyellow',outline_color='black',width_units=1.0)
 
-#canvas.root().add('text',text='SET',x=150,y=125,fill_color='black',font='-adobe-helvetica-bold-r-normal--*-240-75-75-p-*-*-1',anchor=ANCHOR_CENTER)
+canvas.root().add(gnomecanvas.CanvasText,text='SET',x=150,y=125,fill_color='black',font='-adobe-helvetica-bold-r-normal--*-240-75-75-p-*-*-1',anchor=ANCHOR_CENTER)
 
 
 #file='/tmp/bubbles/B_lectin.png'
@@ -43,5 +44,5 @@ group.add('polygon', points=(10,10, 90,10, 90,90, 10,90, 0,50),
 win.show_all()
 
 
-mainloop()
+main()
 
