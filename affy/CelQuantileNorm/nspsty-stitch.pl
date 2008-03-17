9cef9d1a3411f44b60c995a85fe8392e91742142
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-03-17 03:21:24 +0000

    reformat the code using eclipse perl plugin
    git-svn-id: file:///usr/local/svn/repos@393 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- affy/CelQuantileNorm/nspsty-stitch.pl
+++ affy/CelQuantileNorm/nspsty-stitch.pl
@@ -9,10 +9,10 @@ use IO::File;
 
 my %opts;
 
-getopts('i:o:m:h', \%opts);
+getopts( 'i:o:m:h', \%opts );
 
-if ($opts{'h'}) {
-    print << 'USAGE';
+if ( $opts{'h'} ) {
+	print << 'USAGE';
 Usage: 
     nspsty-stitch.pl -o stem -i annotAB.padded+sorted NSPdir1 STYdir1 \
                  NSPdir2 STYdir2 NSPdir3 STYdir3 ...
@@ -25,24 +25,25 @@ Options:
                        (not recommended for duplicate sample id's)
     -h             displaying this help message
 USAGE
-    exit 0;
+	exit 0;
 }
 
-unless ($opts{'i'}) { die "You have to specify a concatenated list file!\n" } ;
-unless ($opts{'o'}) { die "You have to specify an output file stem name!\n" } ;
+unless ( $opts{'i'} ) { die "You have to specify a concatenated list file!\n" }
+unless ( $opts{'o'} ) { die "You have to specify an output file stem name!\n" }
 
 my %pw2sample = ();
 
-if($opts{'m'}) {
-    # print "opening ", $opts{'m'}, "\n";
-    open(MAPFILE, $opts{'m'});
-    while(<MAPFILE>) {
-	my ($sample, $platewell);
-	($sample, undef, undef, undef, $platewell, undef) = split;
-	$platewell = uc $platewell;
-	$pw2sample{$platewell . "_A"} = $sample . "_A";
-	$pw2sample{$platewell . "_B"} = $sample . "_B";
-    }
+if ( $opts{'m'} ) {
+
+	# print "opening ", $opts{'m'}, "\n";
+	open( MAPFILE, $opts{'m'} );
+	while (<MAPFILE>) {
+		my ( $sample, $platewell );
+		( $sample, undef, undef, undef, $platewell, undef ) = split;
+		$platewell                      = uc $platewell;
+		$pw2sample{ $platewell . "_A" } = $sample . "_A";
+		$pw2sample{ $platewell . "_B" } = $sample . "_B";
+	}
 }
 
 #foreach my $key (keys %pw2sample) {
@@ -51,18 +52,19 @@ if($opts{'m'}) {
 
 my $current_chrom = "";
 
-my $full_list = IO::File->new($opts{'i'}, "r"); 
+#03/16/08 yh: open the annotation file
+my $full_list = IO::File->new( $opts{'i'}, "r" );
 
-my @NSPdirs =();
-my @STYdirs =();
+my @NSPdirs = ();
+my @STYdirs = ();
 
-while(@ARGV) {
-    push @NSPdirs, (shift @ARGV);
-    push @STYdirs, (shift @ARGV);
+while (@ARGV) {
+	push @NSPdirs, ( shift @ARGV );
+	push @STYdirs, ( shift @ARGV );
 }
 
-if ((scalar @NSPdirs) != (scalar @STYdirs)) {
-    die "You don't have the files in pairs!\n";
+if ( ( scalar @NSPdirs ) != ( scalar @STYdirs ) ) {
+	die "You don't have the files in pairs!\n";
 }
 
 my $num_pairs = scalar @NSPdirs;
@@ -72,130 +74,143 @@ my @styfiles = ();
 
 my $outfile = undef;
 
-my $snp = undef;
-my $nsp_snp = undef;
-my $sty_snp = undef;
-my $nsp_the_rest = ""; 
+my $snp          = undef;
+my $nsp_snp      = undef;
+my $sty_snp      = undef;
+my $nsp_the_rest = "";
 my $sty_the_rest = "";
 
-while (my $current_line = <$full_list>) {
-    chomp $current_line;
-    ($snp, undef, $chrom_pos, undef, undef) = split /\s+/, $current_line, 4;
-    #print $snp, " ", $chrom_pos, "\n";
-    (my $chrom = $chrom_pos) =~ s/_.+//;
+while ( my $current_line = <$full_list> ) {
+	chomp $current_line;
+	( $snp, undef, $chrom_pos, undef, undef ) = split /\s+/, $current_line, 4;
 
-    my $read_nsp = 0;
-    my $read_sty = 0;
-    my $current_outname = "";
+	#print $snp, " ", $chrom_pos, "\n";
+	( my $chrom = $chrom_pos ) =~ s/_.+//;
 
-    if ($chrom ne $current_chrom) {
-	if ($outfile) {
-	    $outfile->close();
-	    foreach my $file (@nspfiles, @nspfiles) {
-		$file->close();		
-	    }
-	}
-	$current_outname = $opts{'o'}. "_${chrom}.txt.gz";
-	$outfile = IO::File->new("|gzip -c > $current_outname");
-	
-	print $outfile join("\t", ("AFFYID", "RSID", "pos", "AlleleA", "AlleleB"));
-	
-	@nspfiles = ();
-	@styfiles = ();
+	my $read_nsp        = 0;
+	my $read_sty        = 0;
+	my $current_outname = "";
 
-	foreach my $nspdir (@NSPdirs) {
-	    push @nspfiles, (IO::File->new($nspdir . "/NSP_chromosome_${chrom}.txt", "r"));
-	}
-	foreach my $stydir (@STYdirs) {
-	    push @styfiles, (IO::File->new($stydir . "/STY_chromosome_${chrom}.txt", "r"));
-	}
-	
-	for (my $i = 0; $i < $num_pairs ; $i++) {
-	    my $nspheader = ${nspfiles[$i]}->getline();
-	    my $styheader = ${styfiles[$i]}->getline();
-	    if ($nspheader ne $styheader) {
-		print $nspheader;
-		print $styheader;
-		die "header don't match\n";
-	    } else {
-		chomp $nspheader;
-		if($opts{'m'}) {
-		    $nspheader =~ s/^\s+//;  #remove tab at beginning
-		    my @a = split /\s+/, $nspheader;
-		    my @b = map {$_ =~ s/^WTCCC01-// ; $pw2sample{$_} } @a;
-		    print $outfile "\t", join("\t", @b);
-		} else {
-		    print $outfile $nspheader;
+	if ( $chrom ne $current_chrom ) {
+		if ($outfile) {
+			$outfile->close();
+			foreach my $file ( @nspfiles, @nspfiles ) {
+				$file->close();
+			}
 		}
-	    }
+		$current_outname = $opts{'o'} . "_${chrom}.txt.gz";
+		$outfile         = IO::File->new("|gzip -c > $current_outname");
+
+		print $outfile
+		  join( "\t", ( "AFFYID", "RSID", "pos", "AlleleA", "AlleleB" ) );
+
+		@nspfiles = ();
+		@styfiles = ();
+
+		foreach my $nspdir (@NSPdirs) {
+			push @nspfiles,
+			  ( IO::File->new( $nspdir . "/NSP_chromosome_${chrom}.txt", "r" )
+			  );
+		}
+		foreach my $stydir (@STYdirs) {
+			push @styfiles,
+			  ( IO::File->new( $stydir . "/STY_chromosome_${chrom}.txt", "r" )
+			  );
+		}
+
+		for ( my $i = 0 ; $i < $num_pairs ; $i++ ) {
+			my $nspheader = ${ nspfiles [$i] }->getline();
+			my $styheader = ${ styfiles [$i] }->getline();
+			if ( $nspheader ne $styheader ) {
+				print $nspheader;
+				print $styheader;
+				die "header don't match\n";
+			}
+			else {
+				chomp $nspheader;
+				if ( $opts{'m'} ) {
+					$nspheader =~ s/^\s+//;    #remove tab at beginning
+					my @a = split /\s+/, $nspheader;
+					my @b = map { $_ =~ s/^WTCCC01-//; $pw2sample{$_} } @a;
+					print $outfile "\t", join( "\t", @b );
+				}
+				else {
+					print $outfile $nspheader;
+				}
+			}
+		}
+		print $outfile "\n";
+		&read_nsp();
+		&read_sty();
+		$current_chrom = $chrom;
 	}
-	print $outfile "\n";
-	&read_nsp();
-	&read_sty();
-	$current_chrom = $chrom ;
-    }
 
-    $current_line =~ s/[012]._0*//; # remove the chromosome part
-    print $outfile $current_line;
+	$current_line =~ s/[012]._0*//;    # remove the chromosome part
+	print $outfile $current_line;
 
-    if (($nsp_snp) && ($snp eq $nsp_snp)) {
-	print $outfile $nsp_the_rest;
-	&read_nsp();
-    } elsif (($sty_snp) && ($snp eq $sty_snp)) {
-	print $outfile $sty_the_rest;
-	&read_sty();
-    } else {
-	if ($nsp_snp && $sty_snp) {
-	    print "next snps $nsp_snp $sty_snp\n";
-	    die "sty/nsp not in sequence\n"; 
-	} 
-    }
+	if ( ($nsp_snp) && ( $snp eq $nsp_snp ) ) {
+		print $outfile $nsp_the_rest;
+		&read_nsp();
+	}
+	elsif ( ($sty_snp) && ( $snp eq $sty_snp ) ) {
+		print $outfile $sty_the_rest;
+		&read_sty();
+	}
+	else {
+		if ( $nsp_snp && $sty_snp ) {
+			print "next snps $nsp_snp $sty_snp\n";
+			die "sty/nsp not in sequence\n";
+		}
+	}
 }
 
-# close the last filehandle
+#close the last filehandle
 if ($outfile) {
-    $outfile->close();
+	$outfile->close();
 }
-
 
 sub read_nsp {
-    $nsp_the_rest = "";
-    $nsp_snp = undef;
-    foreach my $nspfile (@nspfiles) {
-	my $line = $nspfile->getline();
-	if ($line) {
-	    my ($snp_here, $the_rest) = split /\s+/, $line, 2;
-	    chomp $the_rest;
-	    if (($nsp_snp) &&  ($snp_here ne $nsp_snp)) {
-		die "not all nsp files are the same\n";
-	    } else {
-		$nsp_the_rest .= ("\t" . $the_rest);
-		unless ($nsp_snp) {
-		    $nsp_snp = $snp_here;
+	$nsp_the_rest = "";
+	$nsp_snp      = undef;
+	foreach my $nspfile (@nspfiles) {
+		my $line = $nspfile->getline();
+		if ($line) {
+			my ( $snp_here, $the_rest ) = split /\s+/, $line, 2;
+			chomp $the_rest;
+			if ( ($nsp_snp) && ( $snp_here ne $nsp_snp ) ) {
+				die "not all nsp files are the same\n";
+			}
+			else {
+				$nsp_the_rest .= ( "\t" . $the_rest );
+				unless ($nsp_snp) {
+					$nsp_snp = $snp_here;
+				}
+			}
 		}
-	    }
 	}
-    }
-    $nsp_the_rest .= "\n";
+	$nsp_the_rest .= "\n";
 }
 
 sub read_sty {
-    $sty_the_rest = "";
-    $sty_snp = undef;
-    foreach my $styfile (@styfiles) {
-	my $line = $styfile->getline();
-	if ($line) {
-	    my ($snp_here, $the_rest) = split /\s+/, $line, 2;
-	    chomp $the_rest;
-	    if (($sty_snp) &&  ($snp_here ne $sty_snp)) {
-		die "not all sty files are the same\n";
-	    } else {
-		$sty_the_rest .= ("\t" . $the_rest);
-		unless ($sty_snp) {
-		    $sty_snp = $snp_here;
+	$sty_the_rest = "";
+	$sty_snp      = undef;
+	foreach my $styfile (@styfiles) {
+		my $line = $styfile->getline();
+		if ($line) {
+			my ( $snp_here, $the_rest ) = split /\s+/, $line, 2;
+			chomp $the_rest;
+			if ( ($sty_snp) && ( $snp_here ne $sty_snp ) ) {
+				die "not all sty files are the same\n";
+			}
+			else {
+				$sty_the_rest .= ( "\t" . $the_rest );
+				unless ($sty_snp) {
+					$sty_snp = $snp_here;
+				}
+			}
 		}
-	    }
 	}
-    }
-    $sty_the_rest .= "\n";
+	$sty_the_rest .= "\n";
 }
+
+
