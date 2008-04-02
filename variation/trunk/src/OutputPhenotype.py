0444015f29f15c0c098f6d85bdc2bd05c37be874
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-04-02 21:42:48 +0000

    program to output phenotype_avg table in 250k db.
    git-svn-id: file:///usr/local/svn/repos@399 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

---
+++ variation/trunk/src/OutputPhenotype.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+"""
+Usage: OutputPhenotype.py [OPTIONS] -o OUTPUT_FILE
+
+Option:
+	-z ..., --hostname=...	the hostname, localhost(default)
+	-d ..., --dbname=...	the database name, stock(default)
+	-k ..., --schema=...	which schema in the database
+	-o ...,	output file
+	-e ...,	ecotype table 'stock.ecotype'(default)
+	-p ...,	phenotype_avg_table, 'stock_250k.phenotype_avg'(default)
+	-m ...,	phenotype_method_table, 'stock_250k.phenotype_method'(default)
+	-b, --debug	enable debug
+	-r, --report	enable more progress-related output
+	-h, --help	show this help
+
+Examples:
+	OutputPhenotype.py -o /tmp/phenotype.tsv
+	
+Description:
+	program to output phenotype_avg table.
+"""
+import sys, os, math
+bit_number = math.log(sys.maxint)/math.log(2)
+if bit_number>40:       #64bit
+	sys.path.insert(0, os.path.expanduser('~/lib64/python'))
+	sys.path.insert(0, os.path.join(os.path.expanduser('~/script64')))
+else:   #32bit
+	sys.path.insert(0, os.path.expanduser('~/lib/python'))
+	sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
+
+import time, csv, numpy, getopt
+import traceback
+from pymodule import process_function_arguments, write_data_matrix
+
+class OutputPhenotype:
+	"""
+	2008-04-02
+	"""
+	def __init__(self,  **keywords):
+		"""
+		2008-4-2
+		"""
+		argument_default_dict = {('hostname',1, ):'localhost',\
+								('dbname',1, ):'stock',\
+								('schema',0, ):'',\
+								('output_fname',1, ):None,\
+								('ecotype_table',1, ):'stock.ecotype',\
+								('phenotype_avg_table',1, ):'stock_250k.phenotype_avg',\
+								('phenotype_method_table',1, ):'stock_250k.phenotype_method',\
+								('debug',0, int):0,\
+								('report',0, int):0}
+		"""
+		2008-02-28
+			argument_default_dict is a dictionary of default arguments, the key is a tuple, ('argument_name', is_argument_required, argument_type)
+			argument_type is optional
+		"""
+		#argument dictionary
+		self.ad = process_function_arguments(keywords, argument_default_dict, error_doc=__doc__, class_to_have_attr=self)
+	
+	def get_phenotype_method_id_info(self, curs, phenotype_avg_table, phenotype_method_table ):
+		"""
+		2008-4-2
+		"""
+		sys.stderr.write("Getting phenotype_method_id info ... " )
+		phenotype_method_id2index = {}	#index of the matrix
+		method_id_name_ls = []	#as header for each phenotype
+		curs.execute("select distinct method_id from %s order by method_id"%phenotype_avg_table)
+		rows = curs.fetchall()
+		for row in rows:
+			method_id = row[0]
+			curs.execute("select short_name from %s where id=%s"%(phenotype_method_table, method_id))
+			method_short_name = curs.fetchall()[0][0]
+			method_id_name_ls.append('%s_%s'%(method_id, method_short_name))
+			phenotype_method_id2index[method_id] = len(phenotype_method_id2index)
+		sys.stderr.write("Done\n")
+		return phenotype_method_id2index, method_id_name_ls
+	
+	def get_ecotype_id2info(self, curs, phenotype_avg_table, ecotype_table):
+		"""
+		2008-4-2
+		"""
+		sys.stderr.write("Getting ecotype id info ... " )
+		ecotype_id2index = {}	#index of the matrix
+		ecotype_id_ls = []
+		ecotype_name_ls = []
+		curs.execute("select distinct ecotype_id from %s order by ecotype_id"%phenotype_avg_table)
+		rows = curs.fetchall()
+		for row in rows:
+			ecotype_id = row[0]
+			curs.execute("select nativename from %s where id=%s"%(ecotype_table, ecotype_id))
+			nativename = curs.fetchall()[0][0]
+			ecotype_name_ls.append(nativename)
+			ecotype_id_ls.append(ecotype_id)
+			ecotype_id2index[ecotype_id] = len(ecotype_id2index)
+		sys.stderr.write("Done\n")
+		return ecotype_id2index, ecotype_id_ls, ecotype_name_ls
+	
+	def get_matrix(self, curs, phenotype_avg_table, ecotype_id2index, phenotype_method_id2index):
+		"""
+		2008-4-2
+		"""
+		sys.stderr.write("Getting matrix ... " )
+		data_matrix = numpy.zeros([len(ecotype_id2index), len(phenotype_method_id2index)], numpy.float)
+		data_matrix[:] = numpy.nan
+		curs.execute("select ecotype_id, method_id, value from %s"%phenotype_avg_table)
+		rows = curs.fetchall()
+		for row in rows:
+			ecotype_id, phenotype_method_id, value = row
+			data_matrix[ecotype_id2index[ecotype_id]][phenotype_method_id2index[phenotype_method_id]] = value
+		sys.stderr.write("Done\n")
+		return data_matrix
+	
+	def run(self):
+		import MySQLdb
+		conn = MySQLdb.connect(db=self.dbname,host=self.hostname)
+		curs = conn.cursor()
+		
+		
+		phenotype_method_id2index, method_id_name_ls = self.get_phenotype_method_id_info(curs, self.phenotype_avg_table, self.phenotype_method_table)
+		ecotype_id2index, ecotype_id_ls, ecotype_name_ls = self.get_ecotype_id2info(curs, self.phenotype_avg_table, self.ecotype_table)
+		data_matrix = self.get_matrix(curs, self.phenotype_avg_table, ecotype_id2index, phenotype_method_id2index)
+		
+		header = ['ecotype id', 'nativename'] + method_id_name_ls
+		write_data_matrix(data_matrix, self.output_fname, header, ecotype_id_ls, ecotype_name_ls)
+
+if __name__ == '__main__':
+	if len(sys.argv) == 1:
+		print __doc__
+		sys.exit(2)
+	
+	long_options_list = ["help", "type=", "debug", "report"]
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:o:e:p:m:br", long_options_list)
+	except:
+		traceback.print_exc()
+		print sys.exc_info()
+		print __doc__
+		sys.exit(2)
+	
+	
+	hostname = 'localhost'
+	dbname = 'stock'
+	schema = None
+	output_fname = None
+	ecotype_table = None
+	phenotype_avg_table = None
+	phenotype_method_table = None
+	debug = None
+	report = None
+	
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			help = 1
+			print __doc__
+		elif opt in ("-z", "--hostname"):
+			hostname = arg
+		elif opt in ("-d", "--dbname"):
+			dbname = arg
+		elif opt in ("-k", "--schema"):
+			schema = arg
+		elif opt in ("-o",):
+			output_fname = arg
+		elif opt in ("-e",):
+			ecotype_table = arg
+		elif opt in ("-p",):
+			phenotype_avg_table = arg
+		elif opt in ("-m",):
+			phenotype_method_table = arg
+		
+		elif opt in ("-b", "--debug"):
+			debug = 1
+		elif opt in ("-r", "--report"):
+			report = 1
+	
+	instance = OutputPhenotype(hostname=hostname, dbname=dbname, schema=schema, output_fname=output_fname,
+					ecotype_table=ecotype_table, phenotype_avg_table=phenotype_avg_table, \
+					phenotype_method_table = phenotype_method_table, debug=debug, report=report)
+	instance.run()
