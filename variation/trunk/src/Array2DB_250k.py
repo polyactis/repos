440ef6063d2f31fd6bc01d5c9cf293e678a64788
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-04-11 20:03:25 +0000

    2008-04-11 new way of handling raw cel files:
    	md5sum each array, check if each array is in db already.
    	if yes:
    		skip it
    	else:
    		assign a new id and insert an entry into array_info_table
    		copy the original cel file to output_dir and put array_id in the beginning of the filename.
    
    git-svn-id: file:///usr/local/svn/repos@433 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/Array2DB_250k.py
+++ variation/trunk/src/Array2DB_250k.py
@@ -1,5 +1,24 @@
+#!/usr/bin/env python
 """
-2008-02-26
+Usage:	Array2DB_250k.py [OPTIONS] -i INPUT_DIR
+
+Argument list:
+	-z ..., --hostname=...	the hostname, papaya.usc.edu(default)
+	-d ..., --dbname=...	the database name, stock_250k(default)
+	-u ..., --user=...	the db username, (otherwise it will ask for it).
+	-p ..., --passwd=...	the db password, (otherwise it will ask for it).
+	-i ...,	input_dir*	where all the .cel files sit
+	-o ...,	output_dir	to store the renamed .cel files. '/Network/Data/250k/db/raw_data/'(default)
+	-g ...,	array_info_table, 'array_info'(default)
+	-c,	commit db transaction
+	-b,	toggle debug
+	-r, toggle report
+Examples:
+	Array2DB_250k.py -i /Network/Data/250k/raw_data/ -d stock_250k -c
+
+Description:
+	Dump .cel array files from input_dir (files in up to 2-level directories) into db and associated file-system storage (output-dir).
+
 """
 import sys, os, math
 bit_number = math.log(sys.maxint)/math.log(2)
@@ -10,41 +29,104 @@ else:   #32bit
 	sys.path.insert(0, os.path.expanduser('~/lib/python'))
 	sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
-import csv, numpy, stat, rpy
-import traceback, gc
-from pymodule import process_function_arguments
+import csv, stat, getopt
+import traceback, gc, subprocess
 
-class Array2DB_250k:
-	"""
-	2008-02-26
-	Argument list:
-		-z ..., --hostname=...	the hostname, localhost(default)
-		-d ..., --dbname=...	the database name, stock20071008(default)
-		-k ..., --schema=...	which schema in the database, (IGNORE)
-		-i ...,	input_dir*
-		-a ...,	array_data_table, array_data(default)
-		-e ...,	probes_table, 'probes'(default)
-		-f ...,	strain_info_table, 'strain_info'(default)
-		-g ...,	array_info_table, 'array_info'(default)
-		-c,	commit db transaction
-		-b,	toggle debug
-		-r, toggle report
-	Examples:
-		main.py -y 3 -i /Network/Data/250k/raw_data/ -d stock_250k -c
-	Description:
-		Dump .cel array files in input_dir (files in up to 2-level directories) into db.
-	"""
+class ArrayInfo(object):
 	def __init__(self, **keywords):
+		from pymodule import process_function_arguments
+		argument_default_dict = {('curs',1, ): None,\
+								('array_info_table',1, ):None,\
+								('user',1, ):None,\
+								('debug',0, int):0,\
+								('report',0, int):0}
 		"""
 		2008-02-28
+			argument_default_dict is a dictionary of default arguments, the key is a tuple, ('argument_name', is_argument_required, argument_type)
+			argument_type is optional
 		"""
-		argument_default_dict = {('hostname',1, ):'localhost',\
+		#argument dictionary
+		self.ad = process_function_arguments(keywords, argument_default_dict, error_doc=self.__doc__, class_to_have_attr=self, howto_deal_with_required_none=2)
+		self.md5sum2array_id, self.max_array_id = self.get_md5sum2array_id(self.curs, self.array_info_table)
+		
+	def get_md5sum2array_id(self, curs, array_info_table):
+		"""
+		2008-04-10
+		"""
+		sys.stderr.write("Getting filename2array_id_in_db ... ")
+		md5sum2array_id = {}
+		curs.execute("select id, md5sum, filename from %s"%(array_info_table))
+		rows = curs.fetchall()
+		max_array_id = 0
+		for row in rows:
+			array_id, md5sum, filename = row
+			if md5sum in md5sum2array_id:
+				sys.stderr.write("Error: two array_id (%s and %s) have same md5sum %s in db. Check db.\n"%(array_id, md5sum2array_id[md5sum], md5sum))
+				sys.exit(3)
+			md5sum2array_id[md5sum] = array_id
+			if array_id>max_array_id:
+				max_array_id = array_id
+		sys.stderr.write("Done.\n")
+		return md5sum2array_id, max_array_id
+	
+	get_md5sum2array_id = classmethod(get_md5sum2array_id)
+	
+	def get_md5sum(self, filename):
+		"""
+		"""
+		md5sum_command = 'md5sum'
+		md5sum_p = subprocess.Popen([md5sum_command, filename], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+		md5sum_stdout_out = md5sum_p.stdout.read()
+		md5sum_stderr_out = md5sum_p.stderr.read()
+		if md5sum_stderr_out:
+			sys.stderr.write("%s %s failed with stderr: %s.\n"%(md5sum_command, filename, md5sum_stderr_out))
+			sys.exit(4)
+		else:
+			return md5sum_stdout_out.split()[0]
+	
+	get_md5sum = classmethod(get_md5sum)
+	
+	def assignNewIdToThisArray(self, array_filename, output_dir):
+		md5sum = self.get_md5sum(array_filename)
+		if md5sum in self.md5sum2array_id:
+			sys.stderr.write("%s already exists in db with md5sum=%s and array_id=%s. ignored.\n"%\
+							(array_filename, md5sum, self.md5sum2array_id[md5sum]))
+			return -1
+		else:
+			new_array_id = self.max_array_id + 1
+			output_fname = os.path.join(output_dir, '%s_raw_data.cel'%new_array_id)
+			
+			cp_p = subprocess.Popen(['cp', array_filename, output_fname], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+			cp_p_stdout_out = cp_p.stdout.read()
+			cp_p_stderr_out = cp_p.stderr.read()
+			if cp_p_stderr_out:
+				sys.stderr.write("copy error: %s\n"%cp_p_stderr_out)
+				return -1
+			else:
+				self.curs.execute("insert into %s(id, filename, original_filename, md5sum, created_by) values (%s, '%s', '%s', '%s', '%s')"%\
+							(self.array_info_table, new_array_id, output_fname, array_filename, md5sum, self.user))
+				self.md5sum2array_id[md5sum] = new_array_id
+				self.max_array_id += 1
+				return new_array_id
+
+class Array2DB_250k(object):
+	__doc__ = __doc__	#use documentation in the beginning of the file as this class's doc
+	def __init__(self, **keywords):
+		"""
+		2008-04-09
+			array_data_table, probes_table, strain_info_table are no longer required. but leave them in the argument_default_dict.
+		2008-02-28
+		"""
+		from pymodule import process_function_arguments
+		argument_default_dict = {('hostname',1, ):'papaya.usc.edu',\
 								('dbname',1, ):'stock_250k',\
-								('schema',0, ):'',\
+								('user',1, ):None,\
+								('passwd',1, ):None,\
 								('input_dir',1, ):None,\
-								('array_data_table',1, ):'array_data',\
-								('probes_table',1, ):'probes',\
-								('strain_info_table',1, ):'strain_info',\
+								('output_dir',1, ):'/Network/Data/250k/db/raw_data/',\
+								('array_data_table',0, ):'array_data',\
+								('probes_table',0, ):'probes',\
+								('strain_info_table',0, ):'strain_info',\
 								('array_info_table',1, ):'array_info',\
 								('commit',0, int):0,\
 								('debug',0, int):0,\
@@ -56,8 +138,6 @@ class Array2DB_250k:
 		"""
 		#argument dictionary
 		self.ad = process_function_arguments(keywords, argument_default_dict, error_doc=self.__doc__, class_to_have_attr=self)
-		self.debug = self.ad['debug']
-		self.report = self.ad['report']
 		
 	def get_xypos2probes_id(self, curs, probes_table):
 		"""
@@ -87,6 +167,34 @@ class Array2DB_250k:
 		sys.stderr.write("Done.\n")
 		return filename2array_id_in_db
 	
+	def get_all_files_in_input_dir(self, input_dir):
+		"""
+		2008-04-10
+		"""
+		sys.stderr.write("Getting all files from %s ... \n"%input_dir)
+		file_dir_ls = os.listdir(input_dir)
+		sys.stderr.write("\n\tTotally, %d 1st-order file/dir's to be processed.\n"%len(file_dir_ls))
+		filename_ls = []
+		no_of_objects = len(file_dir_ls)
+		for i in range(no_of_objects):
+			file_dir = file_dir_ls[i]
+			sys.stderr.write("%d/%d:\t%s\n"%(i+1, no_of_objects, file_dir))
+			pathname = os.path.join(input_dir, file_dir)
+			if os.path.isdir(pathname):
+				sub_file_dir_ls = os.listdir(pathname)
+				for sub_file_dir in sub_file_dir_ls:
+					sub_pathname = os.path.join(pathname, sub_file_dir)
+					if os.path.isfile(sub_pathname):
+						filename_ls.append(sub_pathname)
+					else:
+						sys.stderr.write("%s is not file. (might be 2nd directory). Ignored.\n"%(sub_pathname))
+			elif os.path.isfile(pathname):
+				filename_ls.append(pathname)
+			else:
+				sys.stderr.write("%s is neither directory nor file. Ignored.\n"%(pathname))
+		sys.stderr.write("Done.\n")
+		return filename_ls
+			
 	def get_filename2array_id(self, input_dir, filename2array_id_in_db):
 		"""
 		2008-02-29
@@ -173,6 +281,7 @@ class Array2DB_250k:
 		2008-02-28
 		"""
 		sys.stderr.write("Submitting array data ... \n")
+		import rpy
 		rpy.r.library('affy')
 		files = filename2array_id.keys()
 		sys.stderr.write("\tTotally, %d files to be processed.\n"%len(files))
@@ -210,16 +319,95 @@ class Array2DB_250k:
 	
 	def run(self):
 		"""
+		2008-04-11
+			new way of handling raw cel files:
+			
+			md5sum each array, check if each array is in db already.
+			if yes:
+				skip it
+			else:
+				assign a new id and insert an entry into array_info_table
+				copy the original cel file to output_dir and put array_id in the beginning of the filename.
 		2008-02-28
 		"""
 		import MySQLdb
-		conn = MySQLdb.connect(db=self.ad['dbname'], host=self.ad['hostname'])
+		conn = MySQLdb.connect(db=self.dbname, host=self.hostname, user = self.user, passwd = self.passwd)
 		curs = conn.cursor()
-		filename2array_id_in_db = self.get_filename2array_id_in_db(curs, self.ad['array_info_table'])
-		filename2array_id = self.get_filename2array_id(self.ad['input_dir'], filename2array_id_in_db)
-		if self.ad['commit']:
-			self.submit_filename2array_id(curs, filename2array_id, self.ad['array_info_table'])
-		xypos2probes_id = self.get_xypos2probes_id(curs, self.ad['probes_table'])
-		if self.ad['commit']:
-			self.submit_all_array_data(filename2array_id, xypos2probes_id, curs, self.ad['array_data_table'])
+		"""
+		filename2array_id_in_db = self.get_filename2array_id_in_db(curs, self.array_info_table)
+		filename2array_id = self.get_filename2array_id(self.input_dir, filename2array_id_in_db)
+		if self.commit:
+			self.submit_filename2array_id(curs, filename2array_id, self.array_info_table)
+		xypos2probes_id = self.get_xypos2probes_id(curs, self.probes_table)
+		self.submit_all_array_data(filename2array_id, xypos2probes_id, curs, self.array_data_table)
+		"""
+		arrayInfo = ArrayInfo(curs=curs, array_info_table=self.array_info_table, user=self.user)
+		input_fname_ls = self.get_all_files_in_input_dir(self.input_dir)
+		for filename in input_fname_ls:
+			sys.stderr.write("Assigning new id to %s ... "%filename)
+			return_value = arrayInfo.assignNewIdToThisArray(filename, self.output_dir)
+			if return_value==-1:
+				sys.stderr.write("Failed.\n")
+			else:
+				sys.stderr.write("\n")
+		if self.commit:
 			curs.execute("commit")
+
+
+if __name__ == '__main__':
+	if len(sys.argv) == 1:
+		print __doc__
+		sys.exit(2)
+	
+	long_options_list = ["hostname=", "dbname=", "user=", "passwd=", "help", "type=", "debug", "report"]
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:u:p:i:o:g:cbr", long_options_list)
+	except:
+		traceback.print_exc()
+		print sys.exc_info()
+		print __doc__
+		sys.exit(2)
+	
+	
+	hostname = None
+	dbname = None
+	user = None
+	passwd = None
+	input_dir = None
+	output_dir = None
+	array_info_table = None
+	commit = None
+	debug = None
+	report = None
+	
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			help = 1
+			print __doc__
+		elif opt in ("-z", "--hostname"):
+			hostname = arg
+		elif opt in ("-d", "--dbname"):
+			dbname = arg
+		elif opt in ("-u", "--user"):
+			user = arg
+		elif opt in ("-p", "--passwd"):
+			passwd = arg
+		elif opt in ("-i",):
+			input_dir = arg
+		elif opt in ("-o",):
+			output_dir = arg
+		elif opt in ("-o",):
+			output_fname = arg
+		elif opt in ("-g",):
+			array_info_table = arg
+		elif opt in ("-c",):
+			commit = 1
+		elif opt in ("-b", "--debug"):
+			debug = 1
+		elif opt in ("-r", "--report"):
+			report = 1
+	
+	instance = Array2DB_250k(hostname=hostname, dbname=dbname, user=user, passwd=passwd, input_dir=input_dir, output_dir=output_dir,
+					array_info_table=array_info_table, \
+					commit=commit, debug=debug, report=report)
+	instance.run()
