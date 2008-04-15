757ccdbeba200cf5758d4dc8e3aba0b7af880186
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-04-15 07:19:31 +0000

    add the file widget in the form.
    two redundant operations:
    1. upload the file into uploads/
    2. base64_encode it and put it into db (column type=text)
    
    1 is also carried out to avoid writing scripts to base64_decode the data out of the db.
    
    git-svn-id: file:///usr/local/svn/repos@452 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- mcb_retreat/trunk/php/symp_reg.php
+++ mcb_retreat/trunk/php/symp_reg.php
@@ -4,28 +4,91 @@ if ($_POST['_submit_check'])
 {
 	$link = pg_connect("host=localhost dbname=yhdb user=yh password=123456")
 		or die('Could not connect: ' . pg_last_error());
-	$name=$_POST['name'];
+	$name=trim($_POST['name']);
 	$email=$_POST['email'];
 	$pi=$_POST['pi'];
 	$Pref=$_POST['Pref'];
 	$title=$_POST['title'];
 	$author=$_POST['author_list'];
 	$abs=$_POST['abs'];
-	if ($pi=='' || $email=='' || $name=='' || $title=='' || $author=='' || $abs=='' )
+	//2008-04-14 file is in the _FILES array
+	$abs_file=$_FILES['abs_file']["tmp_name"];
+	
+	if ($email=='' || $name=='')
 	{
 		echo  "<script  type='text/javascript'> 
-           alert('$name : Please fill in ALL fields.');
+           alert('Eror: Name and E-mail are required.');
+           </script>";
+	}
+	elseif ($Pref!='None' && ( $title=='' || $pi=='' || $author=='' || ($abs=='' && $abs_file=='')))
+	{
+		echo  "<script  type='text/javascript'> 
+           alert('Eror: since your preference is not None, please fill in other fields.');
            </script>";
 	}
 	else
 	{
-	$query=" insert INTO retreat.abstract (name,email,pi,pref,title,author_list,abstract) VALUES('$name','$email','$pi','$Pref','$title','$author','$abs')";
-	$result=pg_query($link,$query) or die('Query failed: ' . pg_last_error());
-	
-	pg_close($link);
-	echo  "<script  type='text/javascript'>
-           alert('$name : Your abstract has been submited. Thank you!');
-          </script>";
+		if ($abs == '')
+		{	
+			/*
+			echo "<pre>";
+			echo "filename: " . $_FILES['abs_file']['name'] . '\n';
+			echo $abs_file . "\n";
+			echo $_FILES['abs_file']['name'] . '\n';
+			echo $_FILES['abs_file']['size'] . '\n';
+			echo $_FILES['abs_file']["type"] . '\n';
+			*/
+			
+			$abs_file_content_type = $_FILES['abs_file']["type"];
+			/*
+			switch ($abs_file_content_type) {
+			case "application/pdf":
+				$abs_file_extension='pdf';
+			    break;
+			case 'application/msword':
+				$abs_file_extension='doc';
+				break;
+			case 'text/plain':
+				$abs_file_extension='txt';
+				break;
+			case 'text/rtf':
+				$abs_file_extension='rtf';
+				break;
+			default:
+				echo  "<script  type='text/javascript'> 
+				alert('Error: Dear $name, your file has the unsupported extension: $abs_file_extension.');
+				</script>";
+				die('');
+			}
+			*/
+			
+			//2008-04-14 easier way to get file extension. but not used.
+			$path_parts = pathinfo($_FILES['abs_file']['name']);
+			$abs_file_extension = strtolower($path_parts['extension']);
+			//echo "File Extension: " . $abs_file_extension . '\n';
+			
+			$abs_binary = file_get_contents($abs_file) or die('Reading file ' . $abs_file . ' failed.');
+			$abs_file_data = base64_encode($abs_binary);	//base64_decode() is for the reverse action to get the file data out.
+			$abs_filename = $name . "." . $abs_file_extension;
+			$abs_file_size = $_FILES['abs_file']["size"];
+			$query="insert INTO retreat.abstract (name,email,pi,pref,title,author_list, abstract_file_data, abstract_file_content_type, abstract_filename, abstract_file_size) 
+				VALUES('$name','$email','$pi','$Pref','$title','$author','$abs_file_data', '$abs_file_content_type', '$abs_filename', '$abs_file_size')";
+			$result=pg_query($link,$query) or die('DB insertion failed: ' . pg_last_error());
+			
+			//2007-04-14 upload the file
+			$target_path = "uploads/";
+			$target_path = $target_path . $abs_filename;
+			move_uploaded_file($abs_file, $target_path);
+		}
+		else
+		{
+			$query=" insert INTO retreat.abstract (name,email,pi,pref,title,author_list,abstract) VALUES('$name','$email','$pi','$Pref','$title','$author','$abs')";
+			$result=pg_query($link,$query) or die('DB insertion failed: ' . pg_last_error());
+		}
+		pg_close($link);
+		echo  "<script  type='text/javascript'>
+			alert('$name : Your abstract has been submited. Thank you!');
+			</script>";
 	}
 }
 ?>
@@ -56,7 +119,7 @@ if ($_POST['_submit_check'])
 </head>
 
 <body>
-<form method="post" name="abstract" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<form enctype="multipart/form-data" method="post" name="abstract" action="<?php echo $_SERVER['PHP_SELF'];?>">
 <table width="800" height="747" border="0" align="center" cellpadding="0" cellspacing="0">
   <tr>
     <td height="747" align="center" valign="top"><table width="800" height="572" border="0" align="center" cellpadding="0" cellspacing="0">
@@ -73,13 +136,13 @@ if ($_POST['_submit_check'])
       
       <tr>
         <td height="34">&nbsp;</td>
-        <td colspan="2" align="left"><strong>Name:</strong></td>
+        <td colspan="2" align="left"><strong>Name<span class="style3">*</span>:</strong></td>
         <td align="left"> <input type="text" name="name" /></td>
         <td>&nbsp;</td>
       </tr>
       <tr>
         <td height="32">&nbsp;</td>
-        <td colspan="2" align="left"><strong>E-mail:</strong></td>
+        <td colspan="2" align="left"><strong>E-mail<span class="style3">*</span>:</strong></td>
         <td align="left"><input type="text" name="email" /></td>
         <td>&nbsp;</td>
       </tr>
@@ -121,6 +184,12 @@ if ($_POST['_submit_check'])
         <td height="120">&nbsp;</td>
         <td colspan="2" align="left" valign="middle"><strong>Abstract: </strong></td>
         <td align="left"> <textarea name="abs" cols="52" rows="7"></textarea></td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td height="33">&nbsp;</td>
+        <td colspan="2" align="left"><strong><span class="style3">OR</span> Abstract File (doc, pdf, txt or rtf): </strong></td>
+        <td align="left"> <input TYPE="FILE" NAME="abs_file" SIZE="55"></td>
         <td>&nbsp;</td>
       </tr>
       <tr>
