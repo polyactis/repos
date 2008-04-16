2ddf93a804a5cc8b166f6ba643b96ce977229685
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-04-16 07:24:47 +0000

    Variation.250k has a problem because 250k can't be a python module name so paster create -t plone3_theme Variation.SNP250k instead.
    git-svn-id: file:///usr/local/svn/repos@461 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

---
+++ variation/trunk/web_interface/Variation.SNP250k/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='Variation.SNP250k',
+      version=version,
+      description="Variation 250k web portal",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='SNP polymorphism 250k DB',
+      author='Yu Huang',
+      author_email='yuhuang@usc.edu',
+      url='http://papaya.usc.edu/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Variation'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
