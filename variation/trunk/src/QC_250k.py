2e1b4773cbf90567fb6f6468bbcd54fb2e87abbb
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-04-21 07:22:06 +0000

    add 	def cal_independent_NA_rate(cls, curs, call_info_table): 		""" 		2008-04-20 			calculate indepent (no data to be compared) NA rates. 			update it in the db. 		"""
    git-svn-id: file:///usr/local/svn/repos@490 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/QC_250k.py
+++ variation/trunk/src/QC_250k.py
@@ -16,6 +16,9 @@ Argument list:
 	-r, toggle report
 
 Examples:
+	#test run (without -c) calculate missing rate for call_info entries. no need for cmp_data_filename. 'nothing' is a place holder.
+	QC_250k.py -i nothing -m 0
+	
 	#test run (without -c) QC between 250k and 2010
 	QC_250k.py -i /home/crocea/script/variation/data/2010/data_2010_x_250k_y0001.tsv -m 1
 	
@@ -28,6 +31,10 @@ Examples:
 Description:
 	QC for 250k call data from call_info_table against 2010, perlegen, 149SNP data.
 	It fills the call_QC_table with NA_rate, mismatch_rate and etc.
+	
+	QC_method_id:
+		Except 0, it corresponds to field/column id in table QC_method.
+		0 is not a real QC method id. It calculates NA rate for call_info entries whose NA_rates haven't been calculated.
 	
 """
 import sys, os, math
@@ -151,15 +158,22 @@ class QC_250k(object):
 		self.ad = process_function_arguments(keywords, argument_default_dict, error_doc=self.__doc__, class_to_have_attr=self)
 	
 	def get_call_info_id2fname(cls, curs, call_info_table, call_QC_table, QC_method_id, array_info_table='array_info'):
+		"""
+		2008-04-20
+			call_info entries that QC haven't been done.
+			and its corresponding maternal_ecotype_id=paternal_ecotype_id (no crosses).
+			
+		"""
 		sys.stderr.write("Getting call_info_id2fname ... ")
-		curs.execute("select distinct c.id, c.filename, a.maternal_ecotype_id, a.paternal_ecotype_id, q.id as qc_id, q.QC_method_id from %s a, %s c left join %s q on c.id=q.call_info_id where c.array_id=a.id and a.maternal_ecotype_id=a.paternal_ecotype_id"%\
-					(array_info_table, call_info_table, call_QC_table))
+		curs.execute("select distinct c.id, c.filename, a.maternal_ecotype_id, a.paternal_ecotype_id, q.id as qc_id, q.QC_method_id \
+				from %s a, %s c left join %s q on c.id=q.call_info_id where c.array_id=a.id and a.maternal_ecotype_id=a.paternal_ecotype_id"%\
+				(array_info_table, call_info_table, call_QC_table))
 		rows = curs.fetchall()
 		call_info_id2fname = {}
 		call_info_id_del_ls = []
 		for row in rows:
 			call_info_id, fname, maternal_ecotype_id, paternal_ecotype_id, qc_id, db_QC_method_id = row
-			if db_QC_method_id==QC_method_id:
+			if db_QC_method_id==QC_method_id:	#done already, mark it for deletion
 				call_info_id_del_ls.append(call_info_id)
 			call_info_id2fname[call_info_id] = [maternal_ecotype_id, fname]	#take all
 		
@@ -208,10 +222,43 @@ class QC_250k(object):
 		row_id_ls.sort()	#try to keep them in call_info_id order
 		for row_id in row_id_ls:
 			NA_mismatch_ls = row_id2NA_mismatch_rate[row_id]
-			data_insert_ls = [row_id[0]] + NA_mismatch_ls + [QC_method_id, user]
-			NA_rate, mismatch_rate, no_of_NAs, no_of_totals, no_of_mismatches, no_of_non_NA_pairs = NA_mismatch_ls	#row_id is (call_info_id, ecotypeid)
+			data_insert_ls = [row_id[0]] + NA_mismatch_ls + [QC_method_id, user]	#row_id is (call_info_id, ecotypeid)
 			curs.execute("insert into " + call_QC_table + " (call_info_id, NA_rate, mismatch_rate, no_of_NAs, no_of_totals, no_of_mismatches, no_of_non_NA_pairs, QC_method_id, created_by)\
 				values(%s, %s, %s, %s, %s, %s, %s, %s, %s)", data_insert_ls)
+		sys.stderr.write("Done.\n")
+	
+	submit_to_call_QC = classmethod(submit_to_call_QC)
+	
+	def cal_independent_NA_rate(cls, curs, call_info_table):
+		"""
+		2008-04-20
+			calculate indepent (no data to be compared) NA rates.
+			update it in the db.
+		"""
+		sys.stderr.write("Calculating indepent NA rate ... \n")
+		curs.execute("select c.id, c.filename from %s c where c.NA_rate is null order by id"%\
+					(call_info_table))
+		rows = curs.fetchall()
+		no_of_rows = len(rows)
+		sys.stderr.write("\tTotally, %d call_info entries to be processed.\n"%no_of_rows)
+		for i in range(no_of_rows):
+			sys.stderr.write("%d/%d:\t%s\n"%(i+1, no_of_rows, rows[i][1]))
+			call_info_id, fname = rows[i]
+			reader = csv.reader(open(fname), delimiter='\t')
+			reader.next()	#throw away the first line
+			no_of_totals = 0
+			no_of_NAs = 0
+			for row in reader:
+				SNP_id, call = row[:2]
+				no_of_totals += 1
+				if call=='NA':
+					no_of_NAs += 1
+			if no_of_totals!=0:
+				NA_rate = float(no_of_NAs)/no_of_totals
+			else:
+				NA_rate = -1
+			curs.execute("update " + call_info_table + " set NA_rate=%s where id=%s",\
+					(NA_rate, call_info_id))
 		sys.stderr.write("Done.\n")
 	
 	def run(self):
@@ -221,19 +268,22 @@ class QC_250k(object):
 		if self.debug:
 			import pdb
 			pdb.set_trace()
-		from variation.src.FilterStrainSNPMatrix import FilterStrainSNPMatrix
-		header, strain_acc_list, category_list, data_matrix = FilterStrainSNPMatrix.read_data(self.cmp_data_filename)
-		snpData2 = SNPData(header=header, strain_acc_list=strain_acc_list, category_list=category_list, data_matrix=data_matrix)
-		
-		call_info_id2fname = self.get_call_info_id2fname(curs, self.call_info_table, self.call_QC_table, self.QC_method_id)
-		
-		header, strain_acc_list, category_list, data_matrix = self.read_call_matrix(call_info_id2fname)
-		snpData1 = SNPData(header=header, strain_acc_list=strain_acc_list, category_list=category_list, data_matrix=data_matrix)
-		twoSNPData = TwoSNPData(SNPData1=snpData1, SNPData2=snpData2, curs=curs, QC_method_id=self.QC_method_id)
-		
-		row_id2NA_mismatch_rate = twoSNPData.cmp_row_wise()
-		
-		self.submit_to_call_QC(curs, row_id2NA_mismatch_rate, self.call_QC_table, self.QC_method_id, self.user)
+		if self.QC_method_id==0:
+			self.cal_independent_NA_rate(curs, self.call_info_table)
+		else:
+			from variation.src.FilterStrainSNPMatrix import FilterStrainSNPMatrix
+			header, strain_acc_list, category_list, data_matrix = FilterStrainSNPMatrix.read_data(self.cmp_data_filename)
+			snpData2 = SNPData(header=header, strain_acc_list=strain_acc_list, category_list=category_list, data_matrix=data_matrix)
+			
+			call_info_id2fname = self.get_call_info_id2fname(curs, self.call_info_table, self.call_QC_table, self.QC_method_id)
+			
+			header, strain_acc_list, category_list, data_matrix = self.read_call_matrix(call_info_id2fname)
+			snpData1 = SNPData(header=header, strain_acc_list=strain_acc_list, category_list=category_list, data_matrix=data_matrix)
+			twoSNPData = TwoSNPData(SNPData1=snpData1, SNPData2=snpData2, curs=curs, QC_method_id=self.QC_method_id)
+			
+			row_id2NA_mismatch_rate = twoSNPData.cmp_row_wise()
+			
+			self.submit_to_call_QC(curs, row_id2NA_mismatch_rate, self.call_QC_table, self.QC_method_id, self.user)
 		if self.commit:
 			curs.execute("commit")
 
