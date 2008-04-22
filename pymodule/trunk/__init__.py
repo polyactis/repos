6e287368ace10cc23c4538fbac26517d71cfb43e
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-04-22 00:12:21 +0000

    modify process_function_arguments() and process_options() due to change in option_default_dict
    add generate_program_doc to automatically generate documentation like "Usage: ... \n Argument List: ... " from option_default_dict
    
    
    git-svn-id: file:///usr/local/svn/repos@495 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- pymodule/trunk/__init__.py
+++ pymodule/trunk/__init__.py
@@ -11,6 +11,24 @@ def process_function_arguments(keywords, argument_default_dict, error_doc='', cl
 	2008-04-02
 		add class_to_have_attr to assign the argument and values
 	2008-02-28
+		example of argument_default_dict:
+		
+			argument_default_dict = {('hostname',1, ):'papaya.usc.edu',\
+								('dbname',1, ):'stock_250k',\
+								('user',1, ):None,\
+								('passwd',1, ):None,\
+								('experimenter',0,):None,\
+								('mapping_file',1,):None,\
+								('input_dir',1, ):None,\
+								('output_dir',1, ):'/Network/Data/250k/db/raw_data/',\
+								('array_data_table',0, ):'array_data',\
+								('probes_table',0, ):'probes',\
+								('strain_info_table',0, ):'strain_info',\
+								('array_info_table',1, ):'array_info',\
+								('commit',0, int):0,\
+								('debug',0, int):0,\
+								('report',0, int):0}
+
 		process arguments from keywords based on argument_default_dict
 		
 		keywords is a dictionary of given arguments. 'argument_name':'argument_value'
@@ -57,8 +75,26 @@ def process_function_arguments(keywords, argument_default_dict, error_doc='', cl
 
 def process_options(argv_list, option_default_dict, error_doc=''):
 	"""
+	
 	2008-04-20
 		wraps the option handling (getopt), the usual block underneath "if __name__ == '__main__':"
+	example of option_default_dict:
+	
+		option_default_dict = {('z', 'hostname', 1, 'hostname of the db server', 1, ): 'papaya.usc.edu',\
+							('d', 'dbname', 1, '', 1, ): 'stock_250k',\
+							('u', 'user', 1, '', 1, ):None,\
+							('p', 'passwd', 1, '', 1, ):None,\
+							('t', 'call_info_table', 1, '', 1, ): 'call_info',\
+							('i', 'cmp_data_filename', 1, 'the data file to be compared with.', 1, ): None,\
+							('q', 'call_QC_table', 1, '', 1, ): 'call_QC',\
+							('m', 'QC_method_id', 1, 'id in table QC_method', 1, int): None,\
+							('c', 'commit', 0, 'commit db transaction', 0, int):0,\
+							('b', 'debug', 0, 'toggle debug mode', 0, int):0,\
+							('r', 'report', 0, 'toggle report, more verbose stdout/stderr.', 0, int):0}
+	
+		option_default_dict is a dictionary for option handling, including argument_default_dict info
+		the key is a tuple, ('short_option', 'long_option', has_argument, description_for_option, is_option_required, argument_type)
+		argument_type is optional
 	"""
 	import sys
 	if len(argv_list) == 1:
@@ -71,8 +107,11 @@ def process_options(argv_list, option_default_dict, error_doc=''):
 	short_option2long_option = {}
 	long_option2has_argument = {}
 	for option_key in option_default_dict:
-		long_option, short_option, has_argument = option_key[0:3]
-		long_options_list.append(long_option)
+		short_option, long_option, has_argument = option_key[0:3]
+		if has_argument:
+			long_options_list.append('%s='%long_option)
+		else:
+			long_options_list.append('%s'%long_option)
 		if has_argument:
 			short_options_list.append('%s:'%short_option)
 		else:
@@ -112,15 +151,67 @@ def turn_option_default_dict2argument_default_dict(option_default_dict):
 	"""
 	argument_default_dict = {}
 	for option_key, default_value in option_default_dict.iteritems():
-		long_option, short_option, has_argument, is_option_required = option_key[0:4]
-		if len(option_key)==5:
-			argument_type = option_key[4]
+		short_option, long_option, has_argument, description_for_option, is_option_required = option_key[0:5]
+		if len(option_key)==6:
+			argument_type = option_key[5]
 			argument_key = (long_option, is_option_required, argument_type)
 		else:
 			argument_key = (long_option, is_option_required, )
 		argument_default_dict[argument_key] = default_value
 	return argument_default_dict
+
+def generate_program_doc(program_name, option_default_dict):
+	"""
+	2008-04-21
+		automatically generate documentation like "Usage: ... \n Argument List: ... " from option_default_dict
 	
+	example of option_default_dict:
+	
+		option_default_dict = {('z', 'hostname', 1, 'hostname of the db server', 1, ): 'papaya.usc.edu',\
+							('d', 'dbname', 1, '', 1, ): 'stock_250k',\
+							('u', 'user', 1, '', 1, ):None,\
+							('p', 'passwd', 1, '', 1, ):None,\
+							('t', 'call_info_table', 1, '', 1, ): 'call_info',\
+							('i', 'cmp_data_filename', 1, 'the data file to be compared with.', 1, ): None,\
+							('q', 'call_QC_table', 1, '', 1, ): 'call_QC',\
+							('m', 'QC_method_id', 1, 'id in table QC_method', 1, int): None,\
+							('c', 'commit', 0, 'commit db transaction', 0, int):0,\
+							('b', 'debug', 0, 'toggle debug mode', 0, int):0,\
+							('r', 'report', 0, 'toggle report, more verbose stdout/stderr.', 0, int):0}
+	
+		option_default_dict is a dictionary for option handling, including argument_default_dict info
+		the key is a tuple, ('short_option', 'long_option', has_argument, description_for_option, is_option_required, argument_type)
+		argument_type is optional
+	"""
+	usage_str = 'Usage: %s'%program_name
+	argument_list_str_ls = ['Argument List:']
+	option_key_ls = option_default_dict.keys()
+	option_key_ls.sort()
+	for option_key in option_key_ls:
+		default_value = option_default_dict[option_key]
+		short_option, long_option, has_argument, description_for_option, is_option_required = option_key[0:5]
+		need_star = 0
+		this_argument_ls = ['\t']
+		if is_option_required and default_value==None or default_value=='':
+			usage_str += ' -%s %s'%(short_option, long_option.upper())
+			need_star = 1
+		if has_argument:
+			this_argument_ls.append('-%s ...,\t--%s=...'%(short_option, long_option))
+		else:
+			this_argument_ls.append('-%s,\t--%s'%(short_option, long_option))
+		if need_star:
+			this_argument_ls.append('*')
+		this_argument_ls.append('\t%s '%description_for_option)
+		if has_argument and default_value!=None and default_value!='':
+			this_argument_ls.append('%s(default)'%default_value)
+		argument_list_str_ls.append(''.join(this_argument_ls))
+	argument_list_str_ls.append("\nFor required(*) options, if no argument is given, you'll be prompted.")
+	program_doc = '\n%s\n\n'%usage_str
+	program_doc += '\n'.join(argument_list_str_ls)
+	program_doc += '\n'
+	return program_doc
+
+
 def write_data_matrix(data_matrix, output_fname, header, strain_acc_list, category_list, rows_to_be_tossed_out=None, cols_to_be_tossed_out=None, nt_alphabet=0):
 	"""
 	2008-04-02
