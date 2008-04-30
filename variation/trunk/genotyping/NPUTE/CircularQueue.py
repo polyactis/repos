ddcf6aeff0dc01878efeeadbd1ae0a5b59a6b486
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-04-30 07:23:44 +0000

    NPUTE SNP imputation program downloaded from http://compgen.unc.edu/?page_id=57
    Modern high-throughput genotyping techniques produce numerous missing calls that confound subsequent analyses, such as disease association studies. Common remedies or this problem include removing affected markers and/or samples or, otherwise, inferring the missing data. On small marker sets imputation is frequently based on a vote of the K-nearest-neighbor KNN) haplotypes, but this technique is neither practical nor ustifiable for large datasets. We have developed a data structure that supports efficient KNN queries over arbitrarily sized, sliding haplotype windows, and employ it for genotype imputation. The performance of our method enables exhaustive exploration over all window sizes and known sites in large (150K, 8.3M) SNP panels. Information about the underlying methods used in our viewer can be found in [Roberts2007a][NPUTE Presentation at ISMB 2007].
    
    
    
    
    git-svn-id: file:///usr/local/svn/repos@533 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

---
+++ variation/trunk/genotyping/NPUTE/CircularQueue.py
@@ -0,0 +1,50 @@
+from numpy import *
+
+
+class CircularQueue:
+    '''
+    This class implements a data structure for multiple, nested circular queues
+    using a numpy array.  The class has several extra useful functions used by NPUTE
+    to implement sliding window(s).
+    '''
+    def __init__(self,Ls,width):
+        '''
+        Constructor initializes datastructure.
+        '''
+        height = max(Ls)*2+2
+        self.height = height
+        self.Ls = Ls
+        self.queue = zeros((height,width),uint16)
+        self.half = max(Ls)
+        self.mid = -1
+        
+            
+    def getEnds(self,i):
+        '''
+        Returns the bounding rows of the nested queue specified by i.
+        Used to calculate mismatch vector over window i.
+        '''
+        top = self.queue[(self.mid+self.Ls[i])%self.height]
+        bottom = self.queue[(self.mid-self.Ls[i]-1)%self.height]
+        return top,bottom
+        
+    def enqueue(self,e):
+        '''
+        Adds element e to the end of the largest queue.
+        '''
+        self.incrementMid()
+        nextIn = (self.mid+self.half)%self.height
+        self.queue[nextIn] = e
+        
+    def incrementMid(self):
+        '''
+        Increments the index to the center element of all queues.
+        '''
+        self.mid = (self.mid+1) % self.height
+
+    def getMid(self):
+        '''
+        Returns the center element of all queues. Used to remove bias
+        when imputing called values during window testing.
+        '''
+        return self.queue[self.mid]
