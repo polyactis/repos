b0b0bf96ae363e2f502d0d3066e1aef075f625da
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-05-19 08:42:07 +0000

    move
    ab2number, number2ab, NA_set, nt2number, \
    	number2nt, number2color, nt_number_matching_matrix, get_nt_number2diff_matrix_index,\
    	RawSnpsData_ls2SNPData, transposeSNPData, SNPData2RawSnpsData_ls
    	
    from variation.src.common to here
    
    SNPData
    	use num.int8 as matrix data type
    
    	def tofile(self, output_fname, **keywords):
                """
                  2008-05-18
                  keywords is same as write_data_matrix()
    
    git-svn-id: file:///usr/local/svn/repos@724 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- pymodule/trunk/SNP.py
+++ pymodule/trunk/SNP.py
@@ -9,6 +9,242 @@ sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 from ProcessOptions import  ProcessOptions
 from utils import dict_map, importNumericArray
 
+#2008-05-06 ab2number and number2ab is for 384-illumina data
+ab2number = {'N': 0,
+	'NA': 0,
+	'A': 1,
+	'B': 2,
+	'H': 3}
+
+number2ab = {0: 'NA',
+	1: 'A',
+	2: 'B',
+	3: 'H'}
+
+#2008-05-12	a common NA set
+from sets import Set
+NA_set = Set([0, 'NA', 'N', -2, '|'])
+
+nt2number = {'|': -2,	#2008-01-07 not even tried. 'N'/'NA' is tried but produces inconclusive result.
+	'-': -1,	#deletion
+	'N': 0,
+	'NA': 0,
+	None: 0,
+	'A': 1,
+	'C': 2,
+	'G': 3,
+	'T': 4,
+	'AC':5,
+	'CA':5,
+	'M':5,
+	'AG':6,
+	'GA':6,
+	'R':6,
+	'AT':7,
+	'TA':7,
+	'W':7,
+	'CG':8,
+	'GC':8,
+	'S':8,
+	'CT':9,
+	'TC':9,
+	'Y':9,
+	'GT':10,
+	'TG':10,
+	'K':10
+	}
+
+number2nt = {-2: '|',	#2008-01-07 not even tried. 'N'/'NA' is tried but produces inconclusive result.
+	-1: '-',
+	0: 'NA',
+	1:'A',
+	2:'C',
+	3:'G',
+	4:'T',
+	5:'AC',
+	6:'AG',
+	7:'AT',
+	8:'CG',
+	9:'CT',
+	10:'GT'
+	}
+
+number2color = {-1:(0,0,0), 0:(255,255,255), 1:(0,0,255), 2:(0,255,0), 3:(255,0,0), 4:(122,0,122), 5:(122,122,0), 6:(122,255,255), 7:(122,122,122), 8:(255,122,0), 9:(255,255,122), 10:(122,122,255) }
+
+#2007-04-16 entry[i,j] means whether nucleotide i and j matches. 0(NA) matches everything. singleton(1-4) matches itself and the doublet containing it. doublet(5-10) matches only itself.
+nt_number_matching_matrix = [[1, 1,1,1,1,1, 1,1,1,1,1],
+	[1, 1,0,0,0,1, 1,1,0,0,0],
+	[1, 0,1,0,0,1, 0,0,1,1,0],
+	[1, 0,0,1,0,0, 1,0,1,0,1],
+	[1, 0,0,0,1,0, 0,1,0,1,1],
+	[1, 1,1,0,0,1, 0,0,0,0,0],
+	[1, 1,0,1,0,0, 1,0,0,0,0],
+	[1, 1,0,0,1,0, 0,1,0,0,0],
+	[1, 0,1,1,0,0, 0,0,1,0,0],
+	[1, 0,1,0,1,0, 0,0,0,1,0],
+	[1, 0,0,1,1,0, 0,0,0,0,1]]
+
+def get_nt_number2diff_matrix_index(number2nt):
+	"""
+	2008-05-19
+		moved from variation.src.common
+	2008-01-01 copied from CmpAccession2Ecotype.py
+	2007-10-31/2008-01-07
+		nucleotide number ranges from -2 to 10.
+		the diff_matrix_index ranges from 0 to 12.
+	"""
+	sys.stderr.write("Getting nt_number2diff_matrix_index from nt2number ...")
+	nt_number2diff_matrix_index = {}
+	number_nt_ls = []
+	for number, nt in number2nt.iteritems():
+		number_nt_ls.append([number,nt])
+	number_nt_ls.sort()
+	for i in range(len(number_nt_ls)):
+		nt_number2diff_matrix_index[number_nt_ls[i][0]] = i
+	sys.stderr.write("Done.\n")
+	return nt_number2diff_matrix_index
+
+def RawSnpsData_ls2SNPData(rawSnpsData_ls, report=0, use_nt2number=0):
+	"""
+	2008-05-11
+		adapts RawSnpsData(bjarni's SNP data structure) to SNPData
+		
+		combine all chromsomes together
+	"""
+	import sys
+	if report:
+		sys.stderr.write("Converting RawSnpsData_ls to SNPData ...")
+	from QC_250k import SNPData
+	nt_dict_map = lambda x: nt2number[x]
+	snpData = SNPData(row_id_ls = [], col_id_ls=[], data_matrix=[])
+	for i in range(len(rawSnpsData_ls)):
+		rawSnpsData = rawSnpsData_ls[i]
+		chromosome = rawSnpsData.chromosome
+		for j in range(len(rawSnpsData.positions)):
+			if use_nt2number:
+				data_row = map(nt_dict_map, rawSnpsData.snps[j])
+			else:
+				data_row = rawSnpsData.snps[j]
+			snpData.data_matrix.append(data_row)
+			snpData.row_id_ls.append((chromosome, rawSnpsData.positions[j]))
+		if i==0:	#only need once
+			for j in range(len(rawSnpsData.accessions)):
+				if rawSnpsData.arrayIds:
+					col_id = (rawSnpsData.arrayIds[j], rawSnpsData.accessions[j])
+				else:
+					col_id = rawSnpsData.accessions[j]
+				snpData.col_id_ls.append(col_id)
+	if report:
+		sys.stderr.write("Done.\n")
+	return snpData
+
+def transposeSNPData(snpData, report=0):
+	"""
+	2008-05-18
+		use num.int8 to keep memory small in num.transpose(num.array(snpData.data_matrix, num.int8))
+	2008-05-18
+		no more copy.deepcopy(snpData), data_matrix takes too long and too much memory
+	05/12/08 fix a bug (return snpData)
+	2008-05-11
+	"""
+	import sys
+	if report:
+		sys.stderr.write("Transposing SNPData ...")
+	from pymodule import importNumericArray, SNPData
+	num = importNumericArray()
+	#copy except data_matrix
+	import copy
+	newSnpData = SNPData()
+	"""
+	for option_tuple in SNPData.option_default_dict:
+		var_name = option_tuple[0]
+		if var_name!='data_matrix':
+			setattr(newSnpData, var_name, copy.deepcopy(getattr(snpData, var_name)))
+	"""
+	newSnpData.row_id_ls = copy.deepcopy(snpData.col_id_ls)
+	newSnpData.col_id_ls = copy.deepcopy(snpData.row_id_ls)
+	if isinstance(snpData.data_matrix, list):
+		newSnpData.data_matrix = num.transpose(num.array(snpData.data_matrix, num.int8))
+	else:	#assume it's array type already. Numeric/numarray has ArrayType, but numpy doesn't
+		newSnpData.data_matrix = num.transpose(snpData.data_matrix)
+	if report:
+		sys.stderr.write("Done.\n")
+	return newSnpData
+
+def SNPData2RawSnpsData_ls(snpData, use_number2nt=1, need_transposeSNPData=0, report=0, mask_untouched_deleltion_as_NA=1):
+	"""
+	2008-05-18
+		add mask_untouched_deleltion_as_NA. turned on by default because bjarni's RawSnpsData structure only recognizes NA, A, T, C, G
+		if col_id in newSnpData.col_id_ls is tuple of size >1, the 2nd one  in the tuple is taken as array id.
+	2008-05-12
+		reverse of RawSnpsData_ls2SNPData
+		
+		adapts SNPData (Yu's SNP data structure) to RawSnpsData(bjarni's SNP data structure)
+		
+		split into different chromsomes
+	"""
+	import sys
+	if report:
+		sys.stderr.write("Converting SNPData to RawSnpsData_ls ...")
+	from snpsdata import RawSnpsData
+	
+	if need_transposeSNPData:
+		newSnpData = transposeSNPData(snpData, report=report)
+	else:
+		newSnpData = snpData
+	
+	accessions = []
+	arrayIds = []
+	accession_id = None	#default
+	array_id = None	#default
+	for col_id in newSnpData.col_id_ls:
+		if isinstance(col_id, tuple):
+			if len(col_id)>0:
+				accession_id = col_id[0]
+			if len(col_id)>1:
+				array_id = col_id[1]
+		else:
+			accession_id = col_id
+		accessions.append(accession_id)
+		if array_id is not None:
+			arrayIds.append(array_id)
+	
+	if mask_untouched_deleltion_as_NA:
+		number2nt[-2] = 'NA'	#mask -2 (untouched) as 'NA'
+		number2nt[-1] = 'NA'	#mask -1 (deletion) as 'NA'
+	number2nt_dict_map = lambda x: number2nt[x]
+	rawSnpsData_ls = []
+	
+	rawSnpsData = RawSnpsData(accessions=accessions, arrayIds=arrayIds)
+	rawSnpsData.snps = []
+	rawSnpsData.positions = []
+	row_id0 = newSnpData.row_id_ls[0]
+	old_chromosome = row_id0.split('_')[:1]
+	rawSnpsData.chromosome = old_chromosome
+	rawSnpsData_ls.append(rawSnpsData)
+	rawSnpsData_ls_index = 0
+	for i in range(len(newSnpData.row_id_ls)):
+		row_id = newSnpData.row_id_ls[i]
+		new_chromosome, position = row_id.split('_')[:2]
+		position = int(position)
+		if new_chromosome!=old_chromosome:
+			rawSnpsData = RawSnpsData(accessions=accessions, arrayIds=arrayIds)
+			rawSnpsData.snps = []
+			rawSnpsData.positions = []
+			rawSnpsData_ls.append(rawSnpsData)
+			rawSnpsData_ls_index += 1
+			rawSnpsData.chromosome = new_chromosome
+			old_chromosome = new_chromosome
+		rawSnpsData_ls[rawSnpsData_ls_index].positions.append(position)
+		if use_number2nt:
+			data_row = map(number2nt_dict_map, newSnpData.data_matrix[i])
+		else:
+			data_row = newSnpData.data_matrix[i]
+		rawSnpsData_ls[rawSnpsData_ls_index].snps.append(data_row)
+	if report:
+		sys.stderr.write("Done.\n")
+	return rawSnpsData_ls
+
 def write_data_matrix(data_matrix, output_fname, header, strain_acc_list, category_list, rows_to_be_tossed_out=None, \
 					cols_to_be_tossed_out=None, nt_alphabet=0, transform_to_numpy=1,\
 					discard_all_NA_rows=0, strain_acc2other_info=None, delimiter='\t', predefined_header_row=['strain', 'duplicate', 'latitude', 'longitude', 'nativename', 'stockparent', 'site', 'country']):
@@ -28,7 +264,6 @@ def write_data_matrix(data_matrix, output_fname, header, strain_acc_list, catego
 	from sets import Set
 	import sys, csv
 	sys.stderr.write("Writing data_matrix ...")
-	from variation.src.common import number2nt
 	if rows_to_be_tossed_out==None:
 		rows_to_be_tossed_out = Set()
 	if cols_to_be_tossed_out==None:
@@ -95,8 +330,6 @@ def read_data(input_fname, input_alphabet=0, turn_into_integer=1, double_header=
 		add turn_into_integer
 	"""
 	import csv
-	from variation.src.common import nt2number
-	#from __init__ import dict_map	#already imported by from __init__ import *
 	sys.stderr.write("Reading data ...")
 	if delimiter is None:
 		delimiter = figureOutDelimiter(input_fname)
@@ -161,7 +394,7 @@ class SNPData(object):
 				
 		if not self.data_matrix and self.turn_into_array:
 			num = importNumericArray()
-			self.data_matrix = num.array(self.data_matrix)
+			self.data_matrix = num.array(self.data_matrix, num.int8)
 		
 		if self.row_id_ls is None and self.strain_acc_list is not None:
 			self.row_id_ls = []
@@ -177,6 +410,39 @@ class SNPData(object):
 			for i in range(2,len(self.header)):
 				col_id = self.header[i]
 				self.col_id_ls.append(col_id)
+	
+	def tofile(self, output_fname, **keywords):
+		"""
+		2008-05-18
+			keywords is same as write_data_matrix()
+		"""
+		if self.header is None:
+			self.header = ['', '']
+			for col_id in self.col_id_ls:
+				if isinstance(col_id, tuple):
+					if len(col_id)>1:
+						col_id = '%s_%s'%(col_id[0], col_id[1])	#chromosome, position
+					else:
+						col_id = col_id[0]
+				self.header.append(col_id)
+		if self.strain_acc_list is None:
+			self.strain_acc_list = []
+			self.category_list = []
+			for row_id in self.row_id_ls:
+				strain_acc = None
+				category = None
+				if isinstance(row_id, tuple):
+					if len(row_id)>0:
+						strain_acc = row_id[0]
+					if len(row_id)>1:
+						category = row_id[1]
+				else:
+					strain_acc = row_id
+				if category == None:
+					category = strain_acc	#make them same if category is not available
+				self.strain_acc_list.append(strain_acc)
+				self.category_list.append(category)
+		write_data_matrix(self.data_matrix, output_fname, self.header, self.strain_acc_list, self.category_list, **keywords)
 
 try:
 	from QualityControl import QualityControl
