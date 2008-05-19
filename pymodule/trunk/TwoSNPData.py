9821a5821ce2fe71b44f920e40a216e7acd601ab
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-05-19 14:13:32 +0000

    add these to TwoSNPData 	mergeTwoSNPData() 	mergeTwoSNPData2() (unfinished) 	intersectSNPData1_and_SNPData2_row_wise()
    git-svn-id: file:///usr/local/svn/repos@728 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- pymodule/trunk/TwoSNPData.py
+++ pymodule/trunk/TwoSNPData.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 2008-05-18
 """
@@ -103,6 +104,8 @@ class TwoSNPData(QualityControl):
 	
 	def update_row_col_matching(self):
 		"""
+		2008-05-19
+			add row_id2row_index and col_id2col_index to each SNPData
 		2008-05-11
 			fake two headers from col_id_ls
 		"""
@@ -118,6 +121,11 @@ class TwoSNPData(QualityControl):
 		else:	#use the default from QualityControl
 			self.col_id2col_index1, self.col_id2col_index2, self.col_id12col_id2 = self.get_col_matching_dstruc(self.SNPData1.col_id_ls, self.SNPData2.col_id_ls)
 		self.row_id2row_index1, self.row_id2row_index2, self.row_id12row_id2 = self.get_row_matching_dstruc(self.SNPData1.row_id_ls, self.SNPData2.row_id_ls)
+		
+		self.SNPData1.row_id2row_index = self.row_id2row_index1
+		self.SNPData1.col_id2col_index = self.col_id2col_index1
+		self.SNPData2.row_id2row_index = self.row_id2row_index2
+		self.SNPData2.col_id2col_index = self.col_id2col_index2
 		
 	def cmp_row_wise(self):
 		return QualityControl.cmp_row_wise(self.SNPData1.data_matrix, self.SNPData2.data_matrix, self.col_id2col_index1, self.col_id2col_index2, self.col_id12col_id2, self.row_id2row_index1, self.row_id2row_index2, self.row_id12row_id2)
@@ -154,4 +162,128 @@ class TwoSNPData(QualityControl):
 				snpsqc.readme = readme
 				session.save(snpsqc)
 		sys.stderr.write("Done.\n")
+	
+	def mergeTwoSNPData(self, need_row_id_union=0, need_col_id_union=0, priority=1):
+		"""
+		2008-05-19
+			if need_row_id_union==0, take SNPData1's row_id_ls as newSnpData's.
+				otherwise take the union of SNPData1 and SNPData2
+			same as need_col_id_union
+			not implemented the 'otherwise' yet. in progress in mergeTwoSNPData2()
+		"""
+		sys.stderr.write("Merging two SNPData ...\n")
 		
+		newSnpData = SNPData(row_id_ls=self.SNPData1.row_id_ls, col_id_ls=self.SNPData1.col_id_ls)
+		no_of_rows, no_of_cols = len(newSnpData.row_id_ls), len(newSnpData.col_id_ls)
+		newSnpData.data_matrix = num.zeros([no_of_rows, no_of_cols], num.int8)
+		no_of_replaced_snps = 0
+		for i in range(no_of_rows):
+			row_id = newSnpData.row_id_ls[i]
+			if row_id not in self.row_id12row_id2 or priority==1:
+				newSnpData.data_matrix[i] = self.SNPData1.data_matrix[i]
+			else:
+				row_id2 = self.row_id12row_id2[row_id]
+				SNPData2_row_index = self.SNPData2.row_id2row_index[row_id2]
+				for j in range(no_of_cols):
+					col_id = newSnpData.col_id_ls[j]
+					if col_id not in self.col_id12col_id2:
+						newSnpData.data_matrix[i][j] = self.SNPData1.data_matrix[i][j]
+					else:
+						col_id2 = self.col_id12col_id2[col_id]
+						SNPData2_col_index = self.SNPData2.col_id2col_index[col_id2]
+						newSnpData.data_matrix[i][j] = self.SNPData2.data_matrix[SNPData2_row_index][SNPData2_col_index]
+						no_of_replaced_snps += 1
+		newSnpData.no_of_replaced_snps = no_of_replaced_snps
+		no_of_total_snps = no_of_rows * no_of_cols
+		sys.stderr.write("%s out of %s replaced. Done.\n"%(no_of_replaced_snps, no_of_total_snps))
+		return newSnpData
+	
+	def mergeTwoSNPData2(self, need_row_id_union=0, need_col_id_union=0, priority=1):
+		"""
+		2008-05-19
+			if need_row_id_union==0, take SNPData1's row_id_ls as newSnpData's.
+				otherwise take the union of SNPData1 and SNPData2
+			same as need_col_id_union
+		"""
+		sys.stderr.write("Merging two SNPData ...\n")
+		
+		snpData_ls = [self.SNPData1, self.SNPData2]
+		no_of_snpDatas = len(snpData_ls)
+		
+		#construct new row_id_ls
+		new_row_id_ls = []
+		new_row_id2old_row_index = {}
+		SNPData2_row_id_checked_set = Set()
+		for row_id in snpData_ls[0].row_id_ls:
+			new_row_id2old_row_index[row_id] = [(0, snpData_ls[0].row_id2row_index[row_id])]
+			new_row_id_ls.append(row_id)
+			if row_id in self.row_id12row_id2:
+				SNPData2_row_id = self.row_id12row_id2[row_id]
+				SNPData2_row_id_checked_set.add(SNPData2_row_id)
+				new_row_id2old_row_index[row_id].append((1, snpData_ls[1].row_id2row_index[SNPData2_row_id]))
+		if need_row_id_union:
+			for row_id in snpData_ls[1].row_id_ls:
+				if row_id not in SNPData2_row_id_checked_set:
+					new_row_id2old_row_index[row_id] = [(1, snpData_ls[1].row_id2row_index[row_id])]
+					new_row_id_ls.append(row_id)
+		
+		#construct new col_id_ls
+		if need_col_id_union:
+			new_col_id_set = Set(self.SNPData1.col_id_ls)|Set(self.SNPData2.col_id_ls)
+		else:
+			new_col_id_set = Set(self.SNPData1.col_id_ls)
+		new_col_id_ls = list(new_col_id_set)
+		new_col_id_ls.sort()
+		new_col_id2old_col_index = {}
+		for new_col_id in new_col_id_ls:
+			new_col_id2old_col_index[new_col_id] = [None, None]
+			for i in range(no_of_snpDatas):
+				if new_col_id in snpData_ls[i].col_id2col_index:
+					new_col_id2old_col_index[new_col_id][i] = (i, snpData_ls[i].col_id2col_index[new_col_id])
+		
+		newSnpData = SNPData(row_id_ls=new_row_id_ls, col_id_ls=new_col_id_ls)
+		no_of_rows, no_of_cols = len(new_row_id_ls), len(new_col_id_ls)
+		newSnpData.data_matrix = num.zeros([no_of_rows, no_of_cols], num.int8)
+		for i in range(no_of_rows):
+			row_id = newSnpData.row_id_ls[i]
+			old_row_index = new_row_id2old_row_index[row_id]
+			for j in range(no_of_cols):
+				col_id = newSnpData.col_id_ls[j]
+				old_col_index = new_col_id2old_col_index[col_id]
+				
+		
+		for i in range(no_of_rows):
+			row_id = newSnpData.row_id_ls[i]
+			row_index1 = self.row_id2row_index1[row_id]
+			if row_id not in self.row_id12row_id2:
+				newSnpData.data_matrix[i] = self.SNPData1
+		sys.stderr.write("Done.\n")
+		return newSnpData
+	
+	def intersectSNPData1_and_SNPData2_row_wise(self):
+		"""
+		2008-05-19
+		"""
+		sys.stderr.write("Intersecting row-wise ...")		
+		no_of_rows = len(self.row_id12row_id2)
+		no_of_cols = len(self.SNPData1.col_id_ls)
+		newSnpData = SNPData(col_id_ls=self.SNPData1.col_id_ls, row_id_ls=[])
+		newSnpData.data_matrix = num.zeros([no_of_rows, no_of_cols], num.int8)
+		row_index = 0
+		for i in range(len(self.SNPData1.row_id_ls)):
+			row_id = self.SNPData1.row_id_ls[i]
+			if row_id in self.row_id12row_id2:
+				newSnpData.row_id_ls.append(row_id)
+				newSnpData.data_matrix[row_index] = self.SNPData1.data_matrix[i]
+				row_index += 1
+		newSnpData.no_of_rows_removed = len(self.SNPData1.row_id_ls)-no_of_rows
+		sys.stderr.write("%s rows removed. Done.\n"%(newSnpData.no_of_rows_removed))
+		return newSnpData
+
+if __name__ == '__main__':
+	snpData1 = SNPData(input_fname=sys.argv[1], turn_into_array=1, ignore_2nd_column=1)
+	snpData2 = SNPData(input_fname=sys.argv[2], turn_into_array=1, ignore_2nd_column=1)
+	output_fname = sys.argv[3]
+	twoSNPData = TwoSNPData(SNPData1=snpData1, SNPData2=snpData2, debug=1)
+	newSnpData= twoSNPData.intersectSNPData1_and_SNPData2_row_wise()
+	newSnpData.tofile(output_fname)
