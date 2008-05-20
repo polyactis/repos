6c93579864360392101046228482971180b7c4d7
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-05-20 00:42:21 +0000

    def fromfile(self, input_fname, **keywords): 		""" 		2008-05-19 			initialize structure from a file in Strain X SNP format. 1st two columns are about strains. 			for keywords. check read_data()
    class SNPData(object):
    	"""
    	2008-05-19
    		either directly specify row_id_ls, col_id_ls, data_matrix
    			(if strain_acc_list and category_list is given rather than row_id_ls and col_id_ls, row_id_ls and col_id_ls will be formed from them).
    		or give input_fname and associated options to read data from file.
    
    
    def write_data_matrix()
    	2008-05-19
    		add output_fname in report
    
    git-svn-id: file:///usr/local/svn/repos@743 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- pymodule/trunk/SNP.py
+++ pymodule/trunk/SNP.py
@@ -262,6 +262,8 @@ def write_data_matrix(data_matrix, output_fname, header, strain_acc_list, catego
 	strain_acc_list (and category_list) are initial 2 columns in the output.
 	
 	rows_to_be_tossed_out is a set or dict with row index in it. cols_to_be_tossed_out is similar structure.
+	2008-05-19
+		add output_fname in report
 	2008-05-12
 		copied from __init__.py
 	2008-05-08
@@ -273,7 +275,7 @@ def write_data_matrix(data_matrix, output_fname, header, strain_acc_list, catego
 	"""
 	from sets import Set
 	import sys, csv
-	sys.stderr.write("Writing data_matrix ...")
+	sys.stderr.write("Writing data_matrix to %s ..."%(output_fname))
 	if rows_to_be_tossed_out==None:
 		rows_to_be_tossed_out = Set()
 	if cols_to_be_tossed_out==None:
@@ -369,6 +371,10 @@ def read_data(input_fname, input_alphabet=0, turn_into_integer=1, double_header=
 
 class SNPData(object):
 	"""
+	2008-05-19
+		either directly specify row_id_ls, col_id_ls, data_matrix
+			(if strain_acc_list and category_list is given rather than row_id_ls and col_id_ls, row_id_ls and col_id_ls will be formed from them).
+		or give input_fname and associated options to read data from file.
 	2008-05-18 moved from variation.src.QC_250k
 		add more arguments, input_fname, turn_into_array, ignore_2nd_column
 	"""
@@ -401,9 +407,10 @@ class SNPData(object):
 			self.header, self.strain_acc_list, self.category_list, self.data_matrix = read_data(self.input_fname, self.input_alphabet, self.turn_into_integer, self.double_header)
 			if self.ignore_2nd_column:
 				self.category_list = None
-				
+		self.processRowIDColID()
+	
+	def processRowIDColID(self):
 		if not self.data_matrix and self.turn_into_array:
-			num = importNumericArray()
 			self.data_matrix = num.array(self.data_matrix, num.int8)
 		
 		if self.row_id_ls is None and self.strain_acc_list is not None:
@@ -421,6 +428,17 @@ class SNPData(object):
 				col_id = self.header[i]
 				self.col_id_ls.append(col_id)
 	
+	def fromfile(self, input_fname, **keywords):
+		"""
+		2008-05-19
+			initialize structure from a file in Strain X SNP format. 1st two columns are about strains.
+			for keywords. check read_data()
+		"""
+		self.header, self.strain_acc_list, self.category_list, self.data_matrix = read_data(input_fname, **keywords)
+		if self.ignore_2nd_column:
+			self.category_list = None
+		self.processRowIDColID()
+		
 	def tofile(self, output_fname, **keywords):
 		"""
 		2008-05-18
