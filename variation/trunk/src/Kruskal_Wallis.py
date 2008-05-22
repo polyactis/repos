e0e89f77eb1b0025bded988b82f33d3a078146a1
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-05-22 07:56:20 +0000

    program becomes standalone.
    	Input genotype file format is Strain X SNP format (Yu's format, Output by DB_250k2data.py Or Output250KSNPs.py + ConvertBjarniSNPFormat2Yu.py).
    	Input phenotype file format is Strain X phenotype format (Output by OutputPhenotype.py). 
    	
    	It will automatically match strains in two files. NO worry for missing/extra data in either input file.
    
    git-svn-id: file:///usr/local/svn/repos@764 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/Kruskal_Wallis.py
+++ variation/trunk/src/Kruskal_Wallis.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+"""
+
+Examples:
+	./src/Kruskal_Wallis.py -i /tmp/250K_method_5_after_imputation_noRedundant_051908.tsv -p /Network/Data/250k/finalData_051808/phenotypes.tsv -e -r -o /tmp/250K_method_5_after_imputation_noRedundant_051908.LD.pvalue
+
+Description:
+	class to do kruskal wallis test on SNP data.
+	
+	Input genotype file format is Strain X SNP format (Yu's format, Output by DB_250k2data.py Or Output250KSNPs.py + ConvertBjarniSNPFormat2Yu.py).
+	Input phenotype file format is Strain X phenotype format (Output by OutputPhenotype.py). 
+	
+	It will automatically match strains in two files. NO worry for missing/extra data in either input file.
+"""
+
 import sys, os, math
 bit_number = math.log(sys.maxint)/math.log(2)
 if bit_number>40:       #64bit
@@ -6,24 +21,23 @@ if bit_number>40:       #64bit
 else:   #32bit
 	sys.path.insert(0, os.path.expanduser('~/lib/python'))
 	sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
-import csv
-
+import csv, numpy
+from pymodule import read_data, ProcessOptions, PassingData
 
 class Kruskal_Wallis:
-	"""
-	2008-02-14
-		class to do kruskal wallis test on SNP data
-	Argument list:
-		-i ..., input_fname*
-		-o ..., output_fname*
-		-a ..., phenotype_fname(argument1)*
-		-e ..., minus log the pvalue(argument2), 0 or 1
-		-b,	toggle debug
-		-r, toggle report
-	"""
-	def __init__(self, input_fname, phenotype_fname, output_fname, log_pvalue=0, debug=0, report=0):
+	__doc__ = __doc__
+	option_default_dict = {('input_fname', 1, ): ['', 'i', 1, 'input genotype matrix. Strain X SNP format.', ],\
+							('output_fname', 1, ): ['', 'o', 1, 'store the pvalue', ],\
+							('phenotype_fname', 1, ): [None, 'p', 1, 'phenotype file', ],\
+							('minus_log_pvalue', 0, ): [0, 'e', 0, 'toggle -log(pvalue)', ],\
+							('which_phenotype', 1, int): [0, 'w', 1, 'which phenotype, 0=first phenotype (3rd column in phenotype_fname) and so on.',],\
+							('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
+							('report', 0, int):[0, 'r', 0, 'toggle report, more verbose stdout/stderr.']}
+	def __init__(self, **keywords):
 		"""
 		2008-02-14
+		"""
+		ProcessOptions.process_function_arguments(keywords, self.option_default_dict, error_doc=self.__doc__, class_to_have_attr=self)
 		"""
 		if not input_fname or not phenotype_fname or not output_fname:
 			print self.__doc__
@@ -34,22 +48,36 @@ class Kruskal_Wallis:
 		self.log_pvalue = int(log_pvalue)
 		self.debug = int(debug)
 		self.report = int(report)
+		"""
 	
 	def get_phenotype_ls_in_data_matrix_order(self, strain_acc_list, strain_acc_list_phen, category_list_phen, data_type=float):
 		"""
+		2008-05-21
+			strain_acc could be missing in strain_acc_phen2index
+			phenotype_value could be 'NA'
 		2008-02-14
 		"""
 		sys.stderr.write("Getting phenotype_ls in order with data_matrix ...")
 		strain_acc_phen2index = dict(zip(strain_acc_list_phen, range(len(strain_acc_list_phen)) ) )
 		phenotype_ls = []
 		for strain_acc in strain_acc_list:
-			phen_index = strain_acc_phen2index[strain_acc]
-			phenotype_ls.append(float(category_list_phen[phen_index]))
+			if strain_acc in strain_acc_phen2index:
+				phen_index = strain_acc_phen2index[strain_acc]
+				phenotype_value = category_list_phen[phen_index]
+				if phenotype_value !='NA':
+					phenotype_ls.append(data_type(phenotype_value))
+				else:
+					phenotype_ls.append(None)
+			else:
+				phenotype_ls.append(None)
 		sys.stderr.write("Done.\n")
 		return phenotype_ls
 	
 	def _kruskal_wallis(self, data_matrix, phenotype_ls, min_data_point=3):
 		"""
+		2008-05-21
+			phenotype_ls could have None value, skip them
+			each kw result is wrapped in PassingData
 		2008-02-14
 		"""
 		sys.stderr.write("Doing kruskal wallis test ...\n")
@@ -69,7 +97,7 @@ class Kruskal_Wallis:
 			non_NA_phenotype_ls = []
 			non_NA_genotype2count = {}
 			for i in range(no_of_rows):
-				if genotype_ls[i]!=0:
+				if genotype_ls[i]!=0 and phenotype_ls[i]!=None:
 					non_NA_genotype = genotype_ls[i]
 					non_NA_genotype_ls.append(non_NA_genotype)
 					non_NA_phenotype_ls.append(phenotype_ls[i])
@@ -79,7 +107,8 @@ class Kruskal_Wallis:
 			count_ls = non_NA_genotype2count.values()
 			if len(count_ls)>=2 and count_ls[0]>=min_data_point and count_ls[1]>=min_data_point:
 				pvalue = rpy.r.kruskal_test(x=non_NA_phenotype_ls, g=rpy.r.as_factor(non_NA_genotype_ls))['p.value']
-				results.append([j, pvalue])
+				pdata = PassingData(snp_index=j, pvalue=pvalue, count_ls=count_ls)
+				results.append(pdata)
 				real_counter += 1
 			counter += 1
 			if self.report and counter%2000==0:
@@ -91,19 +120,25 @@ class Kruskal_Wallis:
 	
 	def output_kw_results(self, kw_results, SNP_header, output_fname, log_pvalue=0):
 		"""
+		2008-05-21
+			more stuff in kw_results
+			each kw result is wrapped in PassingData
 		2008-02-14
 		"""
 		sys.stderr.write("Outputting pvalue results ...")
 		writer = csv.writer(open(output_fname,'w'), delimiter='\t')
-		for column_index, pvalue in kw_results:
-			SNP_name = SNP_header[column_index]
+		for i in range(len(kw_results)):
+			pdata = kw_results[i]
+			snp_index = pdata.snp_index
+			pvalue = pdata.pvalue
+			SNP_name = SNP_header[snp_index]
 			SNP_pos_ls = SNP_name.split('_')
 			if log_pvalue:
 				if pvalue>0:
 					pvalue = -math.log(pvalue)
 				else:
 					pvalue = 'NA'
-			writer.writerow([SNP_pos_ls[0], SNP_pos_ls[1], pvalue])
+			writer.writerow([SNP_pos_ls[0], SNP_pos_ls[1], pvalue] + pdata.count_ls)
 		del writer
 		sys.stderr.write("Done.\n")
 	
@@ -111,11 +146,18 @@ class Kruskal_Wallis:
 		if self.debug:
 			import pdb
 			pdb.set_trace()
-		from variation.src.FilterStrainSNPMatrix import FilterStrainSNPMatrix
-		FilterStrainSNPMatrix_instance = FilterStrainSNPMatrix()
-		header, strain_acc_list, category_list, data_matrix = FilterStrainSNPMatrix_instance.read_data(self.input_fname)
-		header_phen, strain_acc_list_phen, category_list_phen, data_matrix_phen = FilterStrainSNPMatrix_instance.read_data(self.phenotype_fname)
-		phenotype_ls = self.get_phenotype_ls_in_data_matrix_order(strain_acc_list, strain_acc_list_phen, category_list_phen, data_type=float)
+		header_phen, strain_acc_list_phen, category_list_phen, data_matrix_phen = read_data(self.phenotype_fname, turn_into_integer=0)
+		header, strain_acc_list, category_list, data_matrix = read_data(self.input_fname)
+		data_matrix_phen = numpy.array(data_matrix_phen)
+		#2008-05-21 take the 1st phenotype
+		phenotype_ls = self.get_phenotype_ls_in_data_matrix_order(strain_acc_list, strain_acc_list_phen, data_matrix_phen[:, self.which_phenotype], data_type=float)
 		kw_results = self._kruskal_wallis(data_matrix, phenotype_ls)
-		self.output_kw_results(kw_results, header[2:], self.output_fname, self.log_pvalue)
-		
+		self.output_kw_results(kw_results, header[2:], self.output_fname, self.minus_log_pvalue)
+
+if __name__ == '__main__':
+	from pymodule import ProcessOptions
+	main_class = Kruskal_Wallis
+	po = ProcessOptions(sys.argv, main_class.option_default_dict, error_doc=main_class.__doc__)
+	
+	instance = main_class(**po.long_option2value)
+	instance.run()
