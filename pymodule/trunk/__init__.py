056e2159500f6b37d71ecc161eeca26a15bb73bf
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-05-31 08:20:30 +0000

    from TwoSNPData import TwoSNPData, QualityControl
    git-svn-id: file:///usr/local/svn/repos@809 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- pymodule/trunk/__init__.py
+++ pymodule/trunk/__init__.py
@@ -7,4 +7,5 @@ sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
 from ProcessOptions import ProcessOptions, generate_program_doc, process_options, process_function_arguments, turn_option_default_dict2argument_default_dict
 from SNP import write_data_matrix, read_data, SNPData
+from TwoSNPData import TwoSNPData, QualityControl
 from utils import PassingData, dict_map, importNumericArray, figureOutDelimiter
