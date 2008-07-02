6678035490c93bae99265b6c2eca6796f09a9d70
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-07-02 05:59:41 +0000

    use Rscript plot the array intensity after normalization as 'blue'
    git-svn-id: file:///usr/local/svn/repos@843 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/Rong_codes/plot-mean-intensity-348arrays.R
+++ variation/trunk/Rong_codes/plot-mean-intensity-348arrays.R
@@ -1,12 +1,13 @@
+#!/usr/bin/env Rscript
 #2008-05-17 Rong's piece of code to draw histogram of normalized intensity and unnormalized intensity from each array.
 #modifed to take arguments, for loop, etc
-R --vanilla --args $0 $* << EOF
+#R --vanilla --args $0 $* << EOF	#old way to make R script standalone
 
 command_args = commandArgs()
-print(command_args)
-program_name = command_args[4]
+#program_name = command_args[4]
+program_name = substr(command_args[4], 8, nchar(command_args[4]))	#command_args[4] = "--file=./plot-mean-...R"
 
-if (length(command_args)!=8)	#2008-05-17 command_args starts with ["/usr/lib/R/bin/exec/R", "--vanilla", "--args"]
+if (length(command_args)!=9)	#2008-07-01 command_args starts with ["/usr/lib/R/bin/exec/R", "--slave", "--no-restore", "--file=../test/R/test_command_arguments.R", "--args"]
 {
 	cat("\n")
 	cat("Usage:", program_name, "INPUT_DIR START_ARRAY_ID END_ARRAY_ID PDF_OUTPUT_FNAME\n")
@@ -15,10 +16,10 @@ if (length(command_args)!=8)	#2008-05-17 command_args starts with ["/usr/lib/R/b
 	q('no')	#dont' save this session
 }
 
-input_dir = command_args[5]
-start_array_id = as.real(command_args[6])
-end_array_id = as.real(command_args[7])
-output_fname = command_args[8]
+input_dir = command_args[6]
+start_array_id = as.real(command_args[7])
+end_array_id = as.real(command_args[8])
+output_fname = command_args[9]
 
 load("m.norm.107arrays.rda")	#take this as normalized data
 m.norm <- m.norm/107
@@ -26,11 +27,21 @@ pdf(output_fname)
 
 for (i in seq(start_array_id, end_array_id))
 {
-	probe_mean_fname = paste(input_dir, '/', i , '_mprobe_mean.rda', sep="")
-	cat(probe_mean_fname)
-	load(probe_mean_fname)
+	cat("Array:", i)
+	#1. plot the learning set, 107 arrays' intensity
 	plot(density(m.norm), "l", xlim=c(1, 10), ylim=c(0, 1), main=paste("array id = ", i))
+	
+	#2. plot the original intensity of this array
+	probe_mean_fname = paste(input_dir, '/', i , '_mprobe_mean.rda', sep="")	
+	load(probe_mean_fname)
 	lines(density(mprobe.mean), col="red")
+	
+	#3. plot the intensity after normalization
+	probe_norm_fname = paste(input_dir, '/', i , '_mprobe_norm.rda', sep="")
+	load(probe_norm_fname)
+	lines(density(mprobe.norm), col="blue")
+	
+	rm(mprobe.norm)
 	rm(mprobe.mean)
 	cat("\n")
 }
