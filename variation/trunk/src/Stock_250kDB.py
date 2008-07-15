2f620f1d757e68eed40c593939683d760b767121
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-07-15 07:26:01 +0000

    add unique constraints for gene_list and gene_list_type
    add entities for these tables: CallMethod, PhenotypeMethod, ResultsMethodType, ResultsMethod
    
    git-svn-id: file:///usr/local/svn/repos@877 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/Stock_250kDB.py
+++ variation/trunk/src/Stock_250kDB.py
@@ -21,17 +21,19 @@ else:   #32bit
 	sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
 from sqlalchemy.engine.url import URL
-from elixir import Unicode, DateTime, String, Integer, UnicodeText, Text
+from elixir import Unicode, DateTime, String, Integer, UnicodeText, Text, Boolean
 from elixir import Entity, Field, using_options, using_table_options
 from elixir import OneToMany, ManyToOne, ManyToMany
 from elixir import setup_all, session, metadata, entities
 from elixir.options import using_table_options_handler	#using_table_options() can only work inside Entity-inherited class.
+from sqlalchemy import UniqueConstraint
+
 from datetime import datetime
 
 from pymodule.db import ElixirDB
 
 class GeneListType(Entity):
-	short_name = Field(String(256))
+	short_name = Field(String(256), unique=True)
 	description = Field(String(8192))
 	created_by = Field(String(128))
 	updated_by = Field(String(128))
@@ -50,7 +52,91 @@ class GeneList(Entity):
 	date_updated = Field(DateTime)
 	using_options(tablename='gene_list')
 	using_table_options(mysql_engine='InnoDB')
+	using_table_options(UniqueConstraint('gene_id', 'list_type_id'))
 
+
+
+class Snps(Entity):
+	name = Field(String(200), unique=True, nullable = False)
+	chromosome = Field(Integer)
+	position = Field(Integer)
+	end_position = Field(Integer)
+	allele1 = Field(String(1))
+	allele2 = Field(String(2))
+	created_by = Field(String(200))
+	updated_by = Field(String(200))
+	date_created = Field(DateTime, default=datetime.now)
+	date_updated = Field(DateTime)
+	using_options(tablename='snps')
+	using_table_options(mysql_engine='InnoDB')
+
+class SnpsContext(Entity):
+	snp = ManyToOne('Snps', colname='snps_id', ondelete='CASCADE', onupdate='CASCADE')
+	disp_pos = Field(Integer)
+	gene_id = Field(Integer)
+	gene_strand = Field(String(1))
+	disp_pos_comment = Field(String(2000))
+	created_by = Field(String(200))
+	updated_by = Field(String(200))
+	date_created = Field(DateTime, default=datetime.now)
+	date_updated = Field(DateTime)
+	using_options(tablename='snps_context')
+	using_table_options(mysql_engine='InnoDB')
+
+
+class CallMethod(Entity):
+	short_name = Field(String(20))
+	method_description = Field(String(8000))
+	data_description = Field(String(8000))
+	comment = Field(String(8000))
+	created_by = Field(String(200))
+	updated_by = Field(String(200))
+	date_created = Field(DateTime, default=datetime.now)
+	date_updated = Field(DateTime)
+	using_options(tablename='call_method')
+	using_table_options(mysql_engine='InnoDB')
+	
+class PhenotypeMethod(Entity):
+	short_name = Field(String(20))
+	only_first_96 = Field(Boolean, default=0)
+	method_description = Field(String(8000))
+	data_description = Field(String(8000))
+	comment = Field(String(8000))
+	created_by = Field(String(200))
+	updated_by = Field(String(200))
+	date_created = Field(DateTime, default=datetime.now)
+	date_updated = Field(DateTime)
+	data_type = Field(String(200))
+	transformation_description = Field(String(8000))
+	using_options(tablename='phenotype_method')
+	using_table_options(mysql_engine='InnoDB')
+
+class ResultsMethodType(Entity):
+	short_name = Field(String(30), unique=True)
+	method_description = Field(String(8000))
+	data_description = Field(String(8000))
+	created_by = Field(String(200))
+	updated_by = Field(String(200))
+	date_created = Field(DateTime, default=datetime.now)
+	date_updated = Field(DateTime)
+	using_options(tablename='results_method_type')
+	using_table_options(mysql_engine='InnoDB')
+
+class ResultsMethod(Entity):
+	short_name = Field(String(30), unique=True)
+	filename = Field(String(1000), unique=True)
+	method_description = Field(String(8000))
+	data_description = Field(String(8000))
+	comment = Field(String(8000))
+	phenotype_method = ManyToOne('PhenotypeMethod', colname='phenotype_method_id', ondelete='CASCADE', onupdate='CASCADE')
+	call_method = ManyToOne('CallMethod', colname='call_method_id', ondelete='CASCADE', onupdate='CASCADE')
+	results_method_type = ManyToOne('ResultsMethodType', colname='results_method_type_id', ondelete='CASCADE', onupdate='CASCADE')
+	created_by = Field(String(200))
+	updated_by = Field(String(200))
+	date_created = Field(DateTime, default=datetime.now)
+	date_updated = Field(DateTime)
+	using_options(tablename='results_method')
+	using_table_options(mysql_engine='InnoDB')
 
 class Stock_250kDB(ElixirDB):
 	__doc__ = __doc__
