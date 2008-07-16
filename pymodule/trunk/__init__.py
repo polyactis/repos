16c8b4de1b13090ccc55b513d1b50c721d007a7c
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-07-16 05:19:03 +0000

    git-svn-id: file:///usr/local/svn/repos@882 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- pymodule/trunk/__init__.py
+++ pymodule/trunk/__init__.py
@@ -8,4 +8,4 @@ sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 from ProcessOptions import ProcessOptions, generate_program_doc, process_options, process_function_arguments, turn_option_default_dict2argument_default_dict
 from SNP import write_data_matrix, read_data, SNPData
 from TwoSNPData import TwoSNPData, QualityControl
-from utils import PassingData, dict_map, importNumericArray, figureOutDelimiter
+from utils import PassingData, dict_map, importNumericArray, figureOutDelimiter, get_gene_symbol2gene_id_set
