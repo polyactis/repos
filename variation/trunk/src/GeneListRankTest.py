4c032abfa3d66fc2430db4dea262eb394b4e0e50
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-07-17 00:27:06 +0000

    reverse the order of 1st-read results file, 2nd-read db.snps_context
    class SnpsContextWrapper added to store data structure from snps_context
    
    git-svn-id: file:///usr/local/svn/repos@886 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/GeneListRankTest.py
+++ variation/trunk/src/GeneListRankTest.py
@@ -4,7 +4,8 @@ Examples:
 	GeneListRankTest.py -e 389 -l 1 -u yh
 	
 Description:
-2008-07-14 program to do pvalue rank test based on a given candidate gene list.
+	2008-07-14 program to do pvalue rank test based on a given candidate gene list.
+	
 """
 
 import sys, os, math
@@ -22,6 +23,36 @@ from pymodule import PassingData, figureOutDelimiter
 from Stock_250kDB import Stock_250kDB, Snps, SnpsContext, ResultsMethod, GeneList
 from Results2DB_250k import Results2DB_250k
 from GenomeBrowser import GenomeBrowser
+import rpy
+
+from pymodule.db import TableClass
+class SnpsContextWrapper(TableClass):
+	chrpos2row_index_ls = None
+	data_matrix = None
+	no_of_rows = 0
+	
+	def addOneSnpsContext(self, snps_context):
+		if self.data_matrix is None:
+			self.data_matrix = []
+		self.data_matrix.append([snps_context.snps_id, snps_context.disp_pos, snps_context.gene_id])
+		self.no_of_rows += 1
+		if self.chrpos2row_index_ls is None:
+			self.chrpos2row_index_ls = {}
+		chrpos_key = (snps_context.snp.chromosome, snps_context.snp.position)
+		if chrpos_key not in self.chrpos2row_index_ls:
+			self.chrpos2row_index_ls[chrpos_key] = []
+		self.chrpos2row_index_ls[chrpos_key].append(self.no_of_rows-1)
+	
+	def returnGeneLs(self, chromosome, position):
+		return_matrix = []
+		chrpos_key = (chromosome, position)
+		row_index_ls = self.chrpos2row_index_ls.get(chrpos_key)
+		if row_index_ls is not None:
+			for row_index in row_index_ls:
+				return_matrix.append(self.data_matrix[row_index])
+		return return_matrix
+		
+	
 
 class GeneListRankTest(object):
 	__doc__ = __doc__
@@ -31,9 +62,10 @@ class GeneListRankTest(object):
 							('schema', 0, ): [None, 'k', 1, 'database schema name', ],\
 							('db_user', 1, ): [None, 'u', 1, 'database username', ],\
 							('db_passwd', 1, ): [None, 'p', 1, 'database password', ],\
-							("results_method_id", 1, ): [None, '', 1, ''],\
-							("min_distance", 1, int): [50000, '', 1, ''],\
+							("results_method_id_ls", 1, ): [None, 'e', 1, 'comma-separated results_method_id list'],\
+							("min_distance", 1, int): [50000, 'm', 1, ''],\
 							("list_type_id", 1, int): [None, 'l', 1, 'Gene list type. must be in table gene_list_type beforehand.'],\
+							("output_fname", 0, ): [None, 'o', 1, ''],\
 							('commit', 0, int):[0, 'c', 0, 'commit db transaction'],\
 							('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
 							('report', 0, int):[0, 'r', 0, 'toggle report, more verbose stdout/stderr.']}
@@ -44,9 +76,38 @@ class GeneListRankTest(object):
 		"""
 		from pymodule import ProcessOptions
 		self.ad = ProcessOptions.process_function_arguments(keywords, self.option_default_dict, error_doc=self.__doc__, class_to_have_attr=self)
+		
+		results_method_id_ls = self.results_method_id_ls.split(',')
+		self.results_method_id_ls = map(int, results_method_id_ls)
+		self.results_method_id_ls.sort()
+			
+	def constructDataStruc(self, min_distance=50000):
+		"""
+		2008-07-16
+		"""
+		sys.stderr.write("Loading data structure from db ... \n")
+		offset_index = 0
+		block_size = 5000
+		rows = SnpsContext.query.offset(offset_index).limit(block_size)
+		data_struc = SnpsContextWrapper()
+		counter = 0
+		while rows.count()!=0:
+			for row in rows:
+				if row.disp_pos>=-min_distance and row.snp.end_position==None:	#it's a true SNP, not segment					
+					data_struc.addOneSnpsContext(row)
+					counter += 1
+				offset_index += 1
+			sys.stderr.write("%s%s\t%s"%('\x08'*40, offset_index, counter))
+			if self.debug and offset_index > 1000:
+				break
+			rows = SnpsContext.query.offset(offset_index).limit(block_size)
+		sys.stderr.write("Done.\n")
+		return data_struc
 	
 	def getChrPos2Pvalue(self, results_method_id):
 		"""
+		2008-07-16
+			deprecated
 		"""
 		sys.stderr.write("Getting chrpos2pvalue ...")
 		rm = ResultsMethod.get(results_method_id)
@@ -58,30 +119,30 @@ class GeneListRankTest(object):
 		sys.stderr.write("Done.\n")
 		return chrpos2pvalue
 	
-	def getGeneID2hit(self, chrpos2pvalue, min_distance=50000):
+	def getGeneID2hit(self, results_method_id, snps_context_wrapper):
+		"""
+		2008-07-16
+			reverse the order of 1st-read results file, 2nd-read db.snps_context
+		"""
 		sys.stderr.write("Getting gene_id2hit ... \n")
-		offset_index = 0
-		block_size = 5000
-		rows = SnpsContext.query.offset(offset_index).limit(block_size)
+		
+		rm = ResultsMethod.get(results_method_id)
+		genome_wide_result = GenomeBrowser.getGenomeWideResultFromFile(rm.filename, do_log10_transformation=True)
+		
 		gene_id2hit = {}
 		counter = 0
-		while rows.count()!=0:
-			for row in rows:
-				if row.disp_pos>=-min_distance and row.snp.end_position==None:	#it's a true SNP, not segment
-					chrpos_key = (row.snp.chromosome, row.snp.position)
-					pvalue = chrpos2pvalue.get(chrpos_key)
-					if pvalue is None:	#no pvalue
-						continue
-					passingdata = PassingData(chr=row.snp.chromosome, pos=row.snp.position, pvalue=pvalue, snps_id=row.snp.id)
-					if row.gene_id not in gene_id2hit:
-						gene_id2hit[row.gene_id] = passingdata
-					elif pvalue>gene_id2hit[row.gene_id].pvalue:	#pvalue is -log()
-						gene_id2hit[row.gene_id] = passingdata
-					counter += 1
-				offset_index += 1
-			sys.stderr.write("%s%s\t%s"%('\x08'*40, offset_index, counter))
-			rows = SnpsContext.query.offset(offset_index).limit(block_size)
-		
+		for data_obj in genome_wide_result.data_obj_ls:
+			pvalue = data_obj.value
+			snps_context_matrix = snps_context_wrapper.returnGeneLs(data_obj.chromosome, data_obj.position)
+			for snps_context in snps_context_matrix:
+				snps_id, disp_pos, gene_id = snps_context
+				passingdata = PassingData(chr=data_obj.chromosome, pos=data_obj.position, pvalue=pvalue, disp_pos=disp_pos, snps_id=snps_id)
+				if gene_id not in gene_id2hit:
+					gene_id2hit[gene_id] = passingdata
+				elif pvalue>gene_id2hit[gene_id].pvalue:	#pvalue is -log()
+					gene_id2hit[gene_id] = passingdata
+			counter += 1
+			#sys.stderr.write("%s%s\t%s"%('\x08'*40, offset_index, counter))		
 		sys.stderr.write("Done.\n")
 		return gene_id2hit
 	
@@ -119,18 +180,48 @@ class GeneListRankTest(object):
 								non_candidate_gene_ls=non_candidate_gene_ls, non_candidate_gene_pvalue_list=non_candidate_gene_pvalue_list)
 		return passingdata
 	
+	def output_gene_id2hit(self, gene_id2hit, output_fname):
+		sys.stderr.write("Outputting gene_id2hit ... ")
+		
+		writer = csv.writer(open(output_fname, 'w'), delimiter='\t')
+		writer.writerow(['gene-id', 'chromosome', 'position', 'snps-id', 'disp_pos', 'pvalue', 'rank'])
+		gene_id_ls = gene_id2hit.keys()
+		gene_id_ls.sort()
+		pvalue_ls = [-gene_id2hit[gene_id].pvalue for gene_id in gene_id_ls]
+		rank_ls = rpy.r.rank(pvalue_ls)
+		for i in range(len(gene_id_ls)):
+			gene_id = gene_id_ls[i]
+			rank = rank_ls[i]
+			hit = gene_id2hit[gene_id]
+			one_row = [gene_id, hit.chr, hit.pos, hit.snps_id, hit.disp_pos, hit.pvalue, rank]
+			writer.writerow(one_row)
+		del writer
+		sys.stderr.write("Done.\n")
+	
 	def run(self):
 		db = Stock_250kDB(drivername=self.drivername, username=self.db_user,
 				   password=self.db_passwd, hostname=self.hostname, database=self.dbname, schema=self.schema)
-		chrpos2pvalue = self.getChrPos2Pvalue(self.results_method_id)
-		gene_id2hit = self.getGeneID2hit(chrpos2pvalue, self.min_distance)
-		candidate_gene_list = self.getGeneList(self.list_type_id)
-		passingdata = self.prepareDataForRankTest(candidate_gene_list, gene_id2hit)
-		import rpy
-		w_result = rpy.r.wilcox_test(passingdata.candidate_gene_pvalue_list, passingdata.non_candidate_gene_pvalue_list, conf_int=rpy.r.TRUE)
-		print w_result['p.value']
-		print w_result['statistic']
-		print passingdata.candidate_gene_pvalue_list
+		#chrpos2pvalue = self.getChrPos2Pvalue(self.results_method_id)
+		#gene_id2hit = self.getGeneID2hit(chrpos2pvalue, self.min_distance)
+		snps_context_wrapper = self.constructDataStruc(self.min_distance)
+		if getattr(self, 'output_fname', None):
+			writer = csv.writer(open(self.output_fname, 'w'), delimiter='\t')
+			writer.writerow(['results_method_id', 'wilcox.test.pvalue', 'statistic'])
+		else:
+			writer = None
+		for results_method_id in self.results_method_id_ls:
+			gene_id2hit = self.getGeneID2hit(results_method_id, snps_context_wrapper)
+			#if getattr(self, 'output_fname', None):
+			#	self.output_gene_id2hit(gene_id2hit, self.output_fname)
+			candidate_gene_list = self.getGeneList(self.list_type_id)
+			passingdata = self.prepareDataForRankTest(candidate_gene_list, gene_id2hit)
+			
+			w_result = rpy.r.wilcox_test(passingdata.candidate_gene_pvalue_list, passingdata.non_candidate_gene_pvalue_list, conf_int=rpy.r.TRUE)
+			row = [ results_method_id, w_result['p.value'], w_result['statistic']]
+			print row
+			if writer:
+				writer.writerow(row)
+		#print passingdata.candidate_gene_pvalue_list
 		
 if __name__ == '__main__':
 	from pymodule import ProcessOptions
