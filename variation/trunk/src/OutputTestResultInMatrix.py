d45b5c649f0bd944cfea45405ce768ea48958043
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-08-23 09:27:33 +0000

    add option call_method_id_ls and analysis_method_id_ls
    merge legend and matrix into one figure
    
    git-svn-id: file:///usr/local/svn/repos@1020 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/OutputTestResultInMatrix.py
+++ variation/trunk/src/OutputTestResultInMatrix.py
@@ -4,8 +4,10 @@
 Examples:
 	OutputTestResultInMatrix.py  -l 6 -o /tmp/rank_test_call_method_6_m40000.tsv -m 40000
 	
-	OutputTestResultInMatrix.py  -l 6 -o /tmp/rank_test_call_method_6_a.tsv -a
-
+	OutputTestResultInMatrix.py  -l 6,7 -o /tmp/rank_test_call_method_6_7_a.tsv -a
+	
+	#check TopSNP test results
+	OutputTestResultInMatrix.py  -l 17 -x /tmp/top_snp_test_call_method_17_g_f200.png -g  -y 2 -f 200
 Description:
 	Output Test result in data matrix. either CandidateGeneRankSumTestResult or CandidateGeneTopSNPTest
 	
@@ -18,12 +20,12 @@ sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
 import getopt, csv, math
 import Numeric, cPickle
-from pymodule import PassingData, importNumericArray, write_data_matrix
+from pymodule import PassingData, importNumericArray, write_data_matrix, SNPData
 from TopSNPTest import TopSNPTest
 from Stock_250kDB import Stock_250kDB, Snps, SnpsContext, ResultsMethod, GeneList, GeneListType, \
 	CandidateGeneTopSNPTest, CandidateGeneRankSumTestResult, AnalysisMethod, PhenotypeMethod
 from sets import Set
-from pymodule.DrawMatrix import drawMatrix, drawLegend, drawContinousLegend, get_font
+from pymodule.DrawMatrix import drawMatrix, drawLegend, drawContinousLegend, get_font, combineTwoImages
 
 num = importNumericArray()
 
@@ -41,12 +43,13 @@ class OutputTestResultInMatrix(object):
 							('max_pvalue_per_gene', 0, int): [0, 'a', 0, 'take the most significant among all SNPs associated with one gene'],\
 							('min_sample_size', 0, int): [5, 'i', 1, 'minimum size for both candidate and non-candidate sets to do wilcox.test'],\
 							('no_of_top_snps', 1, int): [100, 'f', 1, 'how many number of top snps based on score or -log10(pvalue).'],\
-							('call_method_id', 1, int):[None, 'l', 1, 'Restrict results based on this call_method. Default is no such restriction.'],\
+							('call_method_id_ls', 1, ):[None, 'l', 1, 'Restrict results based on these call_methods, coma-separated list of ids.'],\
+							('analysis_method_id_ls', 0, ):[None, 'j', 1, 'Restrict results based on these analysis, coma-separated. Default is no such restriction.'],\
 							("result_type", 1, int): [1, 'y', 1, 'Which test result to output. 1: CandidateGeneRankSumTestResult, 2: CandidateGeneTopSNPTest'],\
 							('font_path', 1, ):['/usr/share/fonts/truetype/freefont/FreeSerif.ttf', 'e', 1, 'path of the font used to draw labels'],\
 							('font_size', 1, int):[20, 's', 1, 'size of font, which determines the size of the whole figure.'],\
-							("output_fname", 0, ): [None, 'o', 1, 'Filename tot store data matrix'],\
-							("fig_fname_prefix", 1, ): [None, 'x', 1, 'File name prefix for the figure'],\
+							("output_fname", 0, ): [None, 'o', 1, 'Filename to store data matrix'],\
+							("fig_fname", 1, ): [None, 'x', 1, 'File name for the figure'],\
 							("no_of_ticks", 1, int): [5, 't', 1, 'Number of ticks on the legend'],\
 							('commit', 0, int):[0, 'c', 0, 'commit the db operation. this commit happens after every db operation, not wait till the end.'],\
 							('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
@@ -61,10 +64,10 @@ class OutputTestResultInMatrix(object):
 		from pymodule import ProcessOptions
 		self.ad = ProcessOptions.process_function_arguments(keywords, self.option_default_dict, error_doc=self.__doc__, class_to_have_attr=self)
 	
-	def getListTypeInfo(self, db, result_class, call_method_id, where_condition):
+	def getListTypeInfo(self, db, result_class,  where_condition):
 		sys.stderr.write("Getting list type info ...")
-		rows = db.metadata.bind.execute("select distinct c.list_type_id from %s r, %s c where r.id=c.results_method_id and r.call_method_id=%s and %s order by list_type_id"\
-								%(ResultsMethod.table.name, result_class.table.name, call_method_id, where_condition))
+		rows = db.metadata.bind.execute("select distinct c.list_type_id, g.biology_category_id from %s r, %s c, %s g where %s and g.id=c.list_type_id order by g.biology_category_id, list_type_id"\
+								%(ResultsMethod.table.name, result_class.table.name, GeneListType.table.name, where_condition))
 		list_type_id_ls = []
 		
 		for row in rows:
@@ -73,20 +76,20 @@ class OutputTestResultInMatrix(object):
 		return list_type_id_ls
 			
 	
-	def getAnalysisMethodInfo(self, db, result_class, call_method_id, where_condition):
+	def getAnalysisMethodInfo(self, db, result_class, where_condition):
 		sys.stderr.write("Getting analysis method info ...")
-		rows = db.metadata.bind.execute("select distinct r.analysis_method_id from %s r, %s c where r.id=c.results_method_id and r.call_method_id=%s and %s order by analysis_method_id"\
-								%(ResultsMethod.table.name, result_class.table.name, call_method_id, where_condition))
+		rows = db.metadata.bind.execute("select distinct r.analysis_method_id from %s r, %s c where %s order by analysis_method_id"\
+								%(ResultsMethod.table.name, result_class.table.name, where_condition))
 		analysis_method_id_ls = []
 		for row in rows:
 			analysis_method_id_ls.append(row.analysis_method_id)
 		sys.stderr.write("Done.\n")
 		return analysis_method_id_ls
 	
-	def getPhenotypeInfo(self, db, result_class, call_method_id, where_condition):
+	def getPhenotypeInfo(self, db, result_class,  where_condition):
 		sys.stderr.write("Getting phenotype method info ...")
-		rows = db.metadata.bind.execute("select distinct r.phenotype_method_id from %s r, %s c where r.id=c.results_method_id and r.call_method_id=%s and %s order by phenotype_method_id"\
-								%(ResultsMethod.table.name, result_class.table.name, call_method_id, where_condition))
+		rows = db.metadata.bind.execute("select distinct r.phenotype_method_id, p.biology_category_id from %s r, %s c, %s p where %s and p.id=r.phenotype_method_id order by p.biology_category_id, r.phenotype_method_id"\
+								%(ResultsMethod.table.name, result_class.table.name, PhenotypeMethod.table.name, where_condition))
 		phenotype_method_id_ls = []
 		phenotype_method_id2index = {}
 		phenotype_method_label_ls = []
@@ -114,7 +117,7 @@ class OutputTestResultInMatrix(object):
 				tup = (list_type_id, analysis_method_id)
 				list_type_id_analysis_method_id2index[tup] = len(list_type_id_analysis_method_id_ls)
 				list_type_id_analysis_method_id_ls.append(tup)
-				list_type_analysis_method_label_ls.append('%s_%s_%s'%(list_type_short_name, list_type_id, analysis_method_short_name))
+				list_type_analysis_method_label_ls.append('%s_%s_%s'%(analysis_method_short_name, list_type_short_name, list_type_id))
 		return_data = PassingData()
 		return_data.list_type_id_analysis_method_id_ls = list_type_id_analysis_method_id_ls
 		return_data.list_type_id_analysis_method_id2index = list_type_id_analysis_method_id2index
@@ -122,13 +125,13 @@ class OutputTestResultInMatrix(object):
 		sys.stderr.write("Done.\n")
 		return return_data
 	
-	def get_data_matrix(self, db, phenotype_info, list_type_analysis_method_info, result_class, call_method_id, where_condition):
+	def get_data_matrix(self, db, phenotype_info, list_type_analysis_method_info, result_class,  where_condition):
 		sys.stderr.write("Getting data matrix ...")
 		data_matrix = num.zeros([len(list_type_analysis_method_info.list_type_id_analysis_method_id2index), len(phenotype_info.phenotype_method_id2index)], num.float)
 		data_matrix[:] = -2
 		i = 0
-		rows = db.metadata.bind.execute("select r.analysis_method_id, r.phenotype_method_id, c.* from %s r, %s c where r.id=c.results_method_id and r.call_method_id=%s and %s order by analysis_method_id"\
-								%(ResultsMethod.table.name, result_class.table.name, call_method_id, where_condition))
+		rows = db.metadata.bind.execute("select r.analysis_method_id, r.phenotype_method_id, c.* from %s r, %s c where %s order by analysis_method_id"\
+								%(ResultsMethod.table.name, result_class.table.name, where_condition))
 		min_value = None
 		max_value = None
 		for row in rows:
@@ -174,8 +177,12 @@ class OutputTestResultInMatrix(object):
 		
 	
 	def run(self):
-		where_condition = "c.get_closest=%s and c.min_distance=%s and abs(c.min_MAF-%s)<0.00001"\
+		where_condition = "r.analysis_method_id is not null and r.id=c.results_method_id and c.get_closest=%s and c.min_distance=%s and abs(c.min_MAF-%s)<0.00001"\
 				%(self.get_closest, self.min_distance, self.min_MAF)	#the condition for min_MAF is tricky because of the floating precision.
+		if self.call_method_id_ls:
+			where_condition += " and r.call_method_id in (%s)"%self.call_method_id_ls
+		if self.analysis_method_id_ls:
+			where_condition += " and r.analysis_method_id in (%s)"%self.analysis_method_id_ls
 		if self.result_type==1:
 			result_class = CandidateGeneRankSumTestResult
 			where_condition += " and c.max_pvalue_per_gene=%s"%(self.max_pvalue_per_gene)
@@ -193,26 +200,30 @@ class OutputTestResultInMatrix(object):
 				   password=self.db_passwd, hostname=self.hostname, database=self.dbname, schema=self.schema)
 		session = db.session
 		
-		list_type_id_ls = self.getListTypeInfo(db, result_class, self.call_method_id, where_condition)
-		analysis_method_id_ls = self.getAnalysisMethodInfo(db, result_class, self.call_method_id, where_condition)
+		list_type_id_ls = self.getListTypeInfo(db, result_class, where_condition)
+		analysis_method_id_ls = self.getAnalysisMethodInfo(db, result_class, where_condition)
 		list_type_analysis_method_info = self.orderListTypeAnalysisMethodID(list_type_id_ls, analysis_method_id_ls)
-		phenotype_info = self.getPhenotypeInfo(db, result_class, self.call_method_id, where_condition)
-		rdata = self.get_data_matrix(db, phenotype_info, list_type_analysis_method_info, result_class, self.call_method_id, where_condition)
+		phenotype_info = self.getPhenotypeInfo(db, result_class, where_condition)
+		rdata = self.get_data_matrix(db, phenotype_info, list_type_analysis_method_info, result_class, where_condition)
 		
 		header = ['list_type_analysis_method', ''] + phenotype_info.phenotype_method_label_ls
 		strain_acc_list = list_type_analysis_method_info.list_type_analysis_method_label_ls
 		category_list = list_type_analysis_method_info.list_type_id_analysis_method_id_ls
+		if SNPData.isDataMatrixEmpty(rdata.data_matrix):
+			sys.stderr.write("Nothing fetched from database.\n")
+			sys.exit(3)
 		if self.output_fname:
 			write_data_matrix(rdata.data_matrix, self.output_fname, header, strain_acc_list, category_list)
 		
-		if self.fig_fname_prefix:
+		if self.fig_fname:
 			font = get_font(self.font_path, font_size=self.font_size)	#2008-08-01
 			value2color_func = lambda x: self.value2RGBcolor(x, rdata.min_value, rdata.max_value)
-			im = drawContinousLegend(rdata.min_value, rdata.max_value, self.no_of_ticks, value2color_func, font)
-			im.save('%s_legend.png'%self.fig_fname_prefix)
+			im_legend = drawContinousLegend(rdata.min_value, rdata.max_value, self.no_of_ticks, value2color_func, font)
+			#im.save('%s_legend.png'%self.fig_fname_prefix)
 			im = drawMatrix(rdata.data_matrix, value2color_func, list_type_analysis_method_info.list_type_analysis_method_label_ls,\
 						phenotype_info.phenotype_method_label_ls, with_grid=1, font=font)
-			im.save('%s.png'%self.fig_fname_prefix)
+			im = combineTwoImages(im, im_legend, font=font)
+			im.save(self.fig_fname)
 		
 if __name__ == '__main__':
 	from pymodule import ProcessOptions
