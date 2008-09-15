0d47630452f00f282e072751a4a4c8dba8682e23
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-09-15 04:27:04 +0000

    def return_seqinfoid_name_given_snpid(self, snpid): 	2008-09-12 	the 149 SNPs are processed in 4 plates by sequenom. The order is preserved in table snps. 	snps.id from 1-38 is plate 1. 39-75 is plate 2. 76-112 is plate 3. 113-149 is plate 4.
    def splitCalls_BySeq(self, session):
    	2008-09-12
    	identify a strain by (ecotypeid, plateid, wellid)
    	assign 4 seqinfoids if they all exist
    
    git-svn-id: file:///usr/local/svn/repos@1108 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/Calls_BySeq2Calls.py
+++ variation/trunk/src/Calls_BySeq2Calls.py
@@ -31,7 +31,7 @@ class Calls_BySeq2Calls(object):
 							('db_user', 1, ): [None, 'u', 1, 'database username', ],\
 							('db_passwd', 1, ): [None, 'p', 1, 'database password', ],\
 							('commit',0, int): [0, 'c', 0, 'commit db transaction'],\
-							('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
+							('debug', 0, int):[0, 'b', 0, 'toggle debug mode, test-run 5000 entries from calls_byseq.'],\
 							('report', 0, int):[0, 'r', 0, 'toggle report, more verbose stdout/stderr.']}
 	
 	pa_has_characters = re.compile(r'[a-zA-Z_]')
@@ -43,8 +43,26 @@ class Calls_BySeq2Calls(object):
 		from pymodule import ProcessOptions
 		ProcessOptions.process_function_arguments(keywords, self.option_default_dict, error_doc=self.__doc__, class_to_have_attr=self)
 	
+	def return_seqinfoid_name_given_snpid(self, snpid):
+		"""
+		2008-09-12
+			the 149 SNPs are processed in 4 plates by sequenom. The order is preserved in table snps.
+			snps.id from 1-38 is plate 1. 39-75 is plate 2. 76-112 is plate 3. 113-149 is plate 4.
+		"""
+		if snpid>=1 and snpid<39:
+			return 'seqinfoid1'
+		elif snpid>=39 and snpid<76:
+			return 'seqinfoid2'
+		elif snpid>=76 and snpid<113:
+			return 'seqinfoid3'
+		elif snpid>=113 and snpid<=149:
+			return 'seqinfoid4'
+	
 	def splitCalls_BySeq(self, session):
 		"""
+		2008-09-12
+			identify a strain by (ecotypeid, plateid, wellid)
+			assign 4 seqinfoids if they all exist
 		2008-08-11
 			when checking out Calls_BySeq, order by ecotypeid, plateid
 		2008-08-11
@@ -52,17 +70,37 @@ class Calls_BySeq2Calls(object):
 		sys.stderr.write("Splitting calls_byseq ....\n")
 		offset_index = 0
 		block_size = 5000
-		rows = Calls_BySeq.query.order_by(Calls_BySeq.ecotypeid).order_by(Calls_BySeq.plateid).offset(offset_index).limit(block_size)
+		ecotypeid_plateid_wellid2strain = {}
+		rows = Calls_BySeq.query.order_by(Calls_BySeq.ecotypeid).offset(offset_index).limit(block_size)
 		counter = 0
 		while rows.count()!=0:
 			for row in rows:
 				#search it first
-				strain = Strain.query.filter_by(ecotypeid=row.ecotypeid).filter_by(plateid=row.plateid).first()
-				if not strain:
+				strain_unique_key = (row.ecotypeid, row.plateid, row.wellid)
+				if strain_unique_key in ecotypeid_plateid_wellid2strain:
+					strain = ecotypeid_plateid_wellid2strain[strain_unique_key]
+				else:
 					strain = Strain(ecotypeid=row.ecotypeid, plateid=row.plateid, extractionid=row.extractionid, \
-								seqinfoid=row.seqinfoid, wellid=row.wellid, replicate=row.replicate)
-					session.save(strain)
-					session.flush()
+								wellid=row.wellid, replicate=row.replicate)
+					ecotypeid_plateid_wellid2strain[strain_unique_key] = strain	#add this to ecotypeid_plateid_wellid2strain
+				seqinfoid_name = self.return_seqinfoid_name_given_snpid(row.snpid)
+				seqinfoid = getattr(strain, seqinfoid_name, None)
+				if seqinfoid is None:
+					setattr(strain, seqinfoid_name, row.seqinfoid)
+				else:
+					if seqinfoid!=row.seqinfoid:
+						sys.stderr.write("Strain with ecotypeid=%s, plateid=%s, wellid=%s has two different seqinfoids in this SNP block: %s, %s(snpid=%s).\n"%\
+										(strain.ecotypeid, strain.plateid, strain.wellid, seqinfoid, row.seqinfoid, row.snpid))
+				session.save_or_update(strain)
+				"""
+				strain_in_db = Strain.query.filter_by(ecotypeid=row.ecotypeid).filter_by(plateid=row.plateid).filter_by(wellid=row.wellid).filter_by(extractionid=row.extractionid)
+				if strain_in_db.count()>1:
+					sys.stderr.write("Warning: has more than 1 strain with ecotypeid=%s, plateid=%s, wellid=%s, extractionid=%s.\n"%(row.ecotypeid, row.plateid, row.wellid, row.extractionid))
+				
+				strain = strain_in_db.first()
+				if not strain:
+				"""
+
 				call = row.call1
 				if call:
 					call = call.upper()
@@ -72,14 +110,26 @@ class Calls_BySeq2Calls(object):
 				call = number2nt[nt2number[call]]
 				calls = Calls(snpid=row.snpid, allele=call)
 				calls.strain = strain
-				session.save(calls)
+				session.save_or_update(calls)
 				counter += 1
 				offset_index += 1
 			sys.stderr.write("%s%s\t%s"%('\x08'*40, offset_index, counter))
 			if self.debug and offset_index > 1000:
 				break
-			rows = Calls_BySeq.query.order_by(Calls_BySeq.ecotypeid).order_by(Calls_BySeq.plateid).offset(offset_index).limit(block_size)
+			session.flush()	#flush now and then to avoid possible memory error due to too many objects residing in memory.
+			rows = Calls_BySeq.query.order_by(Calls_BySeq.ecotypeid).offset(offset_index).limit(block_size)
+		"""
+		#no need to explicityly save/flush them, it'll be all handled during 'commit' stage.
+		#save strains in ecotypeid order
+		strain_unique_key_ls = ecotypeid_plateid_wellid2strain.keys()
+		strain_unique_key_ls.sort()
+		for strain_unique_key in strain_unique_key_ls:
+			session.save(ecotypeid_plateid_wellid2strain[strain_unique_key])
 			session.flush()
+		#save calls
+		for calls in calls_ls:
+			session.save(calls)
+		"""
 		sys.stderr.write("Done.\n")
 		
 	def run(self):
@@ -87,18 +137,16 @@ class Calls_BySeq2Calls(object):
 		"""
 		db = StockDB(drivername=self.drivername, username=self.db_user,
 				   password=self.db_passwd, hostname=self.hostname, database=self.dbname, schema=self.schema)
+		db.setup()
 		session = db.session
-		if not self.debug:
-			session.begin()
+		session.begin()
 		self.splitCalls_BySeq(session)
 		
-		if not self.debug:
-			if self.commit:
-				session.flush()
-				session.commit()
-				session.clear()
-			else:	#default is also rollback(). to demonstrate good programming
-				session.rollback()
+		if self.commit:
+			session.commit()
+			session.clear()
+		else:	#default is also rollback(). to demonstrate good programming
+			session.rollback()
 
 if __name__ == '__main__':
 	from pymodule import ProcessOptions
