9b7492269681c7f7d58374c5cf424bece95ca93c
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-10-02 03:25:22 +0000

    set executable remove some debug thing
    git-svn-id: file:///usr/local/svn/repos@1189 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/examples/example_to_call_DrawSNPRegion.py
+++ variation/trunk/src/examples/example_to_call_DrawSNPRegion.py
@@ -8,15 +8,15 @@ sys.path.insert(0, os.path.expanduser('~/lib/python'))
 sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 from variation.src.DrawSNPRegion import DrawSNPRegion
 
-instance = DrawSNPRegion(db_user='yh', db_passwd='', hostname='papaya.usc.edu', database='stock_250k',debug=2, input_fname='/tmp/dumb', output_dir='/tmp')
+instance = DrawSNPRegion(db_user='yh', db_passwd='', hostname='papaya.usc.edu', database='stock_250k', input_fname='/tmp/dumb', output_dir='/tmp', debug=0)
 gene_annotation_picklef = '/Network/Data/250k/tmp-yh/at_gene_model_pickelf'
-#LD_info_picklef = '/Network/Data/250k/tmp-yh/call_method_17_LD_m0.2_n0.1_m40000'
-LD_info_picklef = None
-LD_fname = '/Network/Data/250k/tmp-yh/call_method_17_LD_m0.2.tsv'
+LD_info_picklef = '/Network/Data/250k/tmp-yh/call_method_17_LD_m0.2_n0.1_m40000'
+#LD_info_picklef = None
+LD_fname = '/Network/Data/250k/tmp-yh/call_method_17_LD_m0.2.tsv'	#optional
 phenotype_method_id = 1
 chromosome, start, stop  = 2, 9588685-40000, 9588685+40000
 output_fname = '/tmp/SNP_2_9588685'
 center_snp_position = 9588685
 instance.drawSNPRegion(gene_annotation_picklef, LD_info_picklef, phenotype_method_id, chromosome, start, stop, \
-		output_fname, center_snp_position=None, LD_fname=LD_fname,\
+		output_fname, center_snp_position=center_snp_position, LD_fname=LD_fname,\
                 which_LD_statistic=2, call_method_id=17, min_MAF=0.1)
