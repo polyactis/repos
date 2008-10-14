d9873ebe861e2965d778248bb91327452b48fa73
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-10-14 09:13:46 +0000

    add this
    class TestDrawMap(unittest.TestCase):
    	"""
    	2008-10-14
    		test to connect the matplotlib's map with another plot
    
    git-svn-id: file:///usr/local/svn/repos@1235 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/VariationTest.py
+++ variation/trunk/src/VariationTest.py
@@ -13,6 +13,7 @@ Examples:
 2007-04-17 2: Test_find_smallest_vertex_set_to_remove_all_edges
 2008-10-08 3: TestFetchSNPRegionPlot
 2008-10-08 4: TestGetEcotypeInfo
+2008-10-11 5: TestDrawMap
 """
 import sys, os, math
 bit_number = math.log(sys.maxint)/math.log(2)
@@ -123,6 +124,152 @@ class TestGetEcotypeInfo(unittest.TestCase):
 		pdb.set_trace()
 		getEcotypeInfo(db)
 
+class TestDrawMap(unittest.TestCase):
+	"""
+	2008-10-14
+		test to connect the matplotlib's map with another plot
+	"""
+	def setUp(self):
+		print
+	
+	def test_drawMap(self):
+		sys.stderr.write("Drawing graph on a map ...\n")
+		#import pdb
+		#pdb.set_trace()
+		import StockDB, Stock_250kDB	#StockDB has to be setup otherwise, StockDB.Ecotype.table is None in getEcotypeInfo()
+		hostname='papaya.usc.edu'
+		dbname='stock_250k'
+		db_user='yh'
+		db_passwd = ''
+		drivername='mysql'
+		schema = None
+		db = Stock_250kDB.Stock_250kDB(drivername=drivername, username=db_user,
+						password=db_passwd, hostname=hostname, database=dbname, schema=schema)
+		#doesn't matter which database to connect as far as StockDB is imported
+		#db = StockDB.StockDB(drivername=drivername, username=db_user,
+		#				password=db_passwd, hostname=hostname, database=dbname, schema=schema)
+		db.setup(create_tables=False)
+		from common import getEcotypeInfo
+		ecotype_info = getEcotypeInfo(db)
+			
+		from matplotlib.toolkits.basemap import Basemap
+		#from mpl_toolkits.basemap import Basemap
+		import pylab
+		from matplotlib import rcParams
+		rcParams['font.size'] = 6
+		rcParams['legend.fontsize'] = 6
+		#rcParams['text.fontsize'] = 6	#deprecated. use font.size instead
+		rcParams['axes.labelsize'] = 4
+		rcParams['axes.titlesize'] = 8
+		rcParams['xtick.labelsize'] = 4
+		rcParams['ytick.labelsize'] = 4
+		
+		pylab.clf()
+		
+		#fig = pylab.figure()
+		#fig.add_axes([0.05,0.05,0.9,0.9])	#[left, bottom, width, height]
+		axe_map = pylab.axes([0.5, 0.02, 0.4, 0.8], frameon=False)
+		axe_map.set_title("Global Arabidopsis Ecotype Distribution")
+		axe_map.set_xlabel('ecotype is drawn as circles.')
+		axe_map.set_ylabel('strains')
+		pic_area=[-140,-40,140,70]
+		m = Basemap(llcrnrlon=pic_area[0],llcrnrlat=pic_area[1],urcrnrlon=pic_area[2],urcrnrlat=pic_area[3],\
+		resolution='l',projection='mill', ax=axe_map)
+		"""
+		llcrnrx = -self.rmajor
+		llcrnry = -self.rmajor
+		urcrnrx = -llcrnrx
+		urcrnry = -llcrnry
+		"""
+		#m.drawcoastlines()
+		#m.bluemarble()
+		m.drawparallels(pylab.arange(-90,90,30), labels=[1,1,0,1], size=4, linewidth=0.1)
+		m.drawmeridians(pylab.arange(-180,180,30), labels=[1,1,0,1], size=4, linewidth=0.1)
+		m.fillcontinents()
+		m.drawcountries(linewidth=0.1)
+		#m.drawstates()
+		#m.drawlsmask((0,255,0,255), (0,0,255,255), lakes=True)
+		#m.drawlsmask('coral','aqua',lakes=True)
+
+		print "xlim:", axe_map.get_xlim()
+		print "ylim:", axe_map.get_ylim()
+		
+		xlim = axe_map.get_xlim()
+		ylim = axe_map.get_ylim()
+		
+		"""
+		for strain_id in StrainID2PCAPosInfo.strain_id_ls:
+			img_y_pos = StrainID2PCAPosInfo.strain_id2img_y_pos[strain_id]
+			phenotype_row_index = phenData.row_id2row_index[strain_id]
+			phenotype = phenData.data_matrix[phenotype_row_index][phenotype_col_index]
+			ecotype_id = int(strain_id)
+			ecotype_obj = ecotype_info.ecotype_id2ecotype_obj.get(ecotype_id)
+			if ecotype_obj:
+				lat, lon = ecotype_obj.latitude, ecotype_obj.longitude
+			else:
+				sys.stderr.write("Warning: Ecotype %s not in ecotype_info (fetched from stock db).\n"%ecotype_id)
+				continue
+			if lat and lon:
+				x, y = m(lon, lat)
+				color = cmap(norm(phenotype))
+				ax.plot([0, x], [img_y_pos, y], linestyle='--', alpha=0.2, linewidth=0.2)
+				ax.scatter([x],[y], s=10, linewidth=0, facecolor=color)	#, zorder=10)
+		"""
+		
+		#pylab.title("Global Arabidopsis Ecotype Distribution")
+		output_fname_prefix = '/tmp/map'
+		
+		print "ylim:", axe_map.get_ylim()
+		axe_map.set_xlim(xlim)
+		axe_map.set_ylim(ylim)
+		
+		
+		axe_chromosome = pylab.axes([0.05, 0.02, 0.8, 0.8], frameon=False)
+		axe_chromosome.set_title("chromosome")
+		
+		#fix the two transformations before doing cross-axe drawings
+		axe_map.transData.freeze()  # eval the lazy objects
+		axe_map.transAxes.freeze()
+		axe_chromosome.transData.freeze()  # eval the lazy objects
+		axe_chromosome.transAxes.freeze()
+		no_of_ecotypes = 200
+		ecotype_id_ls = ecotype_info.ecotype_id2ecotype_obj.keys()[:no_of_ecotypes]
+		no_of_ecotypes_drawn = 0
+		for i in range(no_of_ecotypes):
+			ecotype_id = ecotype_id_ls[i]
+			y_pos = i/float(no_of_ecotypes)
+			#y_pos = i/float(no_of_ecotypes)*ylim[1]
+			ecotype_obj = ecotype_info.ecotype_id2ecotype_obj.get(ecotype_id)
+			if ecotype_obj:
+				lat, lon = ecotype_obj.latitude, ecotype_obj.longitude
+			else:
+				sys.stderr.write("Warning: Ecotype %s not in ecotype_info (fetched from stock db).\n"%ecotype_id)
+				continue
+			if lat and lon:
+				x, y = m(lon, lat)
+				#axe_map.plot([0, x], [y_pos, y], linestyle='--', alpha=0.2, linewidth=0.2)
+				axe_map.set_xlim(xlim)
+				axe_map.set_ylim(ylim)
+				axe_map.scatter([x],[y], s=5, linewidth=0, facecolor='r', alpha=0.2, zorder=10)
+				canvas_x, canvas_y = axe_map.transData.xy_tup((x,y))
+				axe_chromosome_xy = axe_chromosome.transData.inverse_xy_tup((canvas_x,canvas_y))
+				axe_chromosome.plot([0,axe_chromosome_xy[0]], [y_pos, axe_chromosome_xy[1]], linestyle='--', alpha=0.2, linewidth=0.2)
+				no_of_ecotypes_drawn += 1
+		#release two transformations
+		axe_map.transData.thaw()  # eval the lazy objects
+		axe_map.transAxes.thaw()
+		axe_chromosome.transData.thaw()  # eval the lazy objects
+		axe_chromosome.transAxes.thaw()
+		#set to the same x/y_lim before cross-axe drawing
+		axe_map.set_xlim(xlim)
+		axe_map.set_ylim(ylim)
+		axe_chromosome.set_xlim([0,1])
+		axe_chromosome.set_ylim([0,1])
+		if output_fname_prefix:
+			pylab.savefig('%s.png'%output_fname_prefix, dpi=600)
+			pylab.savefig('%s.svg'%output_fname_prefix)
+		sys.stderr.write("%s ecotypes drawn. Done.\n"%(no_of_ecotypes_drawn))
+
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
 		print __doc__
@@ -138,7 +285,8 @@ if __name__ == '__main__':
 	TestCaseDict = {1:TestTrioInference,
 		2:Test_find_smallest_vertex_set_to_remove_all_edges,
 		3:TestFetchSNPRegionPlot,
-		4:TestGetEcotypeInfo}
+		4:TestGetEcotypeInfo,
+		5:TestDrawMap}
 	type = 0
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
