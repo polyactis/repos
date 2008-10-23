63ff969f1774c1b43cacfa797439e68e38391780
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-10-23 10:55:52 +0000

    add two more criteria to filter
    		('min_margarita_rank',0, int): [50, '', 1, 'Minimum rank of a margarita score to be considered significant'],\
    		('min_rf_rank',0, int): [25, '', 1, 'Minimum rank of a RandomForest score to be considered significant'],\
    
    git-svn-id: file:///usr/local/svn/repos@1274 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/ValidateSNPRegion.py
+++ variation/trunk/src/ValidateSNPRegion.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """  
 Examples:
-	ValidateSNPRegion.py -i Desktop/ftRegions.csv -o /Network/Data/250k/tmp-yh/ftRegions_sig.tsv -u yh -p yh324
+	ValidateSNPRegion.py -i Desktop/ftRegions.csv -o /Network/Data/250k/tmp-yh/ftRegions_sig.tsv -j /Network/Data/250k/tmp-yh/at_gene_model_pickelf -D /Network/Data/250k/tmp-yh/call_method_17_LD_pickle_dummy_n0.1_m20000 -u yh -p yh324
 
 Description:
 	2008-10-02
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.expanduser('~/lib/python'))
 sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 import matplotlib; matplotlib.use("Agg")	#to avoid popup and collapse in X11-disabled environment
 
-from variation.src.DrawSNPRegion import SNPPassingData
+from DrawSNPRegion import SNPPassingData
 from pymodule import PassingData, figureOutDelimiter
 from sets import Set
 
@@ -43,6 +43,8 @@ class ValidateSNPRegion(object):
 							("which_LD_statistic", 1, int): [2, 'w', 1, 'which LD_statistic to plot, 1=r2, 2=|D_prime|, 3=|D|'],\
 							("LD_info_picklef", 0, ): ['/Network/Data/250k/tmp-yh/call_method_17_LD_pickle_dummy_n0.1_m20000', 'D', 1, 'given the option, If the file does not exist yet, store a pickled LD_info into it (min_MAF and min_distance will be attached to the filename). If the file exists, load LD_info out of it.'],\
 							("gene_annotation_picklef", 0, ): ['/Network/Data/250k/tmp-yh/at_gene_model_pickelf', 'j', 1, 'given the option, If the file does not exist yet, store a pickled gene_annotation into it. If the file exists, load gene_annotation out of it.'],\
+							('min_margarita_rank',0, int): [50, '', 1, 'Minimum rank of a margarita score to be considered significant'],\
+							('min_rf_rank',0, int): [25, '', 1, 'Minimum rank of a RandomForest score to be considered significant'],\
 							('commit', 0, int):[0, 'c', 0, 'commit the db operation. this commit happens after every db operation, not wait till the end.'],\
 							('debug', 0, int):[0, 'b', 1, 'debug mode. 1=level 1 (pdb mode). 2=level 2 (same as 1 except no pdb mode)'],\
 							('report', 0, int):[0, 'r', 0, 'toggle report, more verbose stdout/stderr.']}
@@ -63,7 +65,7 @@ class ValidateSNPRegion(object):
 		"""
 		if self.report:
 			sys.stderr.write("Get SNPs in this region ...")
-		from variation.src.DrawSNPRegion import SNPPassingData
+		from DrawSNPRegion import SNPPassingData
 		chr_pos_ls = []
 		chr_pos2adjacent_window = {}
 		j = 0
@@ -91,8 +93,7 @@ class ValidateSNPRegion(object):
 		return snp_region
 	
 	def get_phenotype_id_ls_given_biology_category_id(self, biology_category_id):
-		from variation.src import Stock_250kDB
-		
+		import Stock_250kDB
 		rows = Stock_250kDB.PhenotypeMethod.query.filter_by(biology_category_id=biology_category_id)
 		phenotype_id_ls = []
 		for row in rows:
@@ -100,14 +101,22 @@ class ValidateSNPRegion(object):
 		return phenotype_id_ls
 	
 	
-	def checkIfRegionSignificant(self, DrawSNPRegion_instance, snp_region, snp_info, analysis_method_id2gwr, value_criteria={1:8., 7:6.}):
+	def checkIfRegionSignificant(self, DrawSNPRegion_instance, snp_region, snp_info, analysis_method_id2gwr, value_criteria):
 		is_significant = 0
-		analysis_method_id_set = Set(value_criteria.keys())
+		#analysis_method_id_set = Set(value_criteria.keys())
 		for chr_pos in snp_region.chr_pos_ls:
-			for analysis_method_id in analysis_method_id_set:
+			for analysis_method_id, criteria_type in value_criteria:
+				criteria_value = value_criteria[(analysis_method_id, criteria_type)]
 				gwr = analysis_method_id2gwr[analysis_method_id]
 				data_obj = gwr.get_data_obj_by_chr_pos(chr_pos[0], chr_pos[1])
-				if data_obj and data_obj.value>=value_criteria[analysis_method_id]:
+				if not data_obj:
+					continue
+				if criteria_type=='rank':
+					data_obj_at_this_rank = gwr.get_data_obj_at_given_rank(criteria_value)
+					if data_obj>=data_obj_at_this_rank:
+						is_significant = 1
+						break
+				elif data_obj.value>=criteria_value:
 					is_significant = 1
 					break
 			if is_significant==1:	#break if already found significant
@@ -137,20 +146,20 @@ class ValidateSNPRegion(object):
 		sys.stderr.write("Done.\n")
 		return snp_region_ls
 	
-	def checkRegions(self, DrawSNPRegion_instance, snp_info, snp_region_ls, output_fname, value_criteria):
+	def checkRegions(self, DrawSNPRegion_instance, snp_info, snp_region_ls, output_fname, value_criteria, biology_category_id=1):
 		sys.stderr.write("Checking regions ...")
 		outf = open(output_fname, 'w')
 		writer = csv.writer(outf, delimiter='\t')
 		header = ['phenotype_id', 'chromosome', 'start', 'stop']
 		writer.writerow(header)
 		
-		ft_phenotype_id_ls = self.get_phenotype_id_ls_given_biology_category_id(1)
+		ft_phenotype_id_ls = self.get_phenotype_id_ls_given_biology_category_id(biology_category_id)
 		ft_phenotype_id_ls.sort()
-		
+		analysis_method_id_ls = [row[0] for row in value_criteria.keys()]
 		for phenotype_id in ft_phenotype_id_ls:
 			analysis_method_id2gwr = DrawSNPRegion_instance.getSimilarGWResultsGivenResultsByGene(phenotype_id, call_method_id=self.call_method_id, \
 																							results_directory=self.results_directory, \
-																							analysis_method_id_ls=value_criteria.keys())
+																							analysis_method_id_ls=analysis_method_id_ls)
 			if not analysis_method_id2gwr:	#no gwr results, skip
 				continue
 			for snp_region in snp_region_ls:
@@ -166,13 +175,13 @@ class ValidateSNPRegion(object):
 			import pdb
 			pdb.set_trace()
 		
-		from variation.src.DrawSNPRegion import DrawSNPRegion
+		from DrawSNPRegion import DrawSNPRegion
 		DrawSNPRegion_instance = DrawSNPRegion(db_user=self.db_user, db_passwd=self.db_passwd, hostname=self.hostname, \
 											database=self.dbname, input_fname='/tmp/dumb', output_dir='/tmp', debug=0)
 		
 		grand_dataStructure = DrawSNPRegion_instance.loadDataStructure(self.gene_annotation_picklef, self.LD_info_picklef, self.LD_fname, min_MAF=self.min_MAF, min_distance=20000, list_type_id=None)		
 		snp_region_ls = self.get_snp_region_ls(self.input_fname, grand_dataStructure.snp_info, self.min_distance)
-		value_criteria={1:8., 7:6.}	#minimum threshold for different analysis methods
+		value_criteria={(1, 'value'):8., (7, 'value'):6., (5, 'rank'):self.min_margarita_rank,(6, 'rank'):self.min_rf_rank}	#minimum threshold for different analysis methods
 		self.checkRegions(DrawSNPRegion_instance, grand_dataStructure.snp_info, snp_region_ls, self.output_fname, value_criteria)
 		
 
