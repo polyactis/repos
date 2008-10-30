1264d15c24566862f507c482adc88d7f3322dc25
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-10-30 07:49:08 +0000

    def getPhenotypeInfo(affiliated_table_name=None, extra_condition=None, extra_tables=None): 	""" 	2008-10-30 		affiliated_table_name becomes optional
    def getListTypeInfo(affiliated_table_name=None, extra_condition=None, extra_tables=None):
    	"""
    	2008-10-30
    		affiliated_table_name becomes optional
    
    git-svn-id: file:///usr/local/svn/repos@1299 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/web_interface/helloworld/helloworld/lib/helpers.py
+++ variation/trunk/web_interface/helloworld/helloworld/lib/helpers.py
@@ -7,8 +7,10 @@ from webhelpers import *
 import os,sys
 sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 import helloworld.model as model
+
 from variation.src.DrawSNPRegion import DrawSNPRegion
 from pymodule import PassingData
+from sets import Set
 
 def returnGeneDescLs(gene_annotation, gene_id_ls=[]):
 	DrawSNPRegion_ins = DrawSNPRegion(db_user=model.db_user, db_passwd=model.db_passwd, hostname=model.hostname, database=model.dbname,\
@@ -27,23 +29,30 @@ def returnGeneDescLs(gene_annotation, gene_id_ls=[]):
 				matrix_of_gene_descriptions.append(gene_desc_ls)
 	return matrix_of_gene_descriptions
 
-def getPhenotypeInfo(affiliated_table_name, extra_condition=None, extra_tables=None):
+def getPhenotypeInfo(affiliated_table_name=None, extra_condition=None, extra_tables=None):
 	"""
+	2008-10-30
+		affiliated_table_name becomes optional
 	2008-10-19
 		add option extra_tables
 	2008-10-16
 		sort phenotype by biology_category_id and return other info as well
 	"""
-	table_str = '%s s, %s p'%(affiliated_table_name, model.Stock_250kDB.PhenotypeMethod.table.name)
+	if affiliated_table_name:
+		table_str = '%s s, %s p'%(affiliated_table_name, model.Stock_250kDB.PhenotypeMethod.table.name)
+		where_condition = ['p.id=s.phenotype_method_id']
+	else:
+		table_str = '%s p'%(model.Stock_250kDB.PhenotypeMethod.table.name)
+		where_condition = []
+	
 	if extra_tables:
 		table_str += ', %s'%extra_tables
-	where_condition = 'p.id=s.phenotype_method_id'
 	if extra_condition:
-		where_condition += ' and %s'%extra_condition
+		where_condition.append(extra_condition)
 	
 	rows = model.db.metadata.bind.execute("select distinct p.id, p.biology_category_id, p.short_name from %s\
 		where %s order by p.biology_category_id, p.id"\
-		%(table_str, where_condition))
+		%(table_str, ' and '.join(where_condition)))
 	phenotype_method_id_ls = []
 	phenotype_method_id2index = {}
 	phenotype_method_label_ls = []
@@ -68,23 +77,31 @@ def getPhenotypeInfo(affiliated_table_name, extra_condition=None, extra_tables=N
 	phenotype_info.phenotype_method_label_ls = phenotype_method_label_ls
 	return phenotype_info
 
-def getListTypeInfo(affiliated_table_name, extra_condition=None, extra_tables=None):
+def getListTypeInfo(affiliated_table_name=None, extra_condition=None, extra_tables=None):
 	"""
+	2008-10-30
+		affiliated_table_name becomes optional
 	2008-10-19
 		add option extra_tables
 	2008-10-16
 		sort gene list type by biology_category_id and return other info as well
 		add -1 as a separator into list_type_id_ls
 	"""
-	table_str = '%s s, %s p'%(affiliated_table_name, model.Stock_250kDB.GeneListType.table.name)
+	if affiliated_table_name:
+		table_str = '%s s, %s p'%(affiliated_table_name, model.Stock_250kDB.GeneListType.table.name)
+		where_condition = ['p.id=s.list_type_id']
+	else:
+		table_str = '%s p'%(model.Stock_250kDB.GeneListType.table.name)
+		where_condition = []
+	
 	if extra_tables:
 		table_str += ', %s'%extra_tables
-	where_condition = 'p.id=s.list_type_id'
+	
 	if extra_condition:
-		where_condition += ' and %s'%extra_condition
+		where_condition.append(extra_condition)
 	rows = model.db.metadata.bind.execute("select distinct p.id, p.biology_category_id, p.short_name from %s \
 		where %s order by p.biology_category_id, p.id"\
-		%(table_str, where_condition))
+		%(table_str, ' and '.join(where_condition)))
 	list_type_id_ls = []
 	list_type_id2index = {}
 	list_type_label_ls = []
@@ -112,14 +129,14 @@ def getAnalysisMethodInfo(affiliated_table_name, extra_condition=None, extra_tab
 	"""
 	2008-10-19
 	"""
-	table_str = '%s s, %s a'%(affiliated_table_name, model.Stock_250kDB.AnalysisMethod.table.name)
+	table_str = '%s s, %s p'%(affiliated_table_name, model.Stock_250kDB.AnalysisMethod.table.name)
 	if extra_tables:
 		table_str += ', %s'%extra_tables
-	where_condition = 'a.id=s.analysis_method_id'
+	where_condition = 'p.id=s.analysis_method_id'
 	if extra_condition:
 		where_condition += ' and %s'%extra_condition
-	rows = model.db.metadata.bind.execute("select distinct a.id, a.short_name from %s \
-		where %s order by a.id"\
+	rows = model.db.metadata.bind.execute("select distinct p.id, p.short_name from %s \
+		where %s order by p.id"\
 		%(table_str, where_condition))
 	id_ls = []
 	id2index = {}
