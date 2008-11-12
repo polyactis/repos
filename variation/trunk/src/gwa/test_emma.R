c62814b9d2bcef3d3174643b24ad3499849b3bf7
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-11-12 10:02:41 +0000

    run emma.REML.t() instead of emma.ML.LRT(). the latter gives correct pvalue.
    output_variance() calculate and report the percentage of genotype variance over total phenotype variance
    
    git-svn-id: file:///usr/local/svn/repos@1337 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/gwa/test_emma.R
+++ variation/trunk/src/gwa/test_emma.R
@@ -9,13 +9,13 @@ objects()
 
 #debug(emma.ML.LRT)
 
-rs <- emma.ML.LRT(emmadat$ys[1,], emmadat$xs,emmadat$K)
-#rs <- emma.REML.t(emmadat$ys[1,], emmadat$xs,emmadat$K)
+#rs <- emma.ML.LRT(emmadat$ys[1,], emmadat$xs,emmadat$K)
+rs <- emma.REML.t(emmadat$ys[1,], emmadat$xs,emmadat$K)
 #traceback()
 
-warnings()
+#warnings()
 
-str(rs)
+#str(rs)
 cat("rs$ML1s: ", rs$ML1s, "\n")
 cat("rs$ML0s: ", rs$ML0s, "\n")
 
@@ -24,11 +24,21 @@ output_variance = function(marker_index)
 {
 cat("pvalue=", rs$ps[marker_index], " stats=", rs$stats[marker_index],  " vgs=", rs$vgs[marker_index], " ves=", rs$ves[marker_index], " beta0_est=", rs$beta0_est[marker_index], " beta1_est=", rs$beta1_est[marker_index], " beta0_est1=", rs$beta0_est1[marker_index], " beta1_est1=", rs$beta1_est1[marker_index], "\n")
 no_of_individuals = length(emmadat$ys[1,])
+
+phenotype_var = var(emmadat$ys[1,])
+cat("variance of phenotype is:", phenotype_var, "\n")
+
 X0 <- matrix(1,no_of_individuals,1)
 beta = matrix(c( rs$beta0_est[marker_index], rs$beta1_est[marker_index]), 2,1)
 x_beta_est = cbind(X0, emmadat$xs[marker_index,]) %*% beta
 #cat("x_beta_est: ", x_beta_est, "\n")
-cat("variance of X*beta_est at min pvalue:", var(x_beta_est), "\n")
+x_beta_var = var(x_beta_est)
+cat("variance of X*beta_est at min pvalue:", x_beta_var, " percentage=", x_beta_var/phenotype_var, "=", rs$genotype_var_perc[marker_index], "\n")
+
+x_beta_est_alt = emmadat$xs[marker_index,]*rs$beta1_est[marker_index]
+cat("variance of X*beta_est (no X0) at min pvalue:", var(x_beta_est_alt), "\n")
+
+
 
 mu = emmadat$K %*% (emmadat$K + diag(rs$ves[marker_index], no_of_individuals)) %*% (emmadat$ys[1,] - x_beta_est)
 #cat("mu: ", mu, "\n")
