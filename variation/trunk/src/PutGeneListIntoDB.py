82d04876e1640ac58ad149d06d09e4da3b2fa625
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2008-12-11 10:35:18 +0000

    def putGeneListIntoDb(self, input_fname, list_type_id, list_type_name, gene_symbol2gene_id_set, db): 		""" 		2008-12-11 			more filtering: 				1. strip the original_name 				2. pick alphanumeric characters out of original_name 			if GeneListType is already in db. check if GeneList has this gene already or not.
    git-svn-id: file:///usr/local/svn/repos@1392 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/PutGeneListIntoDB.py
+++ variation/trunk/src/PutGeneListIntoDB.py
@@ -56,6 +56,11 @@ class PutGeneListIntoDb(object):
 		
 	def putGeneListIntoDb(self, input_fname, list_type_id, list_type_name, gene_symbol2gene_id_set, db):
 		"""
+		2008-12-11
+			more filtering:
+				1. strip the original_name
+				2. pick alphanumeric characters out of original_name
+			if GeneListType is already in db. check if GeneList has this gene already or not.
 		2008-11-20
 			use figureOutDelimiter() to get delimiter automatically
 		2008-07-15
@@ -71,13 +76,16 @@ class PutGeneListIntoDb(object):
 		success_counter = 0
 		import re
 		p_acc_ver = re.compile(r'(\w+)\.(\d+)')
+		p_acc = re.compile(r'(\w+)')	#2008-12-11 only alphanumeric characters in gene_symbol (suzi's file contains weird characters sometimes)
 		
 		gene_id2original_name = {}	#to avoid redundancy in gene list
 		for row in reader:
-			original_name = row[0]
-			gene_symbol = row[0].upper()
+			original_name = row[0].strip()	#2008-12-11 remove spaces/tabs in the beginning/end
+			gene_symbol = original_name.upper()
 			if p_acc_ver.search(gene_symbol):
 				gene_symbol, version = p_acc_ver.search(gene_symbol).groups()
+			if p_acc.search(gene_symbol):	#2008-12-11 pick out alphanumeric characters
+				gene_symbol, = p_acc.search(gene_symbol).groups()
 			gene_id_set = gene_symbol2gene_id_set.get(gene_symbol)
 			if gene_id_set==None:
 				sys.stderr.write("Linking to gene id failed for %s. No such gene_symbol, %s, in gene_symbol2gene_id_set.\n"%(original_name, gene_symbol))
@@ -107,6 +115,11 @@ class PutGeneListIntoDb(object):
 		session.save_or_update(glt)
 		
 		for gene_id, original_name in gene_id2original_name.iteritems():
+			if glt.id:	#2008-12-11 GeneListType is already in db. check if GeneList has this gene already or not.
+				rows = GeneList.query.filter_by(gene_id=gene_id).filter_by(list_type_id=glt.id)
+				if rows.count()>0:
+					sys.stderr.write("Gene: %s (%s) already with list type %s.\n"%(gene_id, original_name, glt.short_name))
+					continue
 			gl = GeneList(gene_id=gene_id, list_type=glt, original_name=original_name)
 			session.save(gl)
 		sys.stderr.write("%s/%s linked successfully.\n"%(success_counter, counter))
@@ -116,6 +129,9 @@ class PutGeneListIntoDb(object):
 		"""
 		2008-08-19
 		"""
+		if self.debug:
+			import pdb
+			pdb.set_trace()
 		db = Stock_250kDB(drivername=self.drivername, username=self.db_user,
 				   password=self.db_passwd, hostname=self.hostname, database=self.dbname, schema=self.schema)
 		db.setup(create_tables=False)
