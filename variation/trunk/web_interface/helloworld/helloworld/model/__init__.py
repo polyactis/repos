1e8267c8b9d42647d294838e37de6864d7863692
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2009-01-09 07:23:58 +0000

    2008-11-05 a dictionary to link two tables of types in order for cross-linking between pages of DisplayTopSNPTestRM and ScoreRankHistogram/DisplayResultsGene
    git-svn-id: file:///usr/local/svn/repos@1423 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/web_interface/helloworld/helloworld/model/__init__.py
+++ variation/trunk/web_interface/helloworld/helloworld/model/__init__.py
@@ -40,3 +40,18 @@ def dealWithGeneAnnotation():
 	return gene_annotation
 
 gene_annotation = dealWithGeneAnnotation()
+
+#2008-11-05 a dictionary to link two tables of types in order for cross-linking between pages of DisplayTopSNPTestRM and ScoreRankHistogram/DisplayResultsGene
+CandidateGeneTopSNPTestRMType_id_min_distance2ScoreRankHistogramType_id = {}
+ScoreRankHistogramType = Stock_250kDB.ScoreRankHistogramType
+CandidateGeneTopSNPTestRMType = Stock_250kDB.CandidateGeneTopSNPTestRMType
+
+rows = db.metadata.bind.execute("select s.id as sid, s.min_distance, c.id as cid from %s s, %s c where s.null_distribution_type_id=c.null_distribution_type_id and\
+				s.results_type=c.results_type and s.get_closest=c.get_closest and s.min_MAF=c.min_MAF and\
+				s.allow_two_sample_overlapping=c.allow_two_sample_overlapping and s.call_method_id=17"%\
+					(ScoreRankHistogramType.table.name, CandidateGeneTopSNPTestRMType.table.name))	#2008-1-8 temporarily set call_method_id=17 cuz CandidateGeneTopSNPTestRMType doesn't include call_method_id
+
+for row in rows:
+	key_tuple = (row.cid, row.min_distance)
+	CandidateGeneTopSNPTestRMType_id_min_distance2ScoreRankHistogramType_id[key_tuple] = row.sid
+	
