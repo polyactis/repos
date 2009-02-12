136c2bc2ac339e265f0b998d3b1775ba2eb89bbf
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2009-02-12 08:21:05 +0000

    def inputNodePrepare(self, snp_info=None): 		""" 		2009-2-11 			refactored out of run() 		""" def generate_params(self, gene_id_fname, pdata, block_size=1000): 		""" 		2009-2-12 			use yield make this function a generator
    git-svn-id: file:///usr/local/svn/repos@1488 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/MpiIntraGeneSNPPairAsso.py
+++ variation/trunk/src/MpiIntraGeneSNPPairAsso.py
@@ -128,7 +128,7 @@ class MpiIntraGeneSNPPairAsso(MPIwrapper):
 							('phenotype_fname', 1, ): [None, 'p', 1, 'phenotype file. Same format as input_fname. but replace the data matrix with phenotype data.', ],\
 							('min_data_point', 1, int): [3, 'm', 1, 'minimum number of ecotypes for either alleles of a single SNP to be eligible for kruskal wallis test'],\
 							('phenotype_method_id_ls', 0, ): [None, 'w', 1, 'which phenotypes to work on. a comma-separated list phenotype_method ids in the phenotype file. Check db Table phenotype_method. Default is to take all.',],\
-							('block_size', 1, int):[1000, 's', 1, 'Minimum number of tests each computing node is gonna handle. The computing node loops over all phenotypes, test all pairwise SNPs within a gene.'],\
+							('block_size', 1, int):[10000, 's', 1, '~Maximum number of tests each computing node is gonna handle. The computing node loops over all phenotypes, test all pairwise SNPs within a gene.'],\
 							('test_type', 1, int): [1, 'y', 1, 'Which type of test to do. 1:Kruskal_Wallis on a SNP boolean-merged from two SNPs, 2:linear model(y=b + SNP1 + SNP2 + SNP1xSNP2 + e)'],\
 							('gene_id_fname', 0, ): [None, 'g', 1, 'A file with gene id on each line.'],\
 							('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
@@ -144,6 +144,8 @@ class MpiIntraGeneSNPPairAsso(MPIwrapper):
 		if self.phenotype_method_id_ls is not None:
 			self.phenotype_method_id_ls = getListOutOfStr(self.phenotype_method_id_ls, data_type=int)
 	
+	test_type2test = {1: TwoSNPAssociation.KWOnBooleanSNP,
+					2: TwoSNPAssociation.IntLMOnTwoSNPs}
 	def get_gene_id2snps_id_ls(self, snps_context_wrapper):
 		"""
 		2009-2-8
@@ -179,6 +181,8 @@ class MpiIntraGeneSNPPairAsso(MPIwrapper):
 	
 	def generate_params(self, gene_id_fname, pdata, block_size=1000):
 		"""
+		2009-2-12
+			use yield make this function a generator
 		2009-2-9
 			add argument gene_id_fname to restrict analysis on genes from it
 		2008-09-09
@@ -186,8 +190,8 @@ class MpiIntraGeneSNPPairAsso(MPIwrapper):
 		2008-09-06
 			each node handles a certain number of genes. identified by the index of the 1st gene and the index of the last gene.
 		"""
-		sys.stderr.write("Generating parameters ...")
-		params_ls = []
+		#sys.stderr.write("Generating parameters ...")
+		#params_ls = []
 		no_of_phenotypes = len(pdata.phenotype_index_ls)
 		start_index = 0	#for each computing node: the index of gene >= start_index
 		no_of_tests_per_node = 0
@@ -210,18 +214,60 @@ class MpiIntraGeneSNPPairAsso(MPIwrapper):
 			est_no_of_tests = (n*(n-1)*5/2.0 + n)*no_of_phenotypes	#this is the upper bound for the number of tests for each gene on a computing node. data missing would make the number smaller.
 			no_of_tests_per_node += est_no_of_tests
 			if no_of_tests_per_node>=block_size:
-				params_ls.append((start_index, i+1))	#the computing node is gonna handle genes from pdata.gene_id_ls[start_index] to pdata.gene_id_ls[i]
+				yield (start_index, i+1)	#the computing node is gonna handle genes from pdata.gene_id_ls[start_index] to pdata.gene_id_ls[i]
 				#reset the starting pointer to the index of the next gene
 				start_index = i+1
 				no_of_tests_per_node = 0	#reset this to 0
 			elif i==no_of_genes-1:	#this is the last gene, have to include them
-				params_ls.append((start_index, i+1))
+				yield (start_index, i+1)
 				#no need to cleanup because this is the end of loop
-		sys.stderr.write("%s params. Done.\n"%(len(params_ls)))
-		return params_ls
+		#sys.stderr.write("%s params. Done.\n"%(len(params_ls)))
+		#return params_ls
 	
-	test_type2test = {1: TwoSNPAssociation.KWOnBooleanSNP,
-					2: TwoSNPAssociation.IntLMOnTwoSNPs}
+	def inputNodePrepare(self, snp_info=None):
+		"""
+		2009-2-11
+			refactored out of run()
+		"""
+		header, strain_acc_list, category_list, data_matrix = read_data(self.input_fname)
+		snpData = SNPData(header=header, strain_acc_list=strain_acc_list, \
+						data_matrix=data_matrix, turn_into_array=1)	#category_list is not used to facilitate row-id matching
+		
+		picklef = open(self.snps_context_fname)
+		snps_context_wrapper = cPickle.load(picklef)
+		del picklef
+		gene_id2snps_id_ls = self.get_gene_id2snps_id_ls(snps_context_wrapper)
+		del snps_context_wrapper
+		gene_id_ls = gene_id2snps_id_ls.keys()
+		gene_id_ls.sort()
+		
+		header_phen, strain_acc_list_phen, category_list_phen, data_matrix_phen = read_data(self.phenotype_fname, turn_into_integer=0)
+		phenData = SNPData(header=header_phen, strain_acc_list=strain_acc_list_phen, data_matrix=data_matrix_phen)
+		phenData.data_matrix = Kruskal_Wallis.get_phenotype_matrix_in_data_matrix_order(snpData.row_id_ls, phenData.row_id_ls, phenData.data_matrix)
+		
+		self.phenotype_index_ls = PlotGroupOfSNPs.findOutWhichPhenotypeColumn(phenData, Set(self.phenotype_method_id_ls))
+		
+		if not self.phenotype_index_ls:
+			self.phenotype_index_ls = range(len(phenData.col_id_ls))
+		pdata = PassingData(gene_id_ls=gene_id_ls, gene_id2snps_id_ls=gene_id2snps_id_ls, \
+						phenotype_index_ls=self.phenotype_index_ls, snp_info=snp_info)
+		params_ls = self.generate_params(self.gene_id_fname, pdata, self.block_size)
+		
+		other_data = PassingData(gene_id2snps_id_ls=gene_id2snps_id_ls, gene_id_ls=pdata.gene_id_ls, phenData=phenData, \
+								phenotype_index_ls=self.phenotype_index_ls, snp_info=snp_info)
+		other_data_pickle = cPickle.dumps(other_data, -1)
+		del other_data
+		
+		output_node_data = PassingData(phenotype_label_ls=phenData.col_id_ls, \
+								phenotype_index_ls=self.phenotype_index_ls)
+		output_node_data_pickle = cPickle.dumps(output_node_data, -1)
+		
+		snpData_pickle = cPickle.dumps(snpData, -1)
+		del snpData, data_matrix
+		return_data = PassingData(snpData_pickle=snpData_pickle, other_data_pickle=other_data_pickle,\
+								output_node_data_pickle=output_node_data_pickle, params_ls=params_ls)
+		return return_data
+	
 	def computing_node_handler(self, communicator, data, param_obj):
 		"""
 		2008-09-07
@@ -232,48 +278,37 @@ class MpiIntraGeneSNPPairAsso(MPIwrapper):
 		result_ls = []
 		snpData = param_obj.snpData
 		no_of_phenotypes = param_obj.phenData.data_matrix.shape[1]
-		for col1_range in data:
-			min_index1, stop1 = col1_range
-			for gene_id in param_obj.gene_id_ls[min_index1: stop1]:
-				snps_id_ls = param_obj.gene_id2snps_id_ls[gene_id]
-				no_of_snps = len(snps_id_ls)
-				for phenotype_index in param_obj.phenotype_index_ls:
-					if phenotype_index>=no_of_phenotypes:	#skip if out of bound
+		min_index1, stop1 = data
+		for gene_id in param_obj.gene_id_ls[min_index1: stop1]:
+			snps_id_ls = param_obj.gene_id2snps_id_ls[gene_id]
+			no_of_snps = len(snps_id_ls)
+			for phenotype_index in param_obj.phenotype_index_ls:
+				if phenotype_index>=no_of_phenotypes:	#skip if out of bound
+					continue
+				phenotype_ls = param_obj.phenData.data_matrix[:, phenotype_index]
+				for i in range(no_of_snps):
+					snp1_id = snps_id_ls[i]
+					#single SNP test
+					snp1_index = snpData.col_id2col_index.get(snp1_id)
+					if snp1_index is None:	#snp1_id not in input matrix
 						continue
-					phenotype_ls = param_obj.phenData.data_matrix[:, phenotype_index]
-					for i in range(no_of_snps):
-						snp1_id = snps_id_ls[i]
-						#single SNP test
-						snp1_index = snpData.col_id2col_index.get(snp1_id)
-						if snp1_index is None:	#snp1_id not in input matrix
+					"""
+					pdata = Kruskal_Wallis._kruskal_wallis(snpData.data_matrix[:,snp1_index]+1, phenotype_ls, param_obj.min_data_point)	#watch, +1 to avoid 0 being treated as NA in Kruskal_Wallis._kruskal_wallis
+					if pdata:
+						pdata = PassingData(snp1_id=snp1_id, gene1_id=gene_id, snp2_id=None, gene2_id=None, pvalue=pdata.pvalue, count1=pdata.count_ls[0],\
+										count2=pdata.count_ls[1], bool_type=0, phenotype_index=phenotype_index)	#single SNP
+						result_ls.append(pdata)
+					"""
+					for j in range(i+1, no_of_snps):
+						snp2_id = snps_id_ls[j]
+						snp2_index = snpData.col_id2col_index.get(snp2_id)
+						if snp2_index is None or snp2_index==snp1_index:	#snp2_id not in input matrix
 							continue
-						"""
-						pdata = Kruskal_Wallis._kruskal_wallis(snpData.data_matrix[:,snp1_index]+1, phenotype_ls, param_obj.min_data_point)	#watch, +1 to avoid 0 being treated as NA in Kruskal_Wallis._kruskal_wallis
-						if pdata:
-							pdata = PassingData(snp1_id=snp1_id, gene1_id=gene_id, snp2_id=None, gene2_id=None, pvalue=pdata.pvalue, count1=pdata.count_ls[0],\
-											count2=pdata.count_ls[1], bool_type=0, phenotype_index=phenotype_index)	#single SNP
-							result_ls.append(pdata)
-						"""
-						for j in range(i+1, no_of_snps):
-							snp2_id = snps_id_ls[j]
-							snp2_index = snpData.col_id2col_index.get(snp2_id)
-							if snp2_index is None or snp2_index==snp1_index:	#snp2_id not in input matrix
-								continue
-							pdata_ls = self.test_type2test[param_obj.test_type](snp1_id, gene_id, snpData.data_matrix[:,snp1_index], \
-																			snp2_id, None, snpData.data_matrix[:,snp2_index], \
-																			phenotype_index, phenotype_ls, \
-																			min_data_point=param_obj.min_data_point)
-							result_ls += pdata_ls
-							"""
-							for bool_type in bool_type2merge_oper:
-								merge_oper_matrix = bool_type2merge_oper[bool_type]
-								genotype_ls = self.mergeTwoGenotypeLs(snpData.data_matrix[:,snp1_index], snpData.data_matrix[:,snp2_index], merge_oper_matrix)
-								pdata = Kruskal_Wallis._kruskal_wallis(genotype_ls, phenotype_ls, param_obj.min_data_point)
-								if pdata:
-									pdata = PassingData(snp1_id=snp1_id, gene1_id=gene_id, snp2_id=snp2_id, gene2_id=None, pvalue=pdata.pvalue,\
-													count1=pdata.count_ls[0], count2=pdata.count_ls[1], bool_type=bool_type, phenotype_index=phenotype_index)
-									result_ls.append(pdata)
-							"""
+						pdata_ls = self.test_type2test[param_obj.test_type](snp1_id, gene_id, snpData.data_matrix[:,snp1_index], \
+																		snp2_id, None, snpData.data_matrix[:,snp2_index], \
+																		phenotype_index, phenotype_ls, \
+																		min_data_point=param_obj.min_data_point)
+						result_ls += pdata_ls
 		sys.stderr.write("Node no.%s done with %s results.\n"%(node_rank, len(result_ls)))
 		return result_ls
 	
@@ -348,51 +383,18 @@ class MpiIntraGeneSNPPairAsso(MPIwrapper):
 		output_node_rank = self.communicator.size-1
 		
 		if node_rank == 0:
-			header, strain_acc_list, category_list, data_matrix = read_data(self.input_fname)
-			snpData = SNPData(header=header, strain_acc_list=strain_acc_list, \
-							data_matrix=data_matrix, turn_into_array=1)	#category_list is not used to facilitate row-id matching
-			
-			picklef = open(self.snps_context_fname)
-			snps_context_wrapper = cPickle.load(picklef)
-			del picklef
-			gene_id2snps_id_ls = self.get_gene_id2snps_id_ls(snps_context_wrapper)
-			del snps_context_wrapper
-			gene_id_ls = gene_id2snps_id_ls.keys()
-			gene_id_ls.sort()
-			
-			header_phen, strain_acc_list_phen, category_list_phen, data_matrix_phen = read_data(self.phenotype_fname, turn_into_integer=0)
-			phenData = SNPData(header=header_phen, strain_acc_list=strain_acc_list_phen, data_matrix=data_matrix_phen)
-			phenData.data_matrix = Kruskal_Wallis.get_phenotype_matrix_in_data_matrix_order(snpData.row_id_ls, phenData.row_id_ls, phenData.data_matrix)
-			
-			self.phenotype_index_ls = PlotGroupOfSNPs.findOutWhichPhenotypeColumn(phenData, Set(self.phenotype_method_id_ls))
-		
-			if not self.phenotype_index_ls:
-				self.phenotype_index_ls = range(len(phenData.col_id_ls))
-			pdata = PassingData(gene_id_ls=gene_id_ls, gene_id2snps_id_ls=gene_id2snps_id_ls, phenotype_index_ls=self.phenotype_index_ls)
-			params_ls = self.generate_params(self.gene_id_fname, pdata, self.block_size)
-			
-			other_data = PassingData(gene_id2snps_id_ls=gene_id2snps_id_ls, gene_id_ls=pdata.gene_id_ls, phenData=phenData, \
-									phenotype_index_ls=self.phenotype_index_ls)
-			other_data_pickle = cPickle.dumps(other_data, -1)
-			
-			output_node_data = PassingData(phenotype_label_ls=phenData.col_id_ls, \
-									phenotype_index_ls=self.phenotype_index_ls)
-			output_node_data_pickle = cPickle.dumps(output_node_data, -1)
-			
-			snpData_pickle = cPickle.dumps(snpData, -1)
-			del snpData, data_matrix
-			
+			dstruc = self.inputNodePrepare()
+			params_ls = dstruc.params_ls
 			#send the output node the phenotype_label_ls
-			self.communicator.send(output_node_data_pickle, output_node_rank, 0)
-			del output_node_data_pickle
+			self.communicator.send(dstruc.output_node_data_pickle, output_node_rank, 0)
+			del dstruc.output_node_data_pickle
 			
 			for node in free_computing_nodes:	#send it to the computing_node
 				sys.stderr.write("passing initial data to nodes from %s to %s ... "%(node_rank, node))
-				self.communicator.send(snpData_pickle, node, 0)
-				self.communicator.send(other_data_pickle, node, 0)
+				self.communicator.send(dstruc.snpData_pickle, node, 0)
+				self.communicator.send(dstruc.other_data_pickle, node, 0)
 				sys.stderr.write(".\n")
-			del snpData_pickle, other_data_pickle
-			del other_data
+			del dstruc
 			
 		elif node_rank in free_computing_node_set:
 			data, source, tag = self.communicator.receiveString(0, 0)
@@ -411,8 +413,8 @@ class MpiIntraGeneSNPPairAsso(MPIwrapper):
 		self.synchronize()
 		if node_rank == 0:
 			param_obj = PassingData(params_ls=params_ls, output_node_rank=output_node_rank, report=self.report, counter=0)
-			self.input_node(param_obj, free_computing_nodes, input_handler=self.input_fetch_handler, message_size=1)
-			#self.input_node(param_obj, free_computing_nodes, self.message_size)
+			self.inputNode(param_obj, free_computing_nodes, param_generator = params_ls)
+			#self.input_node(param_obj, free_computing_nodes, input_handler=self.input_fetch_handler, message_size=1)
 		elif node_rank in free_computing_node_set:
 			computing_parameter_obj = PassingData(snpData=snpData, gene_id_ls=other_data.gene_id_ls, \
 												gene_id2snps_id_ls=other_data.gene_id2snps_id_ls, phenData=other_data.phenData,
