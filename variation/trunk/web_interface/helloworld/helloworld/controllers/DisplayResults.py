89682204f1d143773b05e25be4e3ff48c2fc4f32
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2009-02-22 08:29:24 +0000

    def getCallMethodEigenValue(self): 		""" 		2009-2-2 			get an eigen value given a call_method_id 		"""
    	def getCallInfoData(self, call_method_id, phenotype_method_id):
    		"""
    		2009-2-2
    			get ecotype id, name, latitude, longitude, pc1, pc2, phenotype for all call_infos in one call_method_id
    
    	def fetchCallInfoData(self):
    		"""
    		2009-2-2
    			get a complete catalog of call information given a call_method_id
    			including, ecotypeid, name, latitude, longitude, phenotype value, PC1, PC2
    		"""
    
        def getPhenotypeHistImage(self, id=None, img_type='hist_thumb'):
        def getCallPhenotypeQQImage(self, id=None, img_type='qq_thumb'):
    
    git-svn-id: file:///usr/local/svn/repos@1513 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/web_interface/helloworld/helloworld/controllers/DisplayResults.py
+++ variation/trunk/web_interface/helloworld/helloworld/controllers/DisplayResults.py
@@ -12,6 +12,8 @@ from pymodule import PassingData
 from formencode import htmlfill
 from pylons.decorators import jsonify	#2008-12-30
 from helloworld.lib.base import *
+import gviz_api	#2009-2-1 google visualization python api to export data into json-related formats
+import numpy, datetime
 
 class DisplayresultsController(BaseController):
 
@@ -24,7 +26,7 @@ class DisplayresultsController(BaseController):
 	@staticmethod
 	def getCallMethodLs():
 		"""
-		2008-12-30
+		2009-1-30
 		"""
 		affiliated_table_name = model.Stock_250kDB.ResultsMethod.table.name
 		list_info  = h.getCallMethodInfo(affiliated_table_name=affiliated_table_name)
@@ -38,7 +40,7 @@ class DisplayresultsController(BaseController):
 	@staticmethod
 	def getPhenotypeMethodLs(call_method_id):
 		"""
-		2008-12-30
+		2009-1-30
 		"""
 		affiliated_table_name = model.Stock_250kDB.ResultsMethod.table.name
 		extra_condition = 's.call_method_id=%s'%(call_method_id)
@@ -64,7 +66,7 @@ class DisplayresultsController(BaseController):
 	@staticmethod
 	def getAnalysisMethodLs(call_method_id, phenotype_method_id):
 		"""
-		2008-12-30
+		2009-1-30
 		"""
 		affiliated_table_name = model.Stock_250kDB.ResultsMethod.table.name	#alias is 's'
 		extra_condition = 's.call_method_id=%s and s.phenotype_method_id=%s'%\
@@ -93,7 +95,7 @@ class DisplayresultsController(BaseController):
 	
 	def form(self, id=None):
 		"""
-		2008-12-30
+		2009-1-30
 		"""
 		if id is None:
 			id = request.params.get('id', 1)
@@ -107,6 +109,7 @@ class DisplayresultsController(BaseController):
 	@jsonify
 	def fetchOne(self, id=None):
 		"""
+		2009-1-30
 		"""
 		if id is None:
 			id = request.params.get('id', None)
@@ -125,7 +128,6 @@ class DisplayresultsController(BaseController):
 		genome_wide_result = h.GeneListRankTest.getResultMethodContent(rm, min_MAF=0.1, pdata=param_data)
 		
 		return_ls = []
-		import gviz_api
 		description = {"position": ("number", "Position"),"value": ("number", "-log Pvalue")}
 		chr2data = {}
 		for i in range(10000):
@@ -150,4 +152,149 @@ class DisplayresultsController(BaseController):
 				}
 		"""
 		return json_result
+	
+	def getCallMethodEigenValue(self):
+		"""
+		2009-2-2
+			get an eigen value given a call_method_id
+		"""
+		call_method_id = request.params.getone('call_method_id')
+		which_eigen_value = request.params.getone('which_eigen_value')
+		rows = model.Stock_250kDB.CallMethodEigenValues.query.filter_by(call_method_id=call_method_id).filter_by(which_eigen=which_eigen).all()
+	
+	def getCallInfoData(self, call_method_id, phenotype_method_id):
+		"""
+		2009-2-2
+			get ecotype id, name, latitude, longitude, pc1, pc2, phenotype for all call_infos in one call_method_id
+		"""
+		#1st get call_info_id 2 PC12 mapping
+		rows = model.Stock_250kDB.CallInfo.query.filter_by(method_id=call_method_id).all()
+		call_info_id2pc12 = {}
+		for row in rows:
+			call_info_id2pc12[row.id] = row.pc_value_ls[:2]
 		
+		
+		#2nd check if model.pheno_data exists
+		pheno_data = getattr(model, 'pheno_data', None)
+		if pheno_data is None:
+			from variation.src.OutputPhenotype import OutputPhenotype
+			pheno_data = OutputPhenotype.getPhenotypeData(model.db.metadata.bind, phenotype_avg_table=model.Stock_250kDB.PhenotypeAvg.table.name,\
+														phenotype_method_table=model.Stock_250kDB.PhenotypeMethod.table.name)
+			model.pheno_data = pheno_data
+		#3rd finally construct the full data and turn it into json
+		column_name_type_ls = [("date", ("date", "Date")), ("ecotypeid", ("number", "Ecotype ID")), ("label", ("string","ID Name Phenotype")), \
+							("lat",("number", "Latitude")), ("lon",("number", "Longitude")), ("name", ("string", "Native Name")), \
+							("pc1", ("number","PC1")), ("pc2", ("number", "PC2")), ("phenotype", ("number", "Phenotype"))]
+		
+		description = dict(column_name_type_ls)
+		rows = model.db.metadata.bind.execute("select * from view_call where call_method_id=%s"%(call_method_id))
+		return_ls = []
+		for row in rows:
+			pc12 = call_info_id2pc12.get(row.call_info_id)
+			if pc12:
+				call_info_pc1, call_info_pc2 = pc12[:2]
+				pc1 = call_info_pc1.pc_value
+				pc2 = call_info_pc2.pc_value
+			else:
+				pc1 = pc2 = 0
+			pheno_row_index = pheno_data.row_id2row_index.get(row.ecotype_id)
+			pheno_col_index = pheno_data.col_id2col_index.get(int(phenotype_method_id))
+			if pheno_row_index is not None and pheno_col_index is not None:
+				phenotype_value = pheno_data.data_matrix[pheno_row_index][pheno_col_index]
+				if phenotype_value == 'NA':
+					phenotype_value = -0.01
+			else:
+				phenotype_value = -0.01	#numpy.nan can't be recognized by ToJSon()
+			label = 'ID:%s. Name=%s. Phenotype=%s.'%(row.ecotype_id, row.nativename, phenotype_value)
+			return_ls.append(dict(ecotypeid=row.ecotype_id, name=row.nativename, lat=row.latitude, lon=row.longitude,\
+								pc1=pc1, pc2=pc2, phenotype=phenotype_value, label=label, date=datetime.date(2009,2,3)))
+		
+		data_table = gviz_api.DataTable(description)
+		data_table.LoadData(return_ls)
+		column_ls = [row[0] for row in column_name_type_ls]
+		#column_ls.sort()	#['date', 'ecotypeid', 'label', 'lat', 'lon', 'name', 'pc1', 'pc2', 'phenotype']
+		json_result = data_table.ToJSon(columns_order=column_ls)	#ToJSonResponse only works with google.visualization.Query
+		return json_result
+	
+	def fetchCallInfoData(self):
+		"""
+		2009-2-2
+			get a complete catalog of call information given a call_method_id
+			including, ecotypeid, name, latitude, longitude, phenotype value, PC1, PC2
+		"""
+		call_method_id = request.params.getone('call_method_id')
+		phenotype_method_id = request.params.getone('phenotype_method_id')
+		
+		key_tup = (call_method_id, phenotype_method_id)
+		CallPhenotypeMethod2call_info_jsdata = getattr(model, 'CallPhenotypeMethod2call_info_data', None)
+		if CallPhenotypeMethod2call_info_jsdata is None:
+			model.CallPhenotypeMethod2call_info_jsdata = {}
+			CallPhenotypeMethod2call_info_jsdata = model.CallPhenotypeMethod2call_info_jsdata
+		
+		call_info_jsdata = CallPhenotypeMethod2call_info_jsdata.get(key_tup)
+		if call_info_jsdata is None:
+			call_info_jsdata = self.getCallInfoData(call_method_id, phenotype_method_id)
+		return call_info_jsdata
+	
+	def getPhenotypeHistImage(self, id=None, img_type='hist_thumb'):
+		phenotype_method_id = request.params.get('phenotype_method_id', id)
+		img_type = request.params.get('img_type', img_type)
+		
+		if phenotype_method_id is None:
+			response.headers['Content-type'] = 'text/plain'
+			return 'No image'
+		phenotype_method_id = int(phenotype_method_id)
+		
+		img_type = request.params.get('img_type', img_type)
+		if img_type.find('svg')!=-1:
+			response.headers['Content-type'] = 'image/svg'
+		else:
+			response.headers['Content-type'] = 'image/png'
+		
+		get_img_data_success = 0
+		if phenotype_method_id:
+			row = model.Stock_250kDB.PhenotypeHistPlots.query.filter_by(phenotype_method_id=phenotype_method_id).first()
+			if row:
+				img_data = getattr(row, img_type, None)
+				if img_data:
+					img_data = img_data.__str__()	#2008-12-26	img_data is a buffer. weird!
+					get_img_data_success = 1
+				else:
+					get_img_data_success = 0
+		if not get_img_data_success:
+			response.headers['Content-type'] = 'text/plain'
+			img_data = 'No Image'
+		return img_data
+	
+	def getCallPhenotypeQQImage(self, id=None, img_type='qq_thumb'):
+		phenotype_method_id = request.params.get('phenotype_method_id', id)
+		call_method_id = request.params.get('call_method_id', 29)
+		img_type = request.params.get('img_type', img_type)
+		
+		if phenotype_method_id is None:
+			response.headers['Content-type'] = 'text/plain'
+			return 'No image'
+		phenotype_method_id = int(phenotype_method_id)
+		call_method_id = int(call_method_id)
+		
+		img_type = request.params.get('img_type', img_type)
+		if img_type.find('svg')!=-1:
+			response.headers['Content-type'] = 'image/svg'
+		else:
+			response.headers['Content-type'] = 'image/png'
+		
+		get_img_data_success = 0
+		if call_method_id and phenotype_method_id:
+			row = model.Stock_250kDB.CallPhenotypeQQPlots.query.filter_by(phenotype_method_id=phenotype_method_id).\
+						filter_by(phenotype_method_id=phenotype_method_id).first()
+			if row:
+				img_data = getattr(row, img_type, None)
+				if img_data:
+					img_data = img_data.__str__()	#2008-12-26	img_data is a buffer. weird!
+					get_img_data_success = 1
+				else:
+					get_img_data_success = 0
+		if not get_img_data_success:
+			response.headers['Content-type'] = 'text/plain'
+			img_data = 'No Image'
+		return img_data
