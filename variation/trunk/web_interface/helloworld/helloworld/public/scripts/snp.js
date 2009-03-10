488334f4f253bee88985fff99f664b94f8a5d916
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2009-03-10 06:49:20 +0000

    add arguments, columnIndexArray & sndElem (2nd element) to function loadTable(baseURL, elemName, columnIndexArray, sndElem): 	columnIndexArray is used to create a dataview out of the google data table. 	sndElem is used to create a motion chart out of the google data table
    git-svn-id: file:///usr/local/svn/repos@1544 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/web_interface/helloworld/helloworld/public/scripts/snp.js
+++ variation/trunk/web_interface/helloworld/helloworld/public/scripts/snp.js
@@ -3,40 +3,37 @@ google.load("visualization", "1", {packages:["scatterchart", 'map', 'motionchart
 
 //var field_ls = ['chromosome', 'position', 'call_method_id', 'phenotype_method_id', 'analysis_method_id', 'score'];
 
-function loadTable(baseURL, elemName) {
+function loadTable(baseURL, elemName, columnIndexArray, sndElem) {
 	var displayTableCallBack = {
 			success: function(o) {
 				var replace_elem = document.getElementById(elemName);
 				var table = new google.visualization.Table(replace_elem);
 				var response = o.responseText;
-				var snpSummaryData = new google.visualization.DataTable(eval("("+response+")"), 0.5);
-				table.draw(snpSummaryData, {allowHtml: true, showRowNumber: true});
+				var jsData = new google.visualization.DataTable(eval("("+response+")"), 0.5);
+				if (!YAHOO.lang.isUndefined(columnIndexArray)){
+					var jsView = new google.visualization.DataView(jsData);		
+					jsView.setColumns(columnIndexArray);
+				}
+				else{
+					var jsView = jsData;
+				}
+				table.draw(jsView, {allowHtml: true, showRowNumber: true});
 				//replace_elem.innerHTML = "<a href="+displayResultsSpace.imgQueryURL+"><img src="+displayResultsSpace.imgQueryURL+"></a>";
 				replace_elem.style.display = 'block';
+				
+				if (!YAHOO.lang.isUndefined(sndElem)){
+					var replace_elem = document.getElementById(sndElem);
+					//var jsView = new google.visualization.DataView(jsData);		
+					//jsView.setColumns(columnIndexArray);
+					var motionChartDiv = document.getElementById(sndElem);
+					var motionChart = new google.visualization.MotionChart(motionChartDiv);
+					motionChart.draw(jsData, {width: 1000, height:700});
+					motionChartDiv.style.display = 'block';
+				}
 			},
 			failure: function(o) {
 				alert("Fetching data for "+elemName+" failed.");
 			}
 	}
 	var transaction = YAHOO.util.Connect.asyncRequest('GET', baseURL, displayTableCallBack, null);
-}
-
-function loadSignificantHits(baseURL, elemName) {
-	var snpSignificantHitsCallBack = {
-			success: function(o) {
-				var replace_elem = document.getElementById(elemName);
-				var table = new google.visualization.Table(replace_elem);
-				var response = o.responseText;
-				//var callInfoData = response.getDataTable();
-				var snpSummaryData = new google.visualization.DataTable(eval("("+response+")"), 0.5);
-				table.draw(snpSummaryData, {showRowNumber: true});
-				//replace_elem.innerHTML = "<a href="+displayResultsSpace.imgQueryURL+"><img src="+displayResultsSpace.imgQueryURL+"></a>";
-				replace_elem.style.display = 'block';
-			},
-			failure: function(o) {
-				alert("Fetching SNP significant hits failed.");
-			}
-	}
-	var transaction = YAHOO.util.Connect.asyncRequest('GET', baseURL, snpSignificantHitsCallBack, null);
-	
 }
