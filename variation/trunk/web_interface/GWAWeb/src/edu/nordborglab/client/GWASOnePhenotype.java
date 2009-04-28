7cc4b848d158ddae8f6d4dd91c46e84f2f38122d
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2009-04-28 07:50:55 +0000

    a client app displaying all GWAS results from one phenotype
    git-svn-id: file:///usr/local/svn/repos@1618 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

---
+++ variation/trunk/web_interface/GWAWeb/src/edu/nordborglab/client/GWASOnePhenotype.java
@@ -0,0 +1,166 @@
+package edu.nordborglab.client;
+
+import java.util.ArrayList;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONException;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.json.client.JSONObject;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.DecoratedPopupPanel;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GWASOnePhenotype implements EntryPoint {
+
+	
+	public DisplayJSONObject jsonErrorDialog;
+	public AccessionConstants constants;
+	private ArrayList<ArrayList<String>> phenotypeCategoryLs;
+	
+	private TabPanel tPanel;
+	
+	
+	private static final String TITLE_DEFAULT_TEXT = "Phenotype";
+	private static final String TITLE_WAITING_TEXT = "Waiting...";
+	
+	private int callMethodID;
+	private int phenotypeMethodID;
+	private String GWABaseURL;
+	private String SNPBaseURL;
+	private String getAnalysisMethodLsURL;
+	
+	
+	private DecoratedPopupPanel popupLink = new DecoratedPopupPanel();
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		jsonErrorDialog = new DisplayJSONObject("Error Dialog");
+		constants = (AccessionConstants) GWT.create(AccessionConstants.class);
+		
+		tPanel = new TabPanel();
+		
+		//tPanel.selectTab(1);
+		//tPanel.setWidth("1000px");
+		tPanel.setAnimationEnabled(true);
+
+		// Add it to the root panel.
+		RootPanel.get("gwt").add(tPanel);
+		
+		//move the onePhenotype division into tabPanel
+		RootPanel onePhenotypeDiv = RootPanel.get("onePhenotype");
+		RootPanel.detachNow(onePhenotypeDiv);	//2009-4-23 detach the element now in order to be attached in another place.
+		tPanel.add(onePhenotypeDiv, "Phenotype");
+		
+		tPanel.selectTab(0);
+		
+		
+		callMethodID = getCallMethodID();
+		phenotypeMethodID = getPhenotypeMethodID();
+		GWABaseURL = getGWABaseURL();
+		SNPBaseURL = getSNPBaseURL();
+		getAnalysisMethodLsURL = getAnalysisMethodLsURL();
+		
+		addAssociationResults();
+	}
+	public native int getCallMethodID()/*-{
+		return $wnd.call_method_id;
+	}-*/;
+	
+	public native int getPhenotypeMethodID()/*-{
+		return $wnd.phenotype_method_id;
+	}-*/;
+	
+	public native String getGWABaseURL()/*-{ return $wnd.GWABaseURL}-*/;
+	public native String getSNPBaseURL()/*-{ return $wnd.SNPBaseURL}-*/;
+	public native String getAnalysisMethodLsURL()/*-{ return $wnd.getAnalysisMethodLsURL}-*/;
+	
+	
+	private class JSONResponseTextHandler implements RequestCallback {
+		public void onError(Request request, Throwable exception) {
+			jsonErrorDialog.displayRequestError(exception.toString());
+			resetTitle();
+		}
+		public void onResponseReceived(Request request, Response response) {
+			String responseText = response.getText();
+			try {
+				JSONValue jsonValue = JSONParser.parse(response.getText());				
+				JSONObject jsonObject = jsonValue.isObject();
+				JSONArray analysisMethodLs = jsonObject.get("options").isArray();
+				for (int i = 0; i < analysisMethodLs.size(); i++) {
+					JSONObject object = analysisMethodLs.get(i).isObject();
+					int analysisMethodID = (int) object.get("value").isNumber().doubleValue();
+					if (analysisMethodID != 0 )
+					{
+						
+						String analysisMethodName = object.get("id").isString().stringValue();
+						String analysisMethodDescription = object.get("description").isString().stringValue();
+						//int analysis_method_id = Integer.parseInt(analysisMethodName);
+						GWASOneResult gwaOneResult = new GWASOneResult(constants, jsonErrorDialog, popupLink, 
+								analysisMethodID, analysisMethodDescription, GWABaseURL, SNPBaseURL+"&analysis_method_id="+analysisMethodID);
+						tPanel.add(gwaOneResult, analysisMethodName);
+					}
+				}
+				tPanel.selectTab(0);
+			}
+			catch (JSONException e) {
+				jsonErrorDialog.displayParseError(responseText);
+			}
+			resetTitle();
+		}
+	}
+	
+	public void addAssociationResults()
+	{
+		/*
+		 * each category is gonna be a tab.
+		 * fetch all categories from server (which further fetches from db)
+		 * 		add a tab for each category
+		 */
+		setIntoWaitState();
+		String url = URL.encode(getAnalysisMethodLsURL);
+		RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, url);
+		try {
+			requestBuilder.sendRequest(null, new JSONResponseTextHandler());
+		} catch (RequestException ex) {
+			jsonErrorDialog.displaySendError(ex.toString());
+			resetTitle();
+		}
+	}
+	
+	public void setIntoWaitState()
+	{
+		//Window.setTitle(TITLE_WAITING_TEXT);
+		//editSaveButton.setEnabled(false);
+	}
+	
+	public void resetTitle()
+	{
+		//Window.setTitle(TITLE_DEFAULT_TEXT);
+		//DOM.getElementById("title").setInnerText(TITLE_DEFAULT_TEXT);
+	}
+}
+
---
+++ variation/trunk/web_interface/GWAWeb/src/edu/nordborglab/client/GWASOneResult.java
@@ -0,0 +1,122 @@
+package edu.nordborglab.client;
+
+import java.util.Set;
+
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONException;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.json.client.JSONString;
+
+import com.google.gwt.user.client.ui.DecoratedPopupPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.visualization.client.DataTable;
+
+
+public class GWASOneResult extends CustomVerticalPanel{
+	private AccessionConstants constants;
+	private DisplayJSONObject jsonErrorDialog;
+	
+	private int analysisMethodID;
+	private String analysisMethodDescription;
+	private String GWABaseURL;
+	private String SNPBaseURL;
+	
+	private HTML statusReport;
+	private String[] colors = {"blue", "green", "red", "cyan", "purple"};
+	
+	private DataTable dataTable;
+	private DecoratedPopupPanel popupLink;
+	
+	public GWASOneResult(AccessionConstants constants, DisplayJSONObject jsonErrorDialog, DecoratedPopupPanel popupLink, 
+			int analysisMethodID, String analysisMethodDescription, String GWABaseURL, String SNPBaseURL)
+	{
+		super(constants, jsonErrorDialog, constants.GWASOneResultHelpID());
+		this.constants = constants;
+		this.jsonErrorDialog = jsonErrorDialog;
+		this.analysisMethodID = analysisMethodID;
+		this.analysisMethodDescription = analysisMethodDescription;
+		this.GWABaseURL = GWABaseURL;
+		this.SNPBaseURL = SNPBaseURL;
+		
+		this.popupLink = popupLink;
+		
+		statusReport = new HTML(this.constants.LoadingText());
+		this.add(statusReport);
+		
+		this.add(new HTML(this.analysisMethodDescription));
+		
+		loadGWA();
+	}
+	
+	private class LoadGWAResponseHandler implements RequestCallback {
+		public void onError(Request request, Throwable exception) {
+			jsonErrorDialog.displayRequestError(exception.toString());
+			resetTitle();
+		}
+
+		public void onResponseReceived(Request request, Response response) {
+			String responseText = response.getText();
+			try {
+				JSONValue jsonValue = JSONParser.parse(response.getText());
+				JSONObject serverData = jsonValue.isObject();	//jsonValue.isArray();
+				JSONObject chr2data = serverData.get("chr2data").isObject();
+				JSONObject chr2length = serverData.get("chr2length").isObject();
+				double max_value = serverData.get("max_value").isNumber().doubleValue();
+				int max_length = (int) serverData.get("max_length").isNumber().doubleValue();
+				Set<String> keys = chr2data.keySet();
+				int i =0;
+				for (String chromosome : keys) 
+				//for (i=0; i<keys.size(); i++)
+				{
+					//String chromosome = String.valueOf(i);
+					//JSONString data = chr2data.get(chromosome).isString();
+					String data = chr2data.get(chromosome).toString();
+					
+					//jsonErrorDialog.displayJSONObject(chr2data.get(chromosome));
+					int chrLength = (int) chr2length.get(chromosome).isNumber().doubleValue();
+					//jsonErrorDialog.displayRequestError("chromosome "+ chromosome + "length: " + chrLength);
+					String color = colors[i%colors.length];
+					AssociationScatterChart associationChart = new AssociationScatterChart(constants, jsonErrorDialog, chromosome,
+							color, chrLength, max_length, max_value, SNPBaseURL);
+					add(associationChart);
+					dataTable = Common.asDataTable(data.substring(1, data.length()-1));	//2009-4-25 data has extra " on both ends
+					associationChart.draw(dataTable);
+					i += 1;
+				}
+			} catch (JSONException e) {
+				jsonErrorDialog.displayParseError(responseText);
+			}
+			resetTitle();
+		}
+	}
+	
+	private void loadGWA()
+	{
+		setIntoWaitState();
+		String url = URL.encode(this.GWABaseURL + "&analysis_method_id="+this.analysisMethodID);
+		RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, url);
+		try {
+			requestBuilder.sendRequest(null, new LoadGWAResponseHandler());
+		} catch (RequestException ex) {
+			jsonErrorDialog.displaySendError(ex.toString());
+			resetTitle();
+		}
+	}
+	
+	public void setIntoWaitState()
+	{
+		statusReport.setVisible(true);
+	}
+	public void resetTitle()
+	{
+		statusReport.setVisible(false);
+	}
+}
