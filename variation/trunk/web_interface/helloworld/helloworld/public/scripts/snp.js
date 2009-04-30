05b433b7190a28f8db9db2e45f6a1a88445ab8fe
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2009-04-30 07:48:33 +0000

    make the status element disappear upon loading complete
    git-svn-id: file:///usr/local/svn/repos@1623 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/web_interface/helloworld/helloworld/public/scripts/snp.js
+++ variation/trunk/web_interface/helloworld/helloworld/public/scripts/snp.js
@@ -1,5 +1,3 @@
-google.load("visualization", "1", {packages:["scatterchart", 'map', 'motionchart', "table"]});
-
 
 //var field_ls = ['chromosome', 'position', 'call_method_id', 'phenotype_method_id', 'analysis_method_id', 'score'];
 
@@ -7,6 +5,7 @@ function loadTable(baseURL, elemName, columnIndexArray, sndElem) {
 	var displayTableCallBack = {
 			success: function(o) {
 				var replace_elem = document.getElementById(elemName);
+				var status_elem = document.getElementById(elemName+"Status");
 				var table = new google.visualization.Table(replace_elem);
 				var response = o.responseText;
 				var jsData = new google.visualization.DataTable(eval("("+response+")"), 0.5);
@@ -20,9 +19,11 @@ function loadTable(baseURL, elemName, columnIndexArray, sndElem) {
 				table.draw(jsView, {allowHtml: true, showRowNumber: true});
 				//replace_elem.innerHTML = "<a href="+displayResultsSpace.imgQueryURL+"><img src="+displayResultsSpace.imgQueryURL+"></a>";
 				replace_elem.style.display = 'block';
+				status_elem.style.display = 'none';
 				
 				if (!YAHOO.lang.isUndefined(sndElem)){
 					var replace_elem = document.getElementById(sndElem);
+					var status_elem = document.getElementById(sndElem+"Status");
 					//var jsView = new google.visualization.DataView(jsData);		
 					//jsView.setColumns(columnIndexArray);
 					var motionChartDiv = document.getElementById(sndElem);
@@ -31,9 +32,10 @@ function loadTable(baseURL, elemName, columnIndexArray, sndElem) {
 					options['state'] = '{"iconType":"BAR","showTrails":false,"xAxisOption":"6","colorOption":"7","yAxisOption":"6","playDuration":15,"orderedByX":true,"orderedByY":false,"sizeOption":"_UNISIZE","nonSelectedAlpha":0.4,"stateVersion":3,"dimensions":{"iconDimensions":["dim0"]},"yLambda":1,"yZoomedIn":false};';
 					//{"iconType":"BAR","xZoomedDataMin":null,"yZoomedDataMax":null,"xZoomedIn":false,"iconKeySettings":[],"showTrails":false,"xAxisOption":"6","colorOption":"7","yAxisOption":"6","playDuration":15,"xZoomedDataMax":null,"orderedByX":true,"duration":{"multiplier":1},"xLambda":1,"orderedByY":false,"sizeOption":"_UNISIZE","yZoomedDataMin":null,"nonSelectedAlpha":0.4,"stateVersion":3,"dimensions":{"iconDimensions":["dim0"]},"yLambda":1,"yZoomedIn":false};
 					options['width'] = 1000;
-					options['height'] = 700;					
+					options['height'] = 700;
 					motionChart.draw(jsData, options);
 					motionChartDiv.style.display = 'block';
+					status_elem.style.display = 'none';
 				}
 			},
 			failure: function(o) {
