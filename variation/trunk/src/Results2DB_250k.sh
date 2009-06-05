f184fcb4fec03fe8ecbef2cf72d99f3cf7838814
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2009-06-05 23:31:41 +0000

    add two more arguments to the program:
    	START_PHENOTYPE_ID END_PHENOTYPE_ID
    
    
    
    git-svn-id: file:///usr/local/svn/repos@1707 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/Results2DB_250k.sh
+++ variation/trunk/src/Results2DB_250k.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if test $# -ne 4
+if test $# -ne 6
 then
 	echo "Usage:"
-	echo "    Results2DB_250k.sh FILE_PREFIX CALL_METHOD_ID ANALYSIS_METHOD_ID DB_PASSWORD"
+	echo "    Results2DB_250k.sh FILE_PREFIX CALL_METHOD_ID ANALYSIS_METHOD_ID DB_PASSWORD START_PHENOTYPE_ID END_PHENOTYPE_ID"
 	echo
 	echo "This script calls Results2DB_250k.py to submit results into database."
 	echo
 	echo "Example:"
-	echo " Results2DB_250k.sh /Network/Data/250k/tmp-bvilhjal/marg_results/Marg_newDataset_ 17 5 secret"
+	echo " Results2DB_250k.sh /Network/Data/250k/tmp-bvilhjal/marg_results/Marg_newDataset_ 17 5 secret 1 180"
 	exit
 fi
 
@@ -16,9 +16,11 @@ file_prefix=$1
 call_method_id=$2
 analysis_method_id=$3
 db_password=$4
+start_phenotype_id=$5
+end_phenotype_id=$6
 
-for((i=1;i<300;i++))
+for((i=$start_phenotype_id;i<=$end_phenotype_id;i++))
 	do echo $i;
-	~/script/variation/src/Results2DB_250k.py -a $call_method_id -e $i -i $file_prefix$i\_* -l $analysis_method_id -u yh -p $db_password -c
+	~/script/variation/src/Results2DB_250k.py -a $call_method_id -e $i -i $file_prefix$i\_*[^s][^r].tsv -l $analysis_method_id -u yh -p $db_password -c
 done
 
