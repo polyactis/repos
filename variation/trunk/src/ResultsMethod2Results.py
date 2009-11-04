abb1bc68f233085e5f348aa7d2fa0d3072b2ac24
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2009-11-04 04:34:24 +0000

    @classmethod 	def rm2result(cls, session, rm, snp_info, max_rank=1000, commit=False, min_rank=1, results_directory=None): 		""" 		2009-11-2 			split out of run() 		"""
    git-svn-id: file:///usr/local/svn/repos@1849 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/src/ResultsMethod2Results.py
+++ variation/trunk/src/ResultsMethod2Results.py
@@ -48,7 +48,41 @@ class ResultsMethod2Results(object):
 		self.ad = ProcessOptions.process_function_arguments(keywords, self.option_default_dict, error_doc=self.__doc__, class_to_have_attr=self)
 		if self.analysis_method_id_ls is not None:
 			self.analysis_method_id_ls = getListOutOfStr(self.analysis_method_id_ls, data_type=int)
+	
+	@classmethod
+	def rm2result(cls, session, rm, snp_info, max_rank=1000, commit=False, min_rank=1, results_directory=None):
+		"""
+		2009-11-2
+			split out of run()
+		"""
 		
+		# 2009-5-1 check whether it's already in db.
+		db_entries = Stock_250kDB.Results.query.filter_by(results_id=rm.id)
+		if db_entries.count()==max_rank-min_rank+1:
+			sys.stderr.write("%s already in db. Ignore.\n"%rm.id)
+			return
+		
+		param_data = PassingData(min_MAC=0)
+		genome_wide_result = GeneListRankTest.getResultMethodContent(rm, results_directory=results_directory, min_MAF=0., \
+																pdata=param_data)
+		if genome_wide_result:
+			for rank in range(min_rank, max_rank+1):
+				data_obj = genome_wide_result.get_data_obj_at_given_rank(rank)
+				if data_obj is not None:
+					snps_id = snp_info.getSnpsIDGivenChrPos(data_obj.chromosome, data_obj.position)
+					if data_obj.extra_col_ls:
+						result_obj = cPickle.dumps(data_obj.extra_col_ls)
+					else:
+						result_obj = None
+					Stock_250kDB.Results(snps_id=snps_id, results_id=rm.id, score=data_obj.value, rank=rank, beta=getattr(data_obj, 'beta1', None),\
+										maf=data_obj.maf, mac=data_obj.mac, genotype_var_perc=data_obj.genotype_var_perc,\
+										object=result_obj)
+		if commit:
+			session.flush()
+			#session.commit()
+		else:
+			session.rollback()
+	
 	def run(self):
 		"""
 		2009-6-10
@@ -61,41 +95,17 @@ class ResultsMethod2Results(object):
 									password=self.db_passwd, hostname=self.hostname, database=self.dbname, schema=self.schema)
 		db.setup(create_tables=False)
 		session = db.session
-		#session.begin()
+		session.begin()
 		
 		snp_info = DrawSNPRegion.getSNPInfo(db)
 		
 		query = Stock_250kDB.ResultsMethod.query.filter_by(call_method_id=self.call_method_id).\
 			filter(Stock_250kDB.ResultsMethod.analysis_method_id.in_(self.analysis_method_id_ls))
-		param_data = PassingData(min_MAC=0)
 		for rm in query:
-			# 2009-5-1 check whether it's already in db.
-			db_entries = Stock_250kDB.Results.query.filter_by(results_id=rm.id).first()
-			if db_entries:
-				sys.stderr.write("%s already in db. Ignore.\n"%rm.id)
-				continue
-			
-			genome_wide_result = GeneListRankTest.getResultMethodContent(rm, results_directory=self.results_directory, min_MAF=0., \
-																	pdata=param_data)
-			if genome_wide_result:
-				for i in range(self.max_rank):
-					data_obj = genome_wide_result.get_data_obj_at_given_rank(i+1)
-					if data_obj is not None:
-						snps_id = snp_info.getSnpsIDGivenChrPos(data_obj.chromosome, data_obj.position)
-						if data_obj.extra_col_ls:
-							result_obj = cPickle.dumps(data_obj.extra_col_ls)
-						else:
-							result_obj = None
-						Stock_250kDB.Results(snps_id=snps_id, results_id=rm.id, score=data_obj.value, rank=i+1, beta=getattr(data_obj, 'beta1', None),\
-											maf=data_obj.maf, mac=data_obj.mac, genotype_var_perc=data_obj.genotype_var_perc,\
-											object=result_obj)
-			if self.commit:
-				session.flush()
-				#session.commit()
-				session.clear()
-			else:
-				session.rollback()
-
+			self.rm2result(session, rm, snp_info, max_rank=self.max_rank, commit=self.commit, results_directory=self.results_directory)
+		if self.commit:
+			session.commit()
+	
 if __name__ == '__main__':
 	from pymodule import ProcessOptions
 	main_class = ResultsMethod2Results
