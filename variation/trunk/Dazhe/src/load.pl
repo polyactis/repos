431a73efba489d027f0020498a8dc6407a95db45
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2010-02-18 23:30:21 +0000

    expose 4 arguments (hostname dbname db_user db_passwd gff_file) to this program
    git-svn-id: file:///usr/local/svn/repos@1918 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/Dazhe/src/load.pl
+++ variation/trunk/Dazhe/src/load.pl
@@ -1,15 +1,19 @@
+#!/usr/bin/env perl
+# Usage example: load.pl hostname dbname db_user db_passwd gff_file
+
 use Bio::DB::SeqFeature::Store;
 use Bio::DB::SeqFeature::Store::GFF3Loader;
 
+
 # Open the sequence database
-my $db      = Bio::DB::SeqFeature::Store->new( -adaptor => 'DBI::mysql',
-                                                 -dsn     => 'dbi:mysql:gbrowse',
-					       -user => 'gbrowse_loader',
-					       -pass => 'loader',
-                                                 -write   => 1 );
+my $db  = Bio::DB::SeqFeature::Store->new( -adaptor => 'DBI::mysql',
+                        -dsn     => 'dbi:mysql:database='.$ARGV[1].';host='.$ARGV[0],
+                        -user => $ARGV[2],
+					    -pass => $ARGV[3],
+                        -write   => 1 );
 #$db->init_database();
 
 my $loader = Bio::DB::SeqFeature::Store::GFF3Loader->new(-store    => $db,
 							   -verbose  => 1);
 
-$loader->load($ARGV[0]);
+$loader->load($ARGV[4]);
