6778bbc7f6a1a50614bb8c5a1bb404d20a58b175
yh <yh@c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40>
2010-02-19 01:01:46 +0000

    modify it to accept arguments such as :
    Usage example: init_db.pl hostname dbname db_user db_passwd
    
    
    git-svn-id: file:///usr/local/svn/repos@1920 c5b91a9c-90b7-4db6-b2ac-9ef0fe68db40

--- variation/trunk/Dazhe/src/init_db.pl
+++ variation/trunk/Dazhe/src/init_db.pl
@@ -1,10 +1,13 @@
+#!/usr/bin/env perl
+# 2010-2-18 Usage example: init_db.pl hostname dbname db_user db_passwd
+
 use Bio::DB::SeqFeature::Store;
 use Bio::DB::SeqFeature::Store::GFF3Loader;
 
 # Open the sequence database
 my $db      = Bio::DB::SeqFeature::Store->new( -adaptor => 'DBI::mysql',
-                                                 -dsn     => 'dbi:mysql:gbrowse',
-					       -user => 'gbrowse_loader',
-					       -pass => 'loader',
-					       -create => 1,
+                                                 -dsn     => 'dbi:mysql:database='.$ARGV[1].';host='.$ARGV[0],
+                            -user => $ARGV[2],
+            	            -pass => $ARGV[3],
+		                    -create => 1,
                                                  -write   => 1 );
